6dc4253103d761f0ecf6f5cf9b429b7e916835a0
sven <sven@svn-users.technikum29.de>
2009-03-16 22:57:27 +0000

    Telefunken T40w mit Oeffnungstechnik (Slider-Widget) implementiert und testweise hochgeladen (Text auf Inhaltsseite noch auskommentiert).
    --sven @ workstation
    
    
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@85 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ shared/js/slider/slider.js
@@ -0,0 +1,114 @@
+/*
+ * technikum29.de | javscripts: slider
+ * 
+ * This is a lightweight slider implementation originally from
+ * http://www.arantius.com/article/lightweight+javascript+slider+control
+ * (2006 Anthony Lieuallen)
+ * 
+ *
+ */
+
+// add event function from http://www.dynarch.com/projects/calendar/
+function addAnEvent(el, evname, func) {
+    if (el.attachEvent) { // IE
+        el.attachEvent("on" + evname, func);
+    } else if (el.addEventListener) { // Gecko / W3C
+        el.addEventListener(evname, func, true);
+    } else {
+        el["on" + evname] = func;
+    }
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function drawSliderByVal(slider) {
+	var knob=slider.getElementsByTagName('img')[0];
+	var p=(slider.val-slider.min)/(slider.max-slider.min);
+	var x=(slider.scrollWidth-30)*p;
+	knob.style.left=x+"px";
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function setSliderByClientX(slider, clientX) {
+	var p=(clientX-slider.offsetLeft-15)/(slider.scrollWidth-30);
+	slider.val=(slider.max-slider.min)*p + slider.min;
+	if (slider.val>slider.max) slider.val=slider.max;
+	if (slider.val<slider.min) slider.val=slider.min;
+
+	drawSliderByVal(slider);
+	slider.onchange(slider.val, slider.num);
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function sliderClick(e) {
+	var el=sliderFromEvent(e);
+	if (!el) return;
+
+	setSliderByClientX(el, e.clientX);
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function sliderMouseMove(e) {
+	var el=sliderFromEvent(e);
+	if (!el) return;
+	if (activeSlider<0) return;
+
+	setSliderByClientX(el, e.clientX);
+	stopEvent(e);
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function sliderFromEvent(e) {
+	if (!e && window.event) e=window.event;
+	if (!e) return false;
+
+	var el;
+	if (e.target) el=e.target;
+	if (e.srcElement) el=e.srcElement;
+
+	if (!el.id || !el.id.match(/slider\d+/)) el=el.parentNode;
+	if (!el) return false;
+	if (!el.id || !el.id.match(/slider\d+/)) return false;
+
+	return el;
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function attachSliderEvents() {
+	var divs=document.getElementsByTagName('div');
+	var divNum;
+	for(var i=0; i<divs.length; i++) {
+		if (divNum=divs[i].id.match(/\bslider(\d+)\b/)) {
+			// set initial properties
+			divNum=parseInt(divNum[1]);
+			divs[i].min=slider[divNum].min;
+			divs[i].max=slider[divNum].max;
+			divs[i].val=slider[divNum].val;
+			divs[i].onchange=slider[divNum].onchange;
+			divs[i].num=divNum;
+			// and make sure the display matches
+			drawSliderByVal(divs[i]);
+			divs[i].onchange(divs[i].val, divNum);
+
+			addAnEvent(divs[i], 'mousedown', function(e){
+				sliderClick(e);
+				var el=sliderFromEvent(e);
+				if (!el) return;
+				activeSlider=el.num;
+				stopEvent(e);
+			});
+			addAnEvent(divs[i], 'mouseup', function(e){
+				activeSlider=-1;
+				stopEvent(e);
+			});
+		}
+	}
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//borrowed from prototype: http://prototype.conio.net/
+function stopEvent(event) {
+	if (event.preventDefault) {
+		event.preventDefault();
+		event.stopPropagation();
+	} else {
+		event.returnValue=false;
+		event.cancelBubble=true;
+	}
+}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+addAnEvent(window, 'load', attachSliderEvents);
+addAnEvent(document, 'mousemove', sliderMouseMove);
+var activeSlider=-1;

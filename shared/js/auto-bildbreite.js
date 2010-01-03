9b075943809ab6ff13c3e7fabfa5f74aa79b72dc
incubator <incubator@svn-users.technikum29.de>
2010-01-03 04:16:55 +0000

    Zwei neue CSS-Klassen: * auto-bildbreite: Passt die Breite einer Bildbox automatisch nach dem Bild an (mittels JavaScript) * clear-after: Macht ein clear-Element in einer floating Bildbox überflüssig
    Diese Klassen wurden in allen Inhaltsdateien (Extradateien noch nicht) angewendet. Außerdem wurden alle Dateien überarbeitet (Sourcecode-Layout und -Validität, etwas Design). Zudem wurde überall das $Id$-SVN-Property eingebaut.
    
    -- sven @ netbook
    
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@130 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ shared/js/auto-bildbreite.js
@@ -0,0 +1,47 @@
+/*
+ * technikum29.de | javscripts: auto-bildbreite
+ * $Id$
+ *
+ * Zur Ergaenzung des Bildbox-Konzepts wurde Januar 2010 die automatische
+ * Erkennung der Breiten der Bilder hinzugefuegt. Funktionsweise:
+ *
+ * <div class="box center auto-bildbreite">
+ *   <img ... />
+ *   <p class="bildtext"> bla bla bla </p>
+ * </div>
+ * 
+ * Dieses Script parst alle divs und setzt die Breite der boxen auf die Breite
+ * des ersten enthaltenen Bildes.
+ *
+ * (c) 2010 Sven Koeppel
+ * Released under the public domain.
+ */
+ 
+var t29_auto_bildbreite_class = /\bauto-bildbreite\b/i;
+ 
+var t29_auto_bildbreite_old_onload = window.onload;
+window.onload = function() {
+	if (typeof(t29_auto_bildbreite_old_onload)=="function")
+		t29_auto_bildbreite_old_onload();
+	
+	// get all divs
+	var divs = document.getElementsByTagName('div');
+	var divNum;
+	
+	for(var i = 0; i < divs.length; i++) {
+		if(!divs[i].getAttribute("class"))
+			continue; // skip this one
+
+		if(divs[i].getAttribute("class").search(t29_auto_bildbreite_class) != -1) {
+			// found class
+			var imgs = divs[i].getElementsByTagName("img");
+			if(imgs[0]) {
+				// has at least one img
+				var width = imgs[0].width;
+				var old_style = divs[i].getAttribute("style");
+				if(!old_style) old_style = "";
+				divs[i].setAttribute("style", "width: " + width + "px; " + old_style);
+			}
+		} // if found class
+	} // for
+};

0cf76e048003094753e6e7e9a97938aaf15f405c
sven <sven@svn-users.technikum29.de>
2012-06-01 15:18:17 +0000

    Redirects: Zahlreiche alte Extraseiten geloescht, stattdessen neues Redirect-System angelegt.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@267 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ lib/redirects.php
@@ -0,0 +1,22 @@
+<?php
+
+array(
+	# Inhaltsseiten
+	'/de/suche' => '/de/wir-suchen',
+	'/en/search' => '/en/wanted',
+
+	# Geraete/Extraseiten
+	'/de/geraete/anita' => '/de/rechnertechnik/elektronenroehren',
+	'/en/devices/anita' => '/en/computer/electron-tubes',
+	'/de/geraete/combitron' => '/de/rechnertechnik/programmierbare',
+	'/en/devices/combitron' => '/en/computer/programmable',
+	'/de/geraete/ibm_77' => '/de/rechnertechnik/lochkarten-edv#ibm77',
+	'/de/geraete/kernspeicher' => '/de/rechnertechnik/speichermedien#kernspeicher',
+	'/de/geraete/laufzeitspeicher' => '/de/rechnertechnik/speichermedien#laufzeitspeicher',
+	'/de/geraete/pdp_8I' => '/de/rechnertechnik/fruehe-computer#pdp8i',
+	'/de/geraete/univac9400' => '/de/rechnertechnik/univac9400',
+	'/de/geraete/univac9400/univac_9300' => '/de/rechnertechnik/univac9200',
+	'/en/devices/univac9400/univac_9300' => '/en/computer/univac9200',
+	'/de/geraete/bull-bs-pr/*' => '/de/rechnertechnik/tabelliermaschine',
+
+);

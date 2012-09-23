97dddb5fbda0170037ef638f4a4e2f84b38bac54
heribert <heribert@svn-users.technikum29.de>
2012-09-23 09:24:40 +0000

    git-svn-id: https://www.technikum29.de/svn/technikum29-www@302 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ en/computer/gamma55.php
@@ -0,0 +1,108 @@
+<?php
+	$seiten_id = 'gamma55';
+	$version = '$Id: gamma55.php 299 2012-09-19 09:38:55Z heribert $';
+	$title = 'A second generation calculator: The BULL GAMMA 55 (GE-55)';
+	
+	require "../../lib/technikum29.php";
+?>
+    
+	<h2>A second generation calculator: The BULL GAMMA 55 (GE-55)</h2>
+	
+	 <div class="box center clear-after">
+		<img src="/shared/photos/rechnertechnik/bull-ge55.jpg" alt="BULL Gamma 55 Computer-Anlage" width="750" height="389" />
+		<div class="bildtext">
+		<p>On the left the printer I41 is visible, in the background the CPU (2m
+wide!) and on the right the paper tape puncher can be seen. On top of
+the desk is a punch card reader with an alphanumeric keyboard in front
+of it.</p>
+		</div>
+		</div>
+	<p>
+        We are now the proud owners of a new BULL computer system: A BULL Gamma
+55 (also known as GE-55 after the merge with General-Electric). The
+system now in the collection was stored in Switzerland with all
+accompanying documentation, punched cards etc. for 26 years.<br>
+
+It was developed in 1966 by BULL in France and hit the market in 1967.
+It was aimed at small and middle sized companies that were too small
+as customers for other, larger computer systems.<br>
+
+This computer demonstrates the tremendous advances of computer
+technology in the 1960s. While all instructions on the (larger) Gamma
+10 are implemented with a lot of active circuitry stepping through
+many cycles, this is done in the Gamma 55 by means of a rather large
+read-only-memory. This is an implementation of a microprogram which
+reduces the necessary hardware significantly, making the machine
+cheaper and more reliable.<br>
+
+The basic implementation of the machines makes heavy use of mechanical
+parts but at least the card reader is an optical device.<br>
+<div class="box left">
+		<img src="/shared/photos/rechnertechnik/ge-55-2.jpg" alt="Während der Restauration" width="566" height="322" />
+		<div class="bildtext">
+
+<p>During reconstruction:
+The picture on the left shows the system during the refurbishment of
+the cabinets etc.</p>
+</div>
+</div>
+
+<p>Our machine can be programmed in machine language or some kind of a
+mini-COBOL (Common Business Oriented Language). The compiler is loaded
+via punched cards. To start a program a Supervisor that is loaded by
+punched cards is necessary.<br>
+
+The system was advertised with 2.5 kB, 5 kB or 10 kB of main memory
+(core memory). Our machine has 5 kB of memory and it is really
+astonishing that a high level language like COBOL - even our
+mini-COBOL - is feasible with such a tiny amount of memory at all.<br>
+
+As an extension a memory drum was offered which also allowed the use of
+a mini-FORTRAN (FORmula TRANslator).<br>
+
+Although BULL was quite inventive concerning software their hardware
+was quite outdated. The boards used in this machine are the same as
+those in the earlier GAMMA 10 (based on Germanium transistors). At the
+same time other companies like UNIVAC or IBM already employed
+integrated circuits (DTL, Diode-Transistor-Logic) for their machines.<br>
+
+We are still working on restoring this wonderful machine to working
+condition. So stay tuned, we will post status updates along the
+process.
+    </p>
+
+	<div class="cols">
+<div class="leftcol">
+<p class="small">Contemporary document: In the following you find some quotations from
+the system description published in 1967/1968:<br>
+<blockquote><p class="small">"After intensive market research, BULL GENERAL ELECTRIC developed a
+versatile computer system: Its internal structure resembles that of
+modern computers. Furthermore it is a data processing system that
+allows direct input via keyboard. It is a real data processing system
+because it can grow with your needs and its memory capacity (internal
+as well as external) can be extended.<br>
+
+Construction: The central processing unit (CPU) of the GE-55 supports
+four channels; three of which are normal speed channels for slow
+input/output devices and one high speed channel used by external
+memory units or a fast line printer...<br>
+</div>
+<div class="rightcol"><blockquote><p class="small">
+....the cycle time is 7.9 us. Characters are represented by their
+respective ISO-code. The core memory is used as data and program
+memory. A since byte, comprised of 8 data bits and one parity bit,
+stores a single alphabetical character or up to two numerical
+digits...<br>
+
+Instructions are analyzed and executed under control of a
+read-only-memory with a capacity of 1024 word of 36 bits each... This
+memory contains micro programs for control, supervision and execution
+as well as arithmetic and character conversion tables.<br>
+
+Software: The programming system is mainly comprised of the following
+parts: Symbolic languages that facilitate the actual programming task
+and an assembler for translating assembler programs to machine
+language...
+"</div>
+</blockquote></p></div>
+<div class="clear">&nbsp;</div><br>

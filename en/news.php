1dcdac302ff2a3f4f7a8cba1c20f1fde66eb03d8
heribert <heribert@svn-users.technikum29.de>
2012-10-29 16:33:26 +0000

    engl. News aktualisiert
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@311 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- en/news.php
+++ en/news.php
@@ -30,6 +30,24 @@ MENU;
 
     <ul class="news-feed">
 	
+	
+	<li><h3>September 2012</h3>
+We have overhauled our web pages. The most important changes are a better
+outline, better navigation and an overall better appearance.
+
+<li><h3>July 2012</h3>
+<div class="box left">
+<img src="/shared/photos/rechnertechnik/bull-ge55.jpg" width="375" height="195"/>
+</div> 
+<div class="clear"></div>
+
+<p>A "new" computer back from the punch card era has arrived at the museum: A BULL
+Gamma 65 (also known as GE-55). A wonderful system that has been built in
+1966/1967 and survived 26 years in storage in Switzerland. Since the system
+comes with all of the necessary documentation we are planning to restore it to
+working condition (cf. the section describing the Gamma 55).</p><br>
+	
+	
 	<li><h3>April 2012</h3>
 	<div class="box left">
 <img src="/shared/photos/rechnertechnik/univac/lochkarten.jpg" width="350" height="87"/>

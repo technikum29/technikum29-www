22ea6cea3e04396d582fdced04ca4dfa350f48eb
heribert <heribert@svn-users.technikum29.de>
2013-02-17 21:02:47 +0000

    git-svn-id: https://www.technikum29.de/svn/technikum29-www@330 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/kommerzielle.php
+++ de/rechnertechnik/kommerzielle.php
@@ -17,7 +17,8 @@
         <img src="/shared/photos/rechnertechnik/olivetti_p203.jpg" alt="Olivetti P203" width="406" height="378" />
         <div class="bildtext">
             <b>Olivetti P 203</b>
-			<p>Im Jahre 1968 wurde die P 101 (mit etwas vergrößerter Speicherkapazität) mit einer elektrischen Schreibmaschine gekoppelt. So war es möglich, z.B. tabellarische Rechnungen unmittelbar auszudrucken. Dieser "Computer" besticht durch seine außergewöhnliche Form und wurde vom berühmten italienischen Designer "Mario Bellini" gestylt. Für das Design hat Olivetti seinerzeit Preise erhalten. Auch heute wird das ästhetische Äußere bewundert. Die nachfolgenden Olivetti Rechner wurden dagegen im Stil der 70er Jahre als schmucklose kantige Würfel präsentiert.</p>
+			<p>Im Jahre 1968 wurde die P 101 (mit etwas vergrößerter Speicherkapazität) mit einer elektrischen Schreibmaschine gekoppelt. So war es möglich, z.B. tabellarische Rechnungen unmittelbar auszudrucken. Jedoch konnten nur Zahlen vom Rechner unmittelbar übernommen werden. Texte musste man nach wie vor manuell auf der Tastatur einfügen.<br>
+			Dieser "Computer" besticht durch seine außergewöhnliche Form und wurde vom berühmten italienischen Designer "Mario Bellini" gestylt. Für das Design hat Olivetti seinerzeit Preise erhalten. Auch heute wird das ästhetische Äußere bewundert. Die nachfolgenden Olivetti Rechner wurden dagegen im Stil der 70er Jahre als schmucklose kantige Würfel präsentiert.</p>
 		</div>
 	</div>
 	
@@ -29,11 +30,32 @@
     <div class="box center auto-bildbreite">
         <a href="/de/geraete/ncr446.php"><img src="/shared/photos/rechnertechnik/ncr-446.jpg" alt="NCR 446 Rechner" width="694" height="520" /></a>
         </div>
-     		<p>Der<b> NCR 446 </b> ist ein Rechner der 2. Generation (Transistortechnik). Er wurde ab 1966/67 in Deutschland (National Registrier Kassen GmbH, Augsburg) von 4 Ingenieuren entwickelt, die gerade ihr Examen an der Universität absolviert hatten. Im Jahre 1968 war er marktreif. Die Architektur ist ungewöhnlich. Das gesamte Programm befindet sich auf dem Lochstreifen im Programmleser. Verknüpfungen werden in einem gefädeltem ROM ausgeführt. Die Anlage wurde als "Elektronische Fakturiermaschine" angeboten, weil das ihr Hauptanwendungsgebiet war. Dennoch verwendeten auch Statiker den Rechner, da er praktisch beliebig programmierbar war (und natürlich immer noch ist!). <br>
-	     	Durch Anklicken des Bildes erhält man eine Detailaufnahme.<br> Die Anlage beinhaltet neben dem Programmleser noch zwei Daten-Lochstreifenleser, einen Lochstreifenstanzer, die Eingabetastatur und eine IBM Kugelkopfmaschine als Drucker.</br>
+     		<p>Der<b> NCR 446 </b> ist ein Rechner der 2. Generation (Transistortechnik). Er wurde ab 1966/67 in Deutschland (National Registrier Kassen GmbH, Augsburg) von 4 Ingenieuren entwickelt, die gerade ihr Examen an der Universität absolviert hatten. Im Jahre 1967/68 war er marktreif. Die Architektur ist ungewöhnlich. Das gesamte Programm befindet sich auf dem Lochstreifen im Programmleser. Verknüpfungen werden in einem gefädeltem ROM ausgeführt. Die Anlage wurde als "Elektronische Fakturiermaschine" angeboten, weil das ihr Hauptanwendungsgebiet war. Dennoch verwendeten auch Statiker den Rechner, da er praktisch beliebig programmierbar war (und natürlich immer noch ist!). 
+			<br> Die Anlage beinhaltet neben dem Programmleser noch zwei Daten-Lochstreifenleser, einen Lochstreifenstanzer, die Eingabetastatur und eine IBM Kugelkopfmaschine als Drucker.
+			Durch Anklicken des Bildes erhält man eine Detailaufnahme.<br>
+			Billig war ein Einstieg in die bisher unbekannte Welt des elektronischen Rechnens nebst Textausgabe nicht: Das einfachste Modell ohne Lochstreifenstanzer und ohne zusätzliche Lochstreifen-Datenleser kostete satte 35.000,- DM (ca. 18.000 Euro); das entsprach damals dem Gegenwert von ca. 3 Mittelklasse Autos.<br>
+			Dafür war die NCR446 der erste "kleine" kommerzielle Rechner, der Text und integrierte mathematische Berechnungen gleichzeitig ausdrucken konnte. Eine solche "Maschine" befähigte z.B. Statiker, textlich dokumentierte Berechnungen unmittelbar versandfertig aufzulisten. Der Konkurrent von Olivetti (Programma 101, 203) konnte dagegen nur Zahlen auf einem kleinen Streifen bzw. auf der Schreibmaschine ausgeben. 
+	     	
 		    <br>Es ist herrlich, dem Rechner z.B. bei der Berechnung der Wurzel einer Zahl zuzuschauen. Spätestens dann versteht man, was ein Algorithmus ist! Da sich das Programm auf dem Lochstreifen befindet, kann man jeden Rechenschritt und alle Programmsprünge anhand der Bewegung des Lochstreifens verfolgen.</br>
 		    <br>Wenn Sie einen kleinen Einblick in das Innenleben des Rechners während der Reparatur sehen möchten, klicken Sie bitte hier: <a class="go" href="/de/geraete/details-ncr446.php">Reparatureinblicke</a>
 		</p>
+		
+		<p>Auch <b>Zubehörteile</b> aus der Zeit der Lochstreifen als Daten- und Programmträger sind recht interessant.<br>
+		In der untenstehenden Abbildung ist links ein "Handlocher" zu sehen, der es erlaubte, 1-Zoll Lochstreifen nach selbstentworfenen Programmen oder Daten zu lochen. Wer den Rechner in einer abgespeckten Version hatte, verfügte nicht über den Lochstreifenstanzer (rechts im Bild oben)!<br>
+		Nach einer Erläuterung des Inhabers eines Ingenieurbüros für Baustatik lief die Prozedur dann etwa wie folgt ab:<br>
+		Zunächst erstellt man das Programm und den dazu gehörigen Text in Reinschrift. Dann musste jedes Zeichen per NCR-Codetabelle in einen 8-Bit-Code für den Lochstreifen übersetzt werden. Sodann durfte die Sekrekärin die vom "Chef" diktierten Ziffernfolge (1 bis 8) eintippen und danach jeweils den Hebel herunterdrücken. Per Handkraft wurden so die Löcher Sprosse für Sprosse hergestellt wobei jede Sprosse (Zeile) genau einem Zeichen entspricht. Das war eine mühsame und zeitraubende Arbeit, insbesondere wenn man sich aus Versehen "verlocht" hat. <br>
+		
+		<div class="box center auto-bildbreite">
+        <img src="/shared/photos/rechnertechnik/lochstreifenlocher.jpg"  width="682" height="352" />
+        <div class="bildtext"><b>Handlocher für Lochstreifen</b> (links) und <b> Tapewinder</b> (rechts)</div>
+		</div>
+	
+		<p>Mit unserer abgebildeten NCR-446 Maschine konnte man Programme und Daten direkt auf der Schreibmaschinentastatur eingeben die simultan im Lochstreifenstanzer ausgestanzt wurden. Dieser "Komfort" kostete jedoch zusätzlich ca. 20.000,- DM (10.000 Euro)!<br>
+		Wenn man den Programmlochstreifen wechseln musste, ergab sich das Problem, den oft sehr langen Streifen per Hand aufzuwickeln. Dazu baute HP einen kleinen handlichen "Tapewinder", der mit einem Akku betrieben wurde. So dauerte das Aufwickeln nur wenige Sekunden.</p>
+		
+		 
+		
+		
   <h3 id="nixdorf820">NIXDORF 820</h3> 
 
      <div class="box center auto-bildbreite">

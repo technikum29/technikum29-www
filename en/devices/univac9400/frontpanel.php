b273d52f655b2dadf1f6ea35b6b125f25a1dc40b
heribert <heribert@svn-users.technikum29.de>
2013-04-10 19:35:04 +0000

    glow-lambs durch neon-lambs ersetzt
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@367 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- en/devices/univac9400/frontpanel.php
+++ en/devices/univac9400/frontpanel.php
@@ -11,7 +11,7 @@
     <div class="bildtext">
         <h2><? print $titel; ?></h2>
         <p>Light ermitting diodes (LEDs) has not been invented yet. Little
-	glow lamps were used to display the binary states because
+	neon lamps were used to display the binary states because
 	filament bulbs would have been too slow to perform the fast
 	changings. The display features four rotary switches so it can
 	be switched between eight different state arrays with 18 bits.</p>

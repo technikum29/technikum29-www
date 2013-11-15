b4eeae0726779d4c158a6bcf1b12ac90475b3091
heribert <heribert@svn-users.technikum29.de>
2013-11-15 08:21:42 +0000

    Kleine Ergänzungen
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@473 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- en/devices/plated-wire-storage.php
+++ en/devices/plated-wire-storage.php
@@ -14,7 +14,8 @@
 
 <p>In the early 1970s, magnetical core memory was quite expensive and slow. That is, the cycle time (time needed to read and rewrite the information) was quite long, because all information had to be rewritten after they had been read. Semiconductor memory was not ready for series production for a long time yet, furthermore nobody even thought about non-volatile semiconductor memory at that time. So there was a need for a new consistent storage medium.
   <br/>As a solution, the plated wire was invented, just before the first semiconductor memories were ready for beeing mass-produced, supposed to be a replacement for the magnetical core memories. The plated wire consists of magnetic wire which replaces the cores (see picture at the right).</p>
-<p>The installations from the UNIVAC 9000 series (like our <a href="/en/devices/univac9400/univac_9300.shtm">UNIVAC 9300</a>) were equipped with plated wire storages. We could offer pictures of our storage units, but the assembly of this type of memory is quite opaque. Therefore we publish self-drawn diagrams to illustrate the plated wire characteristics.</p>
+<p>The installations from the UNIVAC 9000 series (like our <a href="/en/devices/univac9400/univac_9300.shtm">UNIVAC 9300</a>) were equipped with plated wire storages. We could offer pictures of our storage units, but the assembly of this type of memory is quite opaque. Therefore we publish self-drawn diagrams to illustrate the plated wire characteristics.
+</p>
 
 <p>From the physical point of view the plated wire storage is a thin-film storage medium. The information carrier is a 1 micrometer thick permalloy solenoid film (constisting of 81% nickel, 19% iron) that  sheats a beryllium copper wire with 0.13mm in diameter.<br/>Therefore you could save exactly 4 words of three bits each on the pictured detail.</p>
 
@@ -41,5 +42,6 @@ and falls back to the former position. This libration induces a small voltage. T
 <p style="clear:both;">
   In principle this was an ingenius idea: No need to thread the cores, fast cycle time, very cheap to produce...
   <br/>But there is a rub; the construction was very sensitive and error-prone. While the wires could be replaced in the former storage systems, typical plated wire storages were completely stuck together so they could not be repared later on. This was a disaster for the companies which made use of this storage.
-  <br/>Fortunately, the first semiconductor memories were mass-produced in the early 1970s, so most of the computers featuring plated wire storage could be upgraded to semiconductor memories made by INTEL.</p>
+  <br/>Fortunately, the first semiconductor memories were mass-produced in the early 1970s, so most of the computers featuring plated wire storage could be upgraded to semiconductor memories made by INTEL.<br>
+  Other hand our UNIVAC 9200 is still running with the original wire-memory, probably the last remaining functional magnetic wire store in the world!</p>
 

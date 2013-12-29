7a098e08c31d6b496ac88a290a130cd0ffc5199e
heribert <heribert@svn-users.technikum29.de>
2013-12-29 13:19:04 +0000

    AMPEX in 1130 erwähnt und Suche nach Manuals
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@486 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- en/computer/ibm1130.php
+++ en/computer/ibm1130.php
@@ -14,8 +14,9 @@
 		<p><b>IBM 1130</b></p>
 		</div>
 		<br><br>
-		<p>Since December 2013, we own a very nice and also quite interesting "Electronic Computing System" by IBM. This type was delivered for the first time in 1965.
-There is much to report about, but in first place the optical and electronical restoration of the Computer have to be realized. This section will be continued within a short time.</p><br>
+		<p>Since December 2013, we own a very nice and also quite interesting "Electronic Computing System" by IBM. This type was delivered for the first time in 1965 and intended for use by scientists, engineers, and mathematicians.<br>
+		The "AMPEX TMZ" (digital tape memory system) is connected to the equipment, so we are urgently looking for Manuals (schematics).<br>
+There is much to report about, but in first place the optical and electronical restoration of the Computer have to be realized.<br> This section will be continued within a short time.</p><br>
 
 <!-- <p><small>This system was transferred by FITG (Frankfurt) for what we are deeply grateful.</small></p>
 -->

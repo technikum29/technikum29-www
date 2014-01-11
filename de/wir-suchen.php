2a4a2f4f1e55d9b47c5f22150519bbfb83ceb3fa
heribert <heribert@svn-users.technikum29.de>
2014-01-11 20:28:29 +0000

    Bild wieder hochgeladen....
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@493 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/wir-suchen.php
+++ de/wir-suchen.php
@@ -9,7 +9,7 @@
    <h2>Was das Museum noch sucht</h2><br>
    
    <div class="box left clear-after">
-        <img src="/shared/photos/rechnertechnik/ibm-4628-7342.jpg" alt="Gesuchte Boards" width="448" height="231" class="nomargin-bottom" />
+        <img src="/shared/photos/rechnertechnik/ibm-4628-7342.jpg" alt="Gesuchte Boards" width="505" height="261" class="nomargin-bottom" />
         <p class="bildtext">
    Für unsere <b>IBM 1130</b> Anlage suchen wir dringend die Boards "4628" (16-Bit-Register) sowie "7342" (Adressentreiber) von IBM. Hilfreich wären auch die Schaltpläne dieser Boards. Bitte melden Sie sich, wenn Sie hier weiter helfen können!<br><br>
    Auch andere Boards mit SLT-Technik könnten wir gut gebrauchen.</p></div>

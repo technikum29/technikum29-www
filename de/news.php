271d4d4da9d9b4de99dc1d51a249b7605e0a9715
sven <sven@svn-users.technikum29.de>
2014-06-05 12:48:12 +0000

    Diskutiertes Design der Neuigkeiten-Seite umgesetzt.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@565 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/news.php
+++ de/news.php
@@ -36,66 +36,41 @@ MENU;
 	
 	<p>Damit alle die öfters unsere Homepage besuchen einen schnellen Einblick in Neuigkeiten und Erweiterungen haben, wurde diese Rubrik eingeführt. Das Neueste steht ganz oben.</p>
 
-<ul class="news-feed">
+<ul class="news-feed news-ng">
 
-<li><h3>Juni 2014</h3>
-<div class="box left clear-after">
+<li><a href="http://www.kunst-stoff.fr/tresorraum/wir-die-iborgs/">
+	<h3>Juni 2014</h3>
         <img src="/shared/photos/rechnertechnik/tuebingen.jpg" width="153" height="149" />
+	Unser Lochkartenstanzer IBM029 ist auf Reisen: In Tübingen steht er als Rhythmusgerät und zur
+        Einbindung in Kunstobjekte für das Event <em>Notizen zum Digitalen: Wir, die #iBorgs</em>
+	der Ausstellung im <em>Kunstamt Tübingen</em> als Leihgabe zur Verfügung.
+</a></li>
 
-Unser Lochkartenstanzer IBM029 ist auf Reisen: In Tübingen steht er als Rhythmusgerät und zur Einbindung in Kunstobjekte für den Event "Notizen zum Digitalen" von: ***Wir, die #iBorgs*** als Leihgabe zur Verfügung.<br>
-Hier der Link zur Ausstellung:
-<a href="http://www.kunst-stoff.fr/tresorraum/wir-die-iborgs/"> <b>Kunstamt Tübingen</b></a>
-
-<li><h3>Mai 2014</h3>
-<div class="box left clear-after">
+<li><a href="/de/rechnertechnik/ibm1130.php#1130">
+	<h3>Mai 2014</h3>
         <img src="/shared/photos/rechnertechnik/facit4000.jpg" width="352" height="137" />
-		
-		Peripherie der 1130: Facit-Lochstreifengeräte <a href="/de/rechnertechnik/ibm1130.php#1130"><br>
-		<b>siehe hier</b></a> 
-		</div>
+	Peripherie der 1130: Facit-Lochstreifengeräte.
+</a></li>
 
-<li><h3>Januar 2014</h3>
-<div class="box left clear-after">
+<li><a href="/de/rechnertechnik/ibm1130.php#1130">
+	<h3>Januar 2014</h3>
         <img src="/shared/photos/rechnertechnik/ibm-1130.jpg" alt="IBM 1130" width="225" height="194" />
-		
-		IBM 1130, eine neue Herausforderung: <a href="/de/rechnertechnik/ibm1130.php#1130"><br>
-		<b>lesen Sie hier weiter</b></a> 
-		</div>
+	IBM 1130, eine neue Herausforderung.
+</a></li>
 
 
 
-<li><h3>Dezember 2013</h3>
-
-<div class="box left clear-after">
+<li><a href="/de/rechnertechnik/lochkarten-edv.php#doppler">
+	<h3>Dezember 2013</h3>
         <img src="/shared/photos/rechnertechnik/ibm-514.jpg" alt="IBM 514" width="225" height="195" />
-		
-		Neuzugang: Ein schöner Lochkartendoppler aus der Frühzeit der EDV <a href="/de/rechnertechnik/lochkarten-edv.php#doppler"><br>
-		<b>lesen Sie hier</b></a> 
-		</div>
+	Neuzugang: Ein schöner Lochkartendoppler aus der Frühzeit der EDV.
+</a></li>
 	
 
-<li><h3>September 2013</h3>
-<div class="box left">
-		<img src="/shared/photos/rechnertechnik/leser617.jpg" alt="Lochkartenleser" width="241" height="149" /><br><br>
-		
-BULL GAMMA 55 läuft! Ein historischer Moment, <a href="/de/rechnertechnik/gamma55.php#ge55"><b>siehe hier</b></a>
-</div>
+<li><a href="/de/rechnertechnik/gamma55.php#ge55">
+	<h3>September 2013</h3>
+	<img src="/shared/photos/rechnertechnik/leser617.jpg" alt="Lochkartenleser" width="241" height="149" /><br><br>
+	BULL GAMMA 55 läuft! Ein historischer Moment.
+</a></li>
+</ul>
 
-</li><br>
-
-
-
-<div class="clear"></div>
-
-
-
-
-
-
-
-
-
-	
-	</ul>
-	
-	

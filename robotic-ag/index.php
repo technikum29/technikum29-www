6ea721c1e510ba0ba84e385000071e41963d74af
sven <sven@svn-users.technikum29.de>
2014-06-10 07:15:55 +0000

    Robotic-AG-Website der offiziellen Homepage hinzugefügt, weil externe Bearbeitung (SFTP) nicht besonders erfolgreich war.
    Die Robotic-Website ist zwar unabhängig von der technikum29-Website,
    bedient sich aber der gleichen Bearbeitungsfunktion über den
    Meta-Tag, vgl http://labs.technikum29.de/wiki/Website/Lokaler%20Editor
    
    
    
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@572 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ robotic-ag/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Informationen für die Robotic-AG der Max-von-Gagern-Schule Kelkheim</title>
+  <?php /* zwecks Homepagebarbeitungsprogramm, vgl. /lib/host.php: */ ?>
+  <meta name="t29.localfile" content="<?php print $_SERVER['SCRIPT_FILENAME']; ?>" id="localFileSource">
+  <meta name="t29.version" content="$Id$">
+  <link rel="stylesheet" type="text/css" href="http://www.technikum29.de/src/css/graydesign.css">
+  <link rel="stylesheet" type="text/css" href="/schule/styles.css">
+  <!-- MathJax = LaText im Browser -->
+  <script type="text/javascript"
+  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
+</head>
+<body>
+
+<h1>Informationen für die Robotic-AG der Max-von-Gagern-Schule Kelkheim  </h1><br><br>
+
+          
+
+<span style="color:red"><b>Aktualisiert: 6.6.2014</b></span><br>
+<br> <br><br>
+
+<center><img src="robo.jpg" width="847" height="674" alt="Roboter" /></center>
+
+
+
+<br><br>
+<big><b>Spielend Informatik lernen!</big></b><br><br>
+
+<br>Roboter bauen und programmieren macht Spaß, da man sofort sein Ergebnis sieht. In dieser AG lernst du mit grafischer Methode wie man Programme erstellt.<br>
+Du erfährst, wie der Roboter exakt deine Anweisungen ausführt. Du programmierst <b>Schleifen, Programmverzweigungen (Schalter), Unterprogramme (Blöcke)</b> und vieles mehr. Wenn alle Sensoren im Einsatz sind, kann sich der Roboter in einem fremden Umfeld bewegen ohne "hängen" zu bleiben. Er findet sich sogar in einem Labyrinth zurecht!<br>
+Rufe ihn und er kommt, wenn du ihm ein Lichtsignal sendest. Er wird dich finden, begrüßen und strahlen!
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+blockformel latex mit doppeldollar:
+$$  d = \frac{a \cdot \lambda}{ \sqrt{g^2 - \lambda^2 } } $$
+inline-formel:  \( E^2 = \sqrt{ p^2 + m^2 } \)
+-->
+
+</li><br>
+
+</ul><br>
+
+
+
+   
+   
+<!-- DEMO - diese icons gibt es alle:
+<ul class="dir">
+   <li class="dir"> Ordner
+   <li class="text"> text4
+   <li class="img"> bild
+   <li class="box"> box
+   <li class="script"> script
+   <li class="html"> html
+   <li class="tar"> tar
+   <li class="zip"> zip
+</ul>
+
+-->
+
+
+<!-- <div style="background-color: #efefef; border: 1px solid black; padding: 2em; margin:1em 0;">
+  <iframe src="habs-gesehen.php" style="width:100%; height: 30em; border: none"></iframe>
+</div>
+
+
+<div style="background-color: #efe; border: 1px solid green; padding: 2em; margin: 1em 0;">
+<img src="dokuwiki/lib/exe/fetch.php?media=wiki:dokuwiki-128.png" style="float: left; margin: -10px 1em 1em 0; width: 60px;">
+Siehe derzeit auch noch die <a href="dokuwiki/"><b>Wiki (Upload, Texte)</b></a>.
+</div>
+-->
+<!--
+<div style="background-color: #efefef; border: 1px solid black; padding: 2em; margin: 1em 0;">
+ Zum <b><a href="http://www.technikum29.de/schule/Physik-LK/upload">Datei-Upload</a></b>. Hier gehts zum
+ <b><a href="http://www.technikum29.de/schule/Physik-LK/upload/dateien">Download der hochgeladenen Dateien</a></b>.
+</div>   
+-->
+

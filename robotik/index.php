f8d9f0005861b023d6c2db22339cb70bd97a5321
heribert <heribert@svn-users.technikum29.de>
2014-07-11 09:06:51 +0000

    Neue Version Robotik-Homepage.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@605 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- robotik/index.php
+++ robotik/index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
+
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Informationen für die Robotic-AG der Max-von-Gagern-Schule Kelkheim</title>
+  <title>Informationen zum Ferienkurs  "ROBOTIK"</title>
   <?php /* zwecks Homepagebarbeitungsprogramm, vgl. /lib/host.php: */ ?>
   <meta name="t29.localfile" content="<?php print $_SERVER['SCRIPT_FILENAME']; ?>" id="localFileSource">
   <meta name="t29.version" content="$Id$">
@@ -15,55 +16,36 @@
 </head>
 <body>
 
-<h1>Informationen für die Robotic-AG der Max-von-Gagern-Schule Kelkheim  </h1><br><br>
+<h1>Informationen zum Ferienkurs  "Robotik" ............ technikum29</h1><br><br>
 
           
 
-<span style="color:red"><b>Aktualisiert: 7.7.2014</b></span>
+<span style="color:red"><b>Aktualisiert: 10.7.2014</b></span>
 <br> <br><br>
 
 <br>
-<h3><big><big><b>Spielend Informatik lernen!</b></big></big></h3>
+<h3><big><big><big><b>Spielend Informatik lernen!</b></big></big></big></h3>
 
-<br><p>Roboter bauen und programmieren macht Spaß da man sofort sein Ergebnis sieht. In dieser AG lernst du mit grafischer Methode wie man Programme erstellt.<br>
+<br><p>Roboter bauen und programmieren macht Spaß da man sofort sein Ergebnis sieht. In diesem Ferienkurs lernst du mit grafischer Methode wie man Programme erstellt.<br>
 Du erfährst, wie der Roboter exakt deine Anweisungen ausführt. Du programmierst <b>Schleifen, Programmverzweigungen (Schalter), Unterprogramme (Blöcke)</b> und vieles mehr. Wenn alle Sensoren im Einsatz sind, kann sich der Roboter in einem fremden Umfeld bewegen ohne "hängen" zu bleiben. Er findet sich sogar in einem Labyrinth zurecht!<br>
-Rufe ihn und er kommt, wenn du ihm ein Lichtsignal sendest. Er wird dich finden, begrüßen und strahlen!</p>
+Rufe ihn und er kommt, wenn du ihm ein Lichtsignal sendest. Er wird dich finden, begrüßen und strahlen!<br>
+Dies und viel viel mehr kann man programmieren.</p><br>
 
 
-<center><img src="robo.jpg" width="847" height="674" alt="Roboter" /></center>
+<center><img src="robotik.jpg" width="850" height="637" alt="Roboter" /></center>
 
 <br><br>
 
-<p><b>Hinweis für den 9.7.:</b><br>
-Wir werden ein Programm erstellen, mit welchem unser Roboter in der Lage ist, einer schwarzen Linie zu folgen. Er "schnüffelt" wie ein Hund und verfolgt die Spur. Bitte zu Beginn der Stunde die Roboter an die Ladestation anschließen!</p>
-<hr>
+<p>Der Ferienkurs läuft eine Woche lang (5 Tage) mit je 2 Zeitstunden. Er findet im "technikum29", Am Flachsland 29 in Kelkheim-Hornau statt.<br>
+Er richtet sich an technikinteressierte Schüler im Alter von 11 bis 13 (14) Jahren.<br>
+Es wird <b>die</b> Woche in den Ferien gewählt, in welcher mindestens 6 Schüler teilnehmen können. Eine Verlängerung ist eventuell möglich. Uhrzeit nach Absprache, vermutlich jeweils von 14 bis 16 Uhr.<br> 
+Gib bitte bei Interesse die Wochen an, in welchen du Zeit hast. Die Gruppengröße beträgt maximal 10 Personen. Je zwei bauen und programmieren einen Roboter nach dem System LEGO MINDSTORMS. Das Material ist vorhanden und wird während des Kurses zur Verfügung gestellt.<br>
+Voraussetzungen: Es sind keine Programmierkentnisse erforderlich. Du erfährst und lernst alles hier. Aber:<br>
+Du benötigst Geduld, auch mal Durchhaltevermögen, musst teamfähig sein und:<br>
+Du solltest gut logisch denken können!</p>
+<p>Zum Abschluss gibt es einen Experimental-Workshop im technikum29 mit tollen Experimenten!</p>
 
-
-<p><b>Tipps zum 2.7.2014:</b><br>
-Unser Roboter soll nun Hindernisse umfahren und sich so einen freien Weg suchen.<br>
-Weil unsere Zeit sehr knapp wird, hier ausnahmsweise der Lösungsvorschlag:<br>
-Wir starten mit einem Bewegungsblock (geradeaus fahren). Danach setzen wir einen "Warten Block Distanz" (Ultraschallsensor), der mit Port 4 verbunden wird. Den stellen wir so ein, dass so lange gewartet wird (und damit der Roboter weiter geradeaus fährt), bis der Abstand zu einem Hindernis <b>kleiner</b> als z.B. 30cm wird. Dann hat das Warten ein Ende und das Programm läuft weiter.<br>
-Danach kommt wieder ein Bewegungsblock. Diesmal eine rückwärts-Kurvenfahrt um dem Hindernis auszuweichen. Doch wie weit soll er rückwärts fahren?<br>
-Dazu setzen wir anschließend wieder einen "Warten Block Distanz" ein. Der bewirkt ein Warten, bis der Abstand vom Hindernis z.B. <b>größer</b> als 30cm ist. So lange fährt der Roboter also rückwärts. <br>
-Danach könnte unser Roboter wieder geradeaus fahren. Wir setzen die 4 Blöcke also in eine Endlos-Schleife.</p>
-
-<hr>
-<p><b>Tipps zum 25.6.2014:</b><br>
-Heute geht es um Präzision. Euer Roboter soll ganz genau ein vorgegebenes Rechteck umfahren.<br>
-Dazu sollte man erst die beiden Strecken mit dem Bewegungsblock testen und genau einstellen und dann die Kurvenfahrt. Dazu wird nur ein Motor aktiviert. Hier ist Geduld und eine schnelle Reaktion gefragt.<br>
-Die Gruppe, welche die Aufgabe am Besten löst erhält einen Preis (Tafel Schokolade!).</p>
-
-<hr>
-
-Hier kannst du das Blatt vom <b>11.6.</b> öffnen: <a href="Robotic-Sensoren kennenlernen.pdf"> Sensoren besser kennenlernen</a>
-
-
-<br><br><br>
-
-
-
-
-
+<p><b>Wir suchen noch gebrauchsfähige Laptops für diesen Ferienkurs!</b></p>
 
 
 

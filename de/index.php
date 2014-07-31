2088ce99ff116e2b96746424c467319f685a9461
heribert <heribert@svn-users.technikum29.de>
2014-07-31 19:30:41 +0000

    Termine aktualisiert
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@620 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/index.php
+++ de/index.php
@@ -137,7 +137,14 @@ Feriengruppe B<br>
 	Spezial-Führung
 
 </div>
-
+<div class="box termin clear-after">
+		<div class="left">
+			<p class="date"><em>2014</em> 16. Sept.</p>	</div>
+	<h4>Computer-History</h4>
+	Termin: Dienstag, der <b>16. September</b> um 18:00 Uhr <br>
+	Führung der Gruppe Dr. Bach
+	
+	</div>
 <!-- <div class="leftcol">
 	<h3>Führungen</h3> -->
 	

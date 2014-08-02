2cead1c8e7407a7a7413299d2a8f14cf72e45c1f
heribert <heribert@svn-users.technikum29.de>
2014-08-02 09:27:14 +0000

    kleine Änderungen in Robotik
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@623 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- robotik/index.php
+++ robotik/index.php
@@ -20,7 +20,7 @@
 
           
 
-<span style="color:red"><b>Aktualisiert: 27.7.2014</b></span>
+<span style="color:red"><b>Aktualisiert: 1.8.2014</b></span>
 <br> <br><br>
 
 <br>
@@ -36,12 +36,11 @@ Dies und viel viel mehr kann man programmieren.</p><br>
 
 <br><br>
 
-<p>Der Ferienkurs läuft eine Woche lang (5 Tage) mit je 2 Zeitstunden. Er findet im "technikum29", Am Flachsland 29 in Kelkheim-Hornau statt.<br>
-Er richtet sich an technikinteressierte Schüler im Alter von 11 bis 13 (14) Jahren.<br>
-Der Kurs findet vom <b>18.8.2014 bis 22.8.2014</b> statt.<br>
-Er wird möglicherweise vom 1.9. bis 5.9. wiederholt, wenn genügend Interessenten zusammen kommen.<br>
+<p>Der Ferienkurs findet vom <b>18.8.2014 bis 22.8.2014</b> mit je 2 Zeitstunden statt. Ort: "technikum29", Am Flachsland 29 in Kelkheim-Hornau.<br>
+Er richtet sich an technikinteressierte Schüler im Alter von 11 bis 13 Jahren.<br>
+Möglicherweise vom 1.9. bis 5.9. wiederholt, wenn genügend Interessenten zusammen kommen. Auf jeden Fall werden wir in den Herbstferien wieder einen Robotik-Kurs anbieten. Eine Vormerkung wird empfohlen.<br>
 
-Die Gruppengröße beträgt maximal 10 Personen. Je zwei bauen und programmieren einen Roboter nach dem System LEGO MINDSTORMS. Das Material ist vorhanden und wird während der Kurszeit zur Verfügung gestellt.<br>
+Die Gruppengröße beträgt maximal 10 Personen. Je zwei bauen und programmieren einen Roboter nach dem System LEGO MINDSTORMS. Das Material ist vorhanden und wird während der Kurszeit zur Verfügung gestellt.<br><br>
 Voraussetzungen: Es sind keine Programmierkenntnisse erforderlich. Du erfährst und lernst alles hier. Aber:<br>
 Du benötigst Geduld, auch mal Durchhaltevermögen, musst teamfähig sein und
 du solltest gut logisch denken können!</p>

e804ef1e7c865276552507f140e7581df2f6f77c
heribert <heribert@svn-users.technikum29.de>
2014-08-22 20:07:22 +0000

    Fehler berichtigt (Sept. -- Okt.)
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@635 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/index.php
+++ de/index.php
@@ -121,7 +121,7 @@
 	
 <div class="box termin clear-after">	
 	<div class="left">
-			<p class="date"><em>2014</em>Ende Sept.</p>	</div>
+			<p class="date"><em>2014</em>Ende Okt.</p>	</div>
 	<h4>Robotik-Ferienkurs für Kids von 11-13 Jahren.
 	<br></h4> Hochinteressanter Kurs in welchem Roboter gebaut werden, die sich programmieren lassen und damit die verrücktesten Dinge tun. Weitere Informationen unter <a class="go" href="http://www.technikum29.de/robotik"> Robotik-Ferienkurs</a><br>
 	Termin: <b>Herbstferien</b>

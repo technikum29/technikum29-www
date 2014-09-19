2bc5c743dd397ad8ad25b1662394c2b59f2d5b88
sven <sven@svn-users.technikum29.de>
2014-09-19 11:56:28 +0000

    IBM 1130-"Blog": Vergessene Extraseiten hinzugefügt
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@664 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ de/geraete/ibm1130-blog.php
@@ -0,0 +1,11 @@
+<?php
+	$seiten_id = 'ibm1130-blog';
+	$version = '$Id';
+	$titel = 'IBM 1130-Blog';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h2>IBM 1130-Blog</h2>
+
+<p>Hier viel Text</p>
---
+++ en/devices/ibm1130-blog.php
@@ -0,0 +1,11 @@
+<?php
+	$seiten_id = 'ibm1130-blog';
+	$version = '$Id';
+	$titel = 'IBM 1130 Reparation Blog';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h2>IBM 1130 Reparation Blog</h2>
+
+<p>Here to be dragons</p>

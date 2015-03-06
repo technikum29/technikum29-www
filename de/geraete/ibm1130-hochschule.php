22689c4af246727c7bbe4174a78b650218d614e4
heribert <heribert@svn-users.technikum29.de>
2015-03-06 22:09:16 +0000

    git-svn-id: https://www.technikum29.de/svn/technikum29-www@737 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ de/geraete/ibm1130-hochschule.php
@@ -0,0 +1,34 @@
+<?php
+	$seiten_id = 'hochschule';
+	$version = '$Id';
+	$titel = 'IBM-1130 noch an der Hochschule';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h2><?php print $title; ?></h2>
+
+<div class="box center">
+	<div style="white-space:nowrap">
+        <img src="/shared/photos/rechnertechnik/1130-darmstadt-1.jpg" width="860" height="418" alt="IBM-1130" />
+       </div>
+
+    <p>So stand der Rechner bereits abgebaut an der Hochschule Darmstadt. Glücklicherweise sorgte der Professor der die Anlage zuletzt nutzte dafür, dass sie nicht entsorgt wurde.<br>
+	Ganz links ist ein Nachfolger der Anlage zu sehen: Jeder Student hatte nun einen eignen PC am "Arbeitsplatz".	</p>
+	
+	
+	
+	<div class="box center">
+        <img src="/shared/photos/rechnertechnik/1130-darmstadt-2.jpg" width="860" height="587" alt="IBM-1130 in Betrieb" />
+       </div>
+	
+	<p>Hier war die Anlage noch im Betrieb. Der Drucker und die Lochkarteneinheit standen weiter links. Wie in der Zeit der Lochkarten üblich sind auch hier einige Kartons davon zu erkennen. Der "Operator" gönnte sich einen gepolsterten Sessel. Rechts stehen einige Peripheriegeräte, die zum größten Teil noch vorhanden sind. </p>
+	
+	<div class="box center">
+        <img src="/shared/photos/rechnertechnik/1130-darmstadt-3.jpg" width="860" height="587" alt="IBM-1130 in Betrieb" />
+       </div>
+	
+	<p>5 Arbeitsplätze für die Studenten an den Lochkartenstanzern. An jedem Platz stehen frisch aufgefüllte Vorräte an verschiedenfarbigen Lochkarten. Ein einsamer Student im roten T-Shirt ist gerade dabei, sein Programm abzulochen. Manchmal war der Andrang so groß, dass es Wartezeiten gab.<br>
+	Im Allgemeinen wurden die gelochten Karten im Laufe des Tages abgegeben und man konnte am nächsten Morgen den Stapel mitsamt den Ausdrucken abholen. Oft war da nicht viel zu sehen: Das Programm hatte Fehler und lief nicht. Wenn der Operator dem Studenten wohlgesonnen war, legte er einen Zettel an die Stelle des Lochkartenstapels, an der das Programm "klemmte". Das erleichterte die Fehlersuche ungemein.</p>
+	
+</div>

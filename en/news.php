26525d52eaf8106b4c9bbb3fe1d3c86cbcfc430b
heribert <heribert@svn-users.technikum29.de>
2015-04-22 07:46:55 +0000

    News (en) mal auf den aktuellen Stand gebracht
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@770 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- en/news.php
+++ en/news.php
@@ -20,7 +20,19 @@
 				<h3>March 2015</h3>
 				<img src='/shared/photos/rechnertechnik/ibm-1130.jpg' alt='IBM 1130'>	
 The mainframe IBM 1130 is now working perfect. This page has been greatly expanded.			</a></li>
-	   
+
+ <li><a href="/en/computer/storage-media.php#core-memory">
+	<h3 lang="de">December 2014</h3>
+    <img src="/shared/photos/rechnertechnik/speichermedien/demo-kernspeicher.jpg" width="153" height="115" />
+	Huge core memory model of the university
+</a></li>
+
+	   <li><a href="http://www.techrepublic.com/pictures/bringing-the-first-computers-back-to-life-contenders-for-a-computer-conservation-award/8/" target="_blank">  
+<h3>November 2014</h3>
+<img src="http://pixelprosemedia.com/wp-content/uploads/2013/02/techrepublic.png" width="150">
+The technikum29 has applied to the <i> Tony Sales Award </i> and is
+described in the online-journal <i> techrepublic </i> "The world's greatest restoration projects":  
+</a></li>
 	   
 	<li><a href="/en/communication/fax.php#decoder">
 				<h3>July 2014</h3>
@@ -32,9 +44,7 @@ The mainframe IBM 1130 is now working perfect. This page has been greatly expand
 				<h3>May 2014</h3>
 				<img src='/shared/photos/rechnertechnik/facit4000.jpg' alt='Facit papertape device'>				Periphery of the 1130: Facit tape devices			</a></li>
 			
-			<li><a href="/en/computer/punchcard.php#reproducing">
-				<h3>December 2013</h3>
-				<img src='/shared/photos/rechnertechnik/ibm-514.jpg' alt='Reproducing punch'>				Recruit: A large IBM reproducing punch from the early days of computing			</a></li>
+			
 			
 			</ul>
 </ul>

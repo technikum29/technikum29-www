fb632eb398edf2762625ae5918fb33d71de47a86
sven <sven@svn-users.technikum29.de>
2015-06-27 21:51:42 +0000

    Robotik: Rechtschreibfehler und Kommatafehler im Text korrigiert. Text auf "normalisierte" Form gebracht, in dem die unlogischsten Satzkonstellationen umgestellt wurden und seltsame Umbrüche und Linien zu sinnvolleren Listenelementen umgeformt wurden.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@802 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- robotik/index.php
+++ robotik/index.php
@@ -5,14 +5,10 @@
 
 $sidebar_content = <<<SIDEBAR_ENDE
 
-<!--
-<p style="margin-bottom: 200px"></p> -->
 <a href="http://www.fnp.de/lokales/main-taunus-kreis/Lego-Roboter-werden-zu-Rechenkuenstlern;art676,1000098" target="blank">
 <center><img src="hk.jpg" width="210" height="135" alt="Frankfurter Neue Presse" /></center>
 "Lego-Roboter werden zu Rechenkünstlern", Bericht in der "Frankfurter Neuen Presse" </a>
 <br><br><!-- wieder entfernen! -->
-<!--
-<p style="margin-bottom: 725px"></p> -->
 
 <font size="5">
 Anmeldung und Vormerkung unter:<br><br>
@@ -36,11 +32,6 @@ SIDEBAR_ENDE;
 	
 	require "../lib/technikum29.php";
 ?>
-<!--
- Hinweis: Diese Seite wurde am 24.Aug 13:22 testweise auf Basis von Revision 636
- von der Hauptrobotikseite "kopiert".
--->
-
 
 
 <br><br>
@@ -60,45 +51,55 @@ Roboter bauen und programmieren</div>
 <p><small>Bestückt mit 3 Motoren und 5 Sensoren tasten sie sich durch die Welt</small></p>
 </div>
 
-<p>Fun for Future: Roboter werden in Zukunft eine immer größere Bedeutung haben. Diese zu bauen und programmieren macht Spaß da man hier richtig kreativ "spielen" kann. In dem <b>Ferienworkshop</b> lernst du mit einer grafischen Methode wie man Programme erstellt.<br>
+<p>Fun for Future: Roboter werden in Zukunft eine immer größere Bedeutung haben. Diese zu bauen und programmieren macht Spaß, da man hier richtig kreativ "spielen" kann. In dem <b>Ferienworkshop</b> lernst du mit einer grafischen Methode, wie man Programme erstellt.<br>
 Du erfährst, wie der Roboter exakt deine Anweisungen ausführt. Wenn alle Sensoren im Einsatz sind, kann sich der Roboter mühelos in einem fremden Umfeld bewegen. Er findet sich sogar in einem Labyrinth zurecht!<br>
 Als Mars-Rover sucht er selbstständig nach Objekten und nähert sich ihnen vorsichtig an. So scannt er eine unbekannte Fläche ab. Er kann auch tanzen, die Länge von Strecken vermessen oder auf "Straßen" navigieren.<br>
-Es gibt per Programmierung unendlich viele Möglichkeiten den Roboter "intelligent" zu machen.</p>
+Es gibt per Programmierung unendlich viele Möglichkeiten, den Roboter "intelligent" zu machen.</p>
+
+<p class="clear">
+Der Workshop richtet sich an <b>technikinteressierte Schüler(innen)</b> im Alter von <b>11 bis 13 (14) Jahren</b>, sowohl intelligente Einsteiger als auch Fortgeschrittene.
+
+<p>Ort: "technikum29", Am Flachsland 29 in 65779 Kelkheim-Hornau.
+
+<p>Terminvorschläge für die Sommerferien (jeweils 10 bis 12 Uhr, alternativ 14 bis 16 Uhr):
+
+<ul>
+ <li>Kurs für Einsteiger: Montag 3.8. bis Freitag 7.8. [Bisher angemeldet: Simon, Daniel, Rebekka, Tim, Felix, ...]
+ <li>Kurs für Fortgeschrittene: Montag 24.8. bis Freitag 28.8.  [Bisher angemeldet: David, Tim, ...]
+</ul>
+ 
+<p>Es ist sinnvoll, sich für die Kurse in den Sommerferien vormerken zu lassen! Wer zu den aufgeführten Terminen nicht kommen kann, hat die Möglichkeit selbst Terminvorschläge zu machen. Wir sind flexibel, sammeln die Wünsche und werden gegebenenfalls einen zweiten Einführungskurs anbieten.
 
 <p>
-Ort: "technikum29", Am Flachsland 29 in 65779 Kelkheim-Hornau. Er richtet sich an <b>technikinteressierte Schüler(innen)</b> im Alter von<b> 11 bis 13 (14) Jahren</b>, also intelligente "Robotik-Einsteiger" sowie Fortgeschrittene.<br><br>
+Voraussetzungen für den Einsteigerkurs: Es sind keine Programmierkenntnisse erforderlich. Du erfährst und lernst alles hier.
+Allerdings benötigst du Geduld, auch mal Durchhaltevermögen, musst teamfähig sein und solltest <b>Spaß am logischen Denken</b> haben! Im zweiten Kurs sind die grundlegenden Kenntnisse der EV3-Programmierung erforderlich. Er baut auf unsere Einführungskurse auf und eignet sich auch für 14-jährige Schüler.
 
-Terminvorschläge für die Sommerferien (jeweils 10 bis 12 Uhr, alternativ 14 bis 16 Uhr):<hr>
- Kurs für Einsteiger: Montag 3.8. bis Freitag 7.8. [Bisher angemeldet: Simon, Daniel, Rebekka, Tim, Felix,....]<hr>
- Kurs für Fortgeschrittene: Montag 24.8. bis Freitag 28.8.  [Bisher angemeldet: David, Tim, ....] <hr>
- 
- Es ist sinnvoll, sich für die Kurse in den Sommerferien vormerken zu lassen! Wer zu den aufgeführten Terminen nicht kommen kann hat die Möglichkeit selbst Terminvorschläge zu machen. Wir sind flexibel, sammeln die Wünsche und werden gegebenenfalls einen zweiten Einführungskurs anbieten.<br><br>
-
-
-Voraussetzungen für den Einsteigerkurs: Es sind keine Programmierkenntnisse erforderlich. Du erfährst und lernst alles hier. Aber:<br>
-Du benötigst Geduld, auch mal Durchhaltevermögen, musst teamfähig sein und solltest <b>Spaß am logischen Denken</b> haben! Im zweiten Kurs sind die grundlegenden Kenntnisse der EV3-Programmierung erforderlich. Er baut auf unsere Einführungskurse auf und eignet sich auch für 14-jährige Schüler.</p><br>
 
 <div class="box desc-left borderless">
 	<img src="ferienkurs.jpg" width="531" height="313"/>
-	<p class="bildtext"><b>Ein Teil der Schüler beim Entwickeln eines neuen Programms.</b>  Siehe: <a href="Kelkheimer-Zeitung.pdf"> Bericht in der Kelkheimer Zeitung</a><br>
-...oder auch <a href="http://www.fnp.de/lokales/main-taunus-kreis/Junge-Roboter-fahren-zwischen-alten-Computern-Slalom;art676,1352844" target="blank">"Junge Roboter fahren zwischen alten Computern Slalom"</a>, Bericht im Höchster Kreisblatt vom 14.4.2015</p>
+	<p class="bildtext"><b>Einige Schüler beim Entwickeln eines neuen Programms.</b>
+	<br>Siehe: <a href="Kelkheimer-Zeitung.pdf"> Bericht in der Kelkheimer Zeitung</a>, sowie
+ "<a href="http://www.fnp.de/lokales/main-taunus-kreis/Junge-Roboter-fahren-zwischen-alten-Computern-Slalom;art676,1352844" target="blank">Junge Roboter fahren zwischen alten Computern Slalom</a>", Bericht im Höchster Kreisblatt vom 14.4.2015</p>
 </div>
 	
-<p style="font-size: small; text-align: left; line-height: 110%;">
-Aus dem Inhalt:<br><br>
-Aufbau der Roboter, Erklärung der Sensoren.
-Selbstständiges lineares Programmieren nach Vorgaben. Einführung von Schleifen. Eigenes Planen und Testen von Programmen.<br>
-Programmverzweigungen, Unterprogramme und parallel laufende Programme (Multitasking), spezielle Programmiertechniken. Interrupts.<br> 
-Linie fahren. Programmierte Navigation, Mars-Rover sucht Objekte, Farben erkennen, Sprachaufzeichnung, Linien zählen, Roboter zeichnet Figuren (Robograph), Datenleitungen, Variable speichern und verwenden, Mathe-Block. Zählen, vergleichen, logische Entscheidungen treffen usw.<br>
-Das Grundprinzip lautet: Es soll vor allem Spaß machen! Das Niveau wird dem Alter der Schüler angepasst.
-</p>
+<div style="font-size: small; text-align: left; line-height: 110%;">
+Aus dem Inhalt:
+<ul>
+<li>Aufbau der Roboter, Erklärung der Sensoren.
+<li>Selbstständiges lineares Programmieren nach Vorgaben. Einführung von Schleifen. Eigenes Planen und Testen von Programmen.
+<li>Programmverzweigungen, Unterprogramme und parallel laufende Programme (Multitasking), spezielle Programmiertechniken. Interrupts.
+<li>Linie fahren. Programmierte Navigation, Mars-Rover sucht Objekte, Farben erkennen, Sprachaufzeichnung, Linien zählen, Roboter zeichnet Figuren (Robograph), Datenleitungen, Variablen speichern und verwenden, Mathe-Block. Zählen, Vergleichen, logische Entscheidungen Treffen usw.
+</ul>
+<p>Das Grundprinzip lautet: Es soll vor allem Spaß machen! Das Niveau wird dem Alter der Schüler angepasst.
+</div>
 
+<p class="clear">
 Pro Kurs können 10 Personen teilnehmen. Je zwei Teilnehmer bauen und programmieren einen Roboter mit dem System LEGO MINDSTORMS Education EV3. Das Material wird während der Kurszeit zur Verfügung gestellt.
 Die Kursgebühr für den Ferienkurs beträgt 40 Euro pro Teilnehmer.
 
 <div class="box bordered nomargin-bottom">
 	<p class="bildtext"><b>Robotik in der Schule:</b> <br>
-	Das technikum29 sponsert je einen Robotik-Workshop an folgenden Schulen: Albert-Einstein-Schule, Schwalbach sowie Eichendorffschule, Kelkheim. Hier werden für die Klassen 6/7 zwei Stunden pro Woche angeboten und von den Schülern mit Begeisterung angenommen! Die nächsten Workshop´s beginnen am 24.9.2015 (EDS) bzw. 28.9.2015 (AES). Anmeldung an der jeweiligen Schule.</p>
+	Das technikum29 sponsert je einen Robotik-Workshop an folgenden Schulen: Albert-Einstein-Schule, Schwalbach sowie Eichendorffschule, Kelkheim. Hier werden für die Klassen 6/7 zwei Stunden pro Woche angeboten und von den Schülern mit Begeisterung angenommen! Die nächsten Workshops beginnen am 24.9.2015 (EDS) bzw. 28.9.2015 (AES). Anmeldung an der jeweiligen Schule.</p>
 </div>
 
 

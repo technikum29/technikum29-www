62801668723e4070944625899ab876df06af9354
heribert <heribert@svn-users.technikum29.de>
2015-06-28 19:40:27 +0000

    git-svn-id: https://www.technikum29.de/svn/technikum29-www@804 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- robotik/index.php
+++ robotik/index.php
@@ -1,31 +1,56 @@
 <?php
 	$seiten_id = 'robotik-ferienkurs';
 	$version = '$Id: robotik$';
-	$titel = 'Informationen zum Ferienkurs "Robotik"';
+	$titel = 'Ferienkurs "Robotik" mit Lego-Robotern im technikum29 und an Schulen in Kelkheim und Schwalbach';
 
 $sidebar_content = <<<SIDEBAR_ENDE
 
-<a href="http://www.fnp.de/lokales/main-taunus-kreis/Lego-Roboter-werden-zu-Rechenkuenstlern;art676,1000098" target="blank">
-<center><img src="hk.jpg" width="210" height="135" alt="Frankfurter Neue Presse" /></center>
-"Lego-Roboter werden zu Rechenkünstlern", Bericht in der "Frankfurter Neuen Presse" </a>
-<br><br><!-- wieder entfernen! -->
-
-<font size="5">
-Anmeldung und Vormerkung unter:<br><br>
-<a class="email" href="mailto:post@technikum29.de">post@technikum29.de</a>
-</font><br>
-<font size="4">
-Das technikum29 ist <br> 5 Minuten vom Bahnhof Kelkheim-Hornau entfernt, also mit Bahn und Bus gut erreichbar.
-<br><br><br>
-
-<div class="box desc-left borderless">
-<a class="popup" href="2-roboter.jpg">
-<img src="2-roboter.jpg" width="210" height="146" />
-</a>
+<div class="text-block alertbox">
+	Seite zuletzt aktualisiert:
+	<br>19.06.2015
 </div>
-	
 
-</font>
+<div class="spacing"></div>
+
+<div class="bild-block">
+	<a href="https://goo.gl/maps/Rq4Ep" title="5min Fußweg vom Bahnhof Kelkheim-Hornau""><img src="wegskizze-bahnhof.png" style="width:100%"></a>
+	<p class="bildtext">Das technikum29 ist 5 Minuten vom <a href="http://www.rmv.de/auskunft/bin/jp/stboard.exe/dn?input=3004295&time=00:56&maxJourneys=10&dateBegin=28.06.15&dateEnd=12.12.15&selectDate=&productsFilter=1111111111111111&start=yes&dirInput=&view=STATIONINFO">Bahnhof Kelkheim-Hornau</a> entfernt und auch mit Bus 263 gut erreichbar.</p>
+</div>
+
+
+<div class="bild-block">
+	<a href="roboter.jpg" class="popup"><img src="roboter.jpg" style="max-width:100%" alt="Roboter" /></a>
+	<p class="bildtext">Bestückt mit 3 Motoren und 5 Sensoren: <a href="roboter.jpg" class="popup">LEGO Mindstorms-Roboter</a></p>
+</div>
+
+
+<div class="bild-block">
+	<a href="http://www.fnp.de/lokales/main-taunus-kreis/Lego-Roboter-werden-zu-Rechenkuenstlern;art676,1000098" target="blank">
+		<img src="hk.jpg" width="210" height="135" alt="Frankfurter Neue Presse" />
+	</a>
+	<p class="bildtext">Einige Schüler beim Entwickeln eines neuen Programms</p>
+</div>
+
+<div class="text-block">
+	<b>Zeitungsecho</b>
+	<ul>
+	<li>Kelkheimer Zeitung vom 28.07.2014: <a href="Kelkheimer-Zeitung.pdf">Roboter in den Ferien programmieren</a>
+	<li>Höchster Kreisblatt vom 14.04.2015: <a href="http://www.fnp.de/lokales/main-taunus-kreis/Junge-Roboter-fahren-zwischen-alten-Computern-Slalom;art676,1352844" target="blank">Junge Roboter fahren zwischen alten Computern Slalom</a>
+	<li>Frankfurter Neuen Presse vom 25.08.2014: <a href="http://www.fnp.de/lokales/main-taunus-kreis/Lego-Roboter-werden-zu-Rechenkuenstlern;art676,1000098" target="blank">Lego-Roboter werden zu Rechenkünstlern</a>
+	</ul>
+</div>
+
+
+<div class="bild-block">
+	<a class="popup" href="2-roboter.jpg">
+		<img src="2-roboter.jpg" width="210" height="146" />
+	</a>
+</div>
+
+<div class="text">
+<strong>Robotik in der Schule:</strong>
+<p>Das technikum29 sponsert je einen Robotik-Workshop an folgenden Schulen: Albert-Einstein-Schule, Schwalbach sowie Eichendorffschule, Kelkheim. Hier werden für die Klassen 6/7 zwei Stunden pro Woche angeboten und von den Schülern mit Begeisterung angenommen! Die nächsten Workshops beginnen am 24.9.2015 (EDS) bzw. 28.9.2015 (AES). Anmeldung an der jeweiligen Schule.</p>
+</div>
 
 
 SIDEBAR_ENDE;
@@ -33,57 +58,36 @@ SIDEBAR_ENDE;
 	require "../lib/technikum29.php";
 ?>
 
+<header class="teaser">
+	<h2>
+	<span class="subtext">Spaßig intelligente Altenrative für die Ferien:</span>
+	Roboter bauen und programmieren
+	</h2>
+	<img class="no-copyright" src="header-bild.jpg">
+</header>
 
-<br><br>
-<div style="font-weight;text-align:center">
-<div style="font-size: 250%; line-height: 110%">Spaßig intelligente Alternative für die Ferien:<br>
-Roboter bauen und programmieren</div>
-<br>
-<span style="color:red">Aktualisiert: 19.6.2015</span>
-</div>          
-<br>
 
-<br>
+<div class="cols">
 
-<div class="box desc-left borderless">
-<img src="roboter.jpg" width="450" height="360" alt="Roboter" />
+<div class="leftcol">
 
-<p><small>Bestückt mit 3 Motoren und 5 Sensoren tasten sie sich durch die Welt</small></p>
-</div>
+<p>Fun for Future: Roboter werden in Zukunft eine immer größere Bedeutung haben. Diese zu bauen und programmieren macht Spaß, da man hier richtig kreativ "spielen" kann. In dem <b>Ferienworkshop</b> lernst du mit einer grafischen Methode, wie man Programme erstellt.
+<br>Du erfährst, wie der Roboter exakt deine Anweisungen ausführt. Wenn alle Sensoren im Einsatz sind, kann sich der Roboter mühelos in einem fremden Umfeld bewegen. Er findet sich sogar in einem Labyrinth zurecht!<br>
 
-<p>Fun for Future: Roboter werden in Zukunft eine immer größere Bedeutung haben. Diese zu bauen und programmieren macht Spaß, da man hier richtig kreativ "spielen" kann. In dem <b>Ferienworkshop</b> lernst du mit einer grafischen Methode, wie man Programme erstellt.<br>
-Du erfährst, wie der Roboter exakt deine Anweisungen ausführt. Wenn alle Sensoren im Einsatz sind, kann sich der Roboter mühelos in einem fremden Umfeld bewegen. Er findet sich sogar in einem Labyrinth zurecht!<br>
+<p>
+<a href="ferienkurs.jpg" class="popup"><img src="ferienkurs.jpg" style="max-width:100%" /></a>
+
+<p>
 Als Mars-Rover sucht er selbstständig nach Objekten und nähert sich ihnen vorsichtig an. So scannt er eine unbekannte Fläche ab. Er kann auch tanzen, die Länge von Strecken vermessen oder auf "Straßen" navigieren.<br>
 Es gibt per Programmierung unendlich viele Möglichkeiten, den Roboter "intelligent" zu machen.</p>
 
-<p class="clear">
-Der Workshop richtet sich an <b>technikinteressierte Schüler(innen)</b> im Alter von <b>11 bis 13 (14) Jahren</b>, sowohl intelligente Einsteiger als auch Fortgeschrittene.
+</div><!--/leftcol -->
+<div class="rightcol">
 
-<p>Ort: "technikum29", Am Flachsland 29 in 65779 Kelkheim-Hornau.
+<p>Der Workshop richtet sich an <b>technikinteressierte Schüler(innen)</b> im Alter von <b>11 bis 13 (14) Jahren</b>, sowohl intelligente Einsteiger als auch Fortgeschrittene. Er findet im <a href="/" class="go">technikum29</a> in Kelkheim-Hornau statt.
+Es gibt Einsteiger- und Fortgeschrittenenkurse.
 
-<p>Terminvorschläge für die Sommerferien (jeweils 10 bis 12 Uhr, alternativ 14 bis 16 Uhr):
-
-<ul>
- <li>Kurs für Einsteiger: Montag 3.8. bis Freitag 7.8. [Bisher angemeldet: Simon, Daniel, Rebekka, Tim, Felix, ...]
- <li>Kurs für Fortgeschrittene: Montag 24.8. bis Freitag 28.8.  [Bisher angemeldet: David, Tim, ...]
-</ul>
- 
-<p>Es ist sinnvoll, sich für die Kurse in den Sommerferien vormerken zu lassen! Wer zu den aufgeführten Terminen nicht kommen kann, hat die Möglichkeit selbst Terminvorschläge zu machen. Wir sind flexibel, sammeln die Wünsche und werden gegebenenfalls einen zweiten Einführungskurs anbieten.
-
-<p>
-Voraussetzungen für den Einsteigerkurs: Es sind keine Programmierkenntnisse erforderlich. Du erfährst und lernst alles hier.
-Allerdings benötigst du Geduld, auch mal Durchhaltevermögen, musst teamfähig sein und solltest <b>Spaß am logischen Denken</b> haben! Im zweiten Kurs sind die grundlegenden Kenntnisse der EV3-Programmierung erforderlich. Er baut auf unsere Einführungskurse auf und eignet sich auch für 14-jährige Schüler.
-
-
-<div class="box desc-left borderless">
-	<img src="ferienkurs.jpg" width="531" height="313"/>
-	<p class="bildtext"><b>Einige Schüler beim Entwickeln eines neuen Programms.</b>
-	<br>Siehe: <a href="Kelkheimer-Zeitung.pdf"> Bericht in der Kelkheimer Zeitung</a>, sowie
- "<a href="http://www.fnp.de/lokales/main-taunus-kreis/Junge-Roboter-fahren-zwischen-alten-Computern-Slalom;art676,1352844" target="blank">Junge Roboter fahren zwischen alten Computern Slalom</a>", Bericht im Höchster Kreisblatt vom 14.4.2015</p>
-</div>
-	
-<div style="font-size: small; text-align: left; line-height: 110%;">
-Aus dem Inhalt:
+<h3>Inhalt</h3>
 <ul>
 <li>Aufbau der Roboter, Erklärung der Sensoren.
 <li>Selbstständiges lineares Programmieren nach Vorgaben. Einführung von Schleifen. Eigenes Planen und Testen von Programmen.
@@ -91,61 +95,43 @@ Aus dem Inhalt:
 <li>Linie fahren. Programmierte Navigation, Mars-Rover sucht Objekte, Farben erkennen, Sprachaufzeichnung, Linien zählen, Roboter zeichnet Figuren (Robograph), Datenleitungen, Variablen speichern und verwenden, Mathe-Block. Zählen, Vergleichen, logische Entscheidungen Treffen usw.
 </ul>
 <p>Das Grundprinzip lautet: Es soll vor allem Spaß machen! Das Niveau wird dem Alter der Schüler angepasst.
-</div>
 
-<p class="clear">
-Pro Kurs können 10 Personen teilnehmen. Je zwei Teilnehmer bauen und programmieren einen Roboter mit dem System LEGO MINDSTORMS Education EV3. Das Material wird während der Kurszeit zur Verfügung gestellt.
+</div><!--/rightcol-->
+</div><!--/cols -->
+
+<div class="clear cols">
+
+<div class="leftcol">
+<h3>Vorraussetzungen</h3>
+<p>Im Einsteigerkurs sind keine Programmierkenntnisse erforderlich. Du erfährst und lernst alles hier. Du benötigst allerdings Geduld, auch mal Durchhaltevermögen, musst teamfähig sein und solltest <b>Spaß am logischen Denken</b> haben! Im zweiten Kurs sind die grundlegenden Kenntnisse der EV3-Programmierung erforderlich. Er baut auf unsere Einführungskurse auf und eignet sich auch für 14-jährige Schüler.
+
+<h3>Bedingungen und Kosten</h3>
+<p>Pro Kurs können 10 Personen teilnehmen. Je zwei Teilnehmer bauen und programmieren einen Roboter mit dem System LEGO MINDSTORMS Education EV3. Das Material wird während der Kurszeit zur Verfügung gestellt.
 Die Kursgebühr für den Ferienkurs beträgt 40 Euro pro Teilnehmer.
 
-<div class="box bordered nomargin-bottom">
-	<p class="bildtext"><b>Robotik in der Schule:</b> <br>
-	Das technikum29 sponsert je einen Robotik-Workshop an folgenden Schulen: Albert-Einstein-Schule, Schwalbach sowie Eichendorffschule, Kelkheim. Hier werden für die Klassen 6/7 zwei Stunden pro Woche angeboten und von den Schülern mit Begeisterung angenommen! Die nächsten Workshops beginnen am 24.9.2015 (EDS) bzw. 28.9.2015 (AES). Anmeldung an der jeweiligen Schule.</p>
-</div>
 
+</div><!--/leftcol -->
+<div class="rightcol">
+<h3>Termine für die Sommerferien</h3>
+<p>Jeweils 10 bis 12 Uhr, alternativ 14 bis 16 Uhr:
 
-
-
-
-
-
-<!--
-blockformel latex mit doppeldollar:
-$$  d = \frac{a \cdot \lambda}{ \sqrt{g^2 - \lambda^2 } } $$
-inline-formel:  \( E^2 = \sqrt{ p^2 + m^2 } \)
--->
-
-
-   
-   
-<!-- DEMO - diese icons gibt es alle:
-<ul class="dir">
-   <li class="dir"> Ordner
-   <li class="text"> text4
-   <li class="img"> bild
-   <li class="box"> box
-   <li class="script"> script
-   <li class="html"> html
-   <li class="tar"> tar
-   <li class="zip"> zip
+<ul>
+ <li><b>Kurs für Einsteiger</b>
+	<br>Montag 3.8. bis Freitag 7.8.
+	<br><em>Bisher angemeldet:</em> Simon, Daniel, Rebekka, Tim, Felix....
+	<br>
+ <li><b>Kurs für Fortgeschrittene</b>
+	<br>Montag 24.8. bis Freitag 28.8.
+	<br><em>Bisher angemeldet:</em> David, Tim.....
+	<br>
 </ul>
 
--->
 
+<p>Wer zu den aufgeführten Terminen nicht kommen kann, hat die Möglichkeit selbst Terminvorschläge zu machen. Wir sind flexibel, sammeln die Wünsche und werden eventuell einen zweiten Einführungskurs anbieten.
 
-<!-- <div style="background-color: #efefef; border: 1px solid black; padding: 2em; margin:1em 0;">
-  <iframe src="habs-gesehen.php" style="width:100%; height: 30em; border: none"></iframe>
-</div>
+<p><b>Anmeldung</b> per E-Mail an <a class="email" href="mailto:post@technikum29.de">post@technikum29.de</a>
 
+</div><!-- /rightcol -->
+</div><!-- /cols -->
 
-<div style="background-color: #efe; border: 1px solid green; padding: 2em; margin: 1em 0;">
-<img src="dokuwiki/lib/exe/fetch.php?media=wiki:dokuwiki-128.png" style="float: left; margin: -10px 1em 1em 0; width: 60px;">
-Siehe derzeit auch noch die <a href="dokuwiki/"><b>Wiki (Upload, Texte)</b></a>.
-</div>
--->
-<!--
-<div style="background-color: #efefef; border: 1px solid black; padding: 2em; margin: 1em 0;">
- Zum <b><a href="http://www.technikum29.de/schule/Physik-LK/upload">Datei-Upload</a></b>. Hier gehts zum
- <b><a href="http://www.technikum29.de/schule/Physik-LK/upload/dateien">Download der hochgeladenen Dateien</a></b>.
-</div>   
--->
 

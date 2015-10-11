e2e1880af8675eacf26fee6d8fe4d49fc43233e3
heribert <heribert@svn-users.technikum29.de>
2015-10-11 14:55:56 +0000

    git-svn-id: https://www.technikum29.de/svn/technikum29-www@848 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ de/rechnertechnik/lgp30.php
@@ -0,0 +1,12 @@
+<?php
+	$seiten_id = 'lgp30';
+	$version = '$Id: elektro-mechanik.php 688 2014-11-15 09:18:06Z heribert $';
+	$titel = 'Schoppe & Faeser: LGP 30';
+	
+	require "../../lib/technikum29.php";
+?>
+
+<h2>LGP 30 von Schoppe & Faeser</h2>
+<br>
+....diese Seite entsteht in Kürze....
+
---
+++ de/rechnertechnik/roehrenrechner.php
@@ -0,0 +1,9 @@
+<?php
+	$seiten_id = 'roehrenrechner';
+	$version = '$Id: roehrenrechner.php 480 2013-12-26 08:41:48Z heribert $';
+	$titel = 'Rechner mit Elektronenröhren';
+	
+	require "../../lib/technikum29.php";
+?>
+    <h2>Rechner mit Elektronenröhren</h2>
+    

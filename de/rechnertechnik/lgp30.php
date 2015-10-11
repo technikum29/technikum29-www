1c6d2c00dd3e493115df5adb09e0feb5f349e6ca
heribert <heribert@svn-users.technikum29.de>
2015-10-11 21:34:13 +0000

    LGP30-Seite probehalber hochgeladen. Wird weiter bearbeitet
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@851 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/lgp30.php
+++ de/rechnertechnik/lgp30.php
@@ -7,6 +7,30 @@
 ?>
 
 <h2>LGP 30 von Schoppe & Faeser</h2>
-<br>
-....diese Seite entsteht in Kürze....
+
+<p>Ein interessanter Neuzugang im September 2015:<br><br>
+Dieser für damalige Verhältnisse sehr kleine Rechner kam 1958 auf den Europäischen Markt. Er wurde von der deutschen Firma Schoppe & Faeser in Lizenz von Librascope (USA) gebaut. In den USA wurde das Modell ab 1956 verkauft. Gerade mal ca. 40 Stück wurden von dem Röhrencomputer der 1. Generation in Europa gebaut. Er war als wissenschaftlicher Rechner mit Trommelspeicher konzipiert und arbeitet mit nur 115 Elektronenröhren.<br>
+Zum Speichern von Programmen und Daten werden 1"-Lochstreifen verwendet, die ein Schnell-Leser bzw. -Stanzer verarbeitet. Als "Drucker" (besser Ausgabegerät) dient ein "Flexowriter", im Prinzip eine elektrische Schreibmaschine mit Lochstreifenzusatz.</p>
+
+<div class="center">
+		<img src="/shared/photos/rechnertechnik/lgp30-1.jpg" alt="LGP 30 Anlage" width="850" height="383" />
+		<div class="bildtext">	
+		<p><b>LGP 30 Röhrenrechner vor der Restauration</b></p> 
+		</div></div>
+		
+<p>Bei unserem Rechner fehlen die Verbindungskabel zu den Lochstreifengeräten und zum Flexowriter. Daher suchen wir Steckverbindungen, siehe. Ansonsten ist der Zustand altersgemäß. Die Bleche müssen neu lackiert und die Mechanik wieder gangbar gemacht werden. Erst danach wird sich zeigen welche Fehler die Elektronik aufweist. Eine echte Herausforderung.</p>
+		
+	<div class="desc-left auto-bildbreite borderless no-copyright">
+    <a class="popup" href="/shared/photos/rechnertechnik/lgp30-2.jpg">
+		<img src="/shared/photos/rechnertechnik/lgp30-2.jpg" alt="Frontansicht, offen" width="350" height="266" /></a>	
+		<div class="bildtext">	
+			<p><b>Vorderansicht, ohne Verkleidung</b></p> <a class="popup" href="/shared/photos/rechnertechnik/lgp30-2.jpg"> Bild vergrößern</a> 
+		</div></div>	
+				
+		<div class="desc-right auto-bildbreite borderless no-copyright">
+    <a class="popup" href="/shared/photos/rechnertechnik/lgp30-3.jpg">
+		<img src="/shared/photos/rechnertechnik/lgp30-3.jpg" alt="Rückansicht, offen" width="350" height="262" /></a>	
+		<div class="bildtext">	
+			<p><b>Rückansicht, ohne Verkleidung</b></p> <a class="popup" href="/shared/photos/rechnertechnik/lgp30-3.jpg"> Bild vergrößern</a> 
+		</div></div>			
 

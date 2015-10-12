e4544878cfdcdef475a239e2a3357658457da589
heribert <heribert@svn-users.technikum29.de>
2015-10-12 20:24:24 +0000

    zwei Bilder nebeneinander (gas funktioniert ja schon) doch der Text soll nur unter einspaltig weiterlaufen....
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@852 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/lgp30.php
+++ de/rechnertechnik/lgp30.php
@@ -10,27 +10,35 @@
 
 <p>Ein interessanter Neuzugang im September 2015:<br><br>
 Dieser für damalige Verhältnisse sehr kleine Rechner kam 1958 auf den Europäischen Markt. Er wurde von der deutschen Firma Schoppe & Faeser in Lizenz von Librascope (USA) gebaut. In den USA wurde das Modell ab 1956 verkauft. Gerade mal ca. 40 Stück wurden von dem Röhrencomputer der 1. Generation in Europa gebaut. Er war als wissenschaftlicher Rechner mit Trommelspeicher konzipiert und arbeitet mit nur 115 Elektronenröhren.<br>
-Zum Speichern von Programmen und Daten werden 1"-Lochstreifen verwendet, die ein Schnell-Leser bzw. -Stanzer verarbeitet. Als "Drucker" (besser Ausgabegerät) dient ein "Flexowriter", im Prinzip eine elektrische Schreibmaschine mit Lochstreifenzusatz.</p>
+Zum Speichern von Programmen und Daten werden 1"-Lochstreifen verwendet, die ein Schnell-Leser bzw. -Stanzer verarbeitet. Als "Drucker" (besser Ausgabegerät) dient der "Flexowriter" von FRIDEN, im Prinzip eine elektrische Schreibmaschine mit Lochstreifenzusatz.</p>
 
 <div class="center">
-		<img src="/shared/photos/rechnertechnik/lgp30-1.jpg" alt="LGP 30 Anlage" width="850" height="383" />
+		<img src="/shared/photos/rechnertechnik/lgp30-1.jpg" alt="LGP 30 Anlage" width="850" height="380" />
 		<div class="bildtext">	
 		<p><b>LGP 30 Röhrenrechner vor der Restauration</b></p> 
 		</div></div>
 		
 <p>Bei unserem Rechner fehlen die Verbindungskabel zu den Lochstreifengeräten und zum Flexowriter. Daher suchen wir Steckverbindungen, siehe. Ansonsten ist der Zustand altersgemäß. Die Bleche müssen neu lackiert und die Mechanik wieder gangbar gemacht werden. Erst danach wird sich zeigen welche Fehler die Elektronik aufweist. Eine echte Herausforderung.</p>
+
 		
-	<div class="desc-left auto-bildbreite borderless no-copyright">
+	<div class="desc-left borderless no-copyright">
     <a class="popup" href="/shared/photos/rechnertechnik/lgp30-2.jpg">
 		<img src="/shared/photos/rechnertechnik/lgp30-2.jpg" alt="Frontansicht, offen" width="350" height="266" /></a>	
 		<div class="bildtext">	
 			<p><b>Vorderansicht, ohne Verkleidung</b></p> <a class="popup" href="/shared/photos/rechnertechnik/lgp30-2.jpg"> Bild vergrößern</a> 
 		</div></div>	
 				
-		<div class="desc-right auto-bildbreite borderless no-copyright">
+		<div class="desc-right borderless no-copyright">
     <a class="popup" href="/shared/photos/rechnertechnik/lgp30-3.jpg">
 		<img src="/shared/photos/rechnertechnik/lgp30-3.jpg" alt="Rückansicht, offen" width="350" height="262" /></a>	
 		<div class="bildtext">	
 			<p><b>Rückansicht, ohne Verkleidung</b></p> <a class="popup" href="/shared/photos/rechnertechnik/lgp30-3.jpg"> Bild vergrößern</a> 
-		</div></div>			
+		</div></div>	
+
+<p class="small">Hier ein paar Daten zu unserem LGP-30. Der Verbleib bis 1962 ist nicht geklärt. Von 1962 bis 1980 wurde er im Landesvermessungsamt wie folgt eingesetzt.	<br>
+1.  Kataster: Zur Koordinatenberechnung und Herstellung von Steuerlochstreifen für die automatische Kartierung (ZUSE Graphomat Z64), Umrechnung alter Polygonierungen durch Polygonzugberechnungen, Affin- oder Helmert-Transformationen.<br>
+2.  Landesvermessung: Ausgleich trigonometrischer Netze, Auswertung von Aerotriangulationen.</small>	
+
+
+
 

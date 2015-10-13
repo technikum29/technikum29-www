66029d97567230307a654d3af0d6f6cb29c19f3b
heribert <heribert@svn-users.technikum29.de>
2015-10-13 19:52:11 +0000

    git-svn-id: https://www.technikum29.de/svn/technikum29-www@855 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/lgp30.php
+++ de/rechnertechnik/lgp30.php
@@ -6,34 +6,43 @@
 	require "../../lib/technikum29.php";
 ?>
 
-<h2>LGP 30 von Schoppe & Faeser</h2>
+<h2 id="lgp30">LGP 30 von Schoppe & Faeser</h2>
 
 <p>Ein interessanter Neuzugang im September 2015:<br><br>
-Dieser für damalige Verhältnisse sehr kleine Rechner kam 1958 auf den Europäischen Markt. Er wurde von der deutschen Firma Schoppe & Faeser in Lizenz von Librascope (USA) gebaut. In den USA wurde das Modell ab 1956 verkauft. Gerade mal ca. 40 Stück wurden von dem Röhrencomputer der 1. Generation in Europa gebaut. Er war als wissenschaftlicher Rechner mit Trommelspeicher konzipiert und arbeitet mit nur 115 Elektronenröhren.<br>
-Zum Speichern von Programmen und Daten werden 1"-Lochstreifen verwendet, die ein Schnell-Leser bzw. -Stanzer verarbeitet. Als "Drucker" (besser Ausgabegerät) dient der "Flexowriter" von FRIDEN, im Prinzip eine elektrische Schreibmaschine mit Lochstreifenzusatz.</p>
+Die Datenverarbeitungsanlage LGP-30 der 1. Generation wurde in den USA Mitte der fünfziger Jahre von den Firmen <b>L</b>IPRASCOP und <b>G</b>erneral-<b>P</b>recision entwickelt. Etwa 40 Stück dieser Anlagen wurden ab 1958 in Minden von der Firma Schoppe & Faeser in Lizens gebaut. Der LGP war als wissenschaftlicher Rechner konzipiert. Im Jahre 1962 wurde er schließlich vom <a class="go" href="/de/rechnertechnik/fruehe-computer.php#lgp21"> LGP-21</a> abgelöst.<br>
+Zum Speichern von Programmen und Daten werden 1"-Lochstreifen verwendet, die ein Schnell-Leser bzw. -Stanzer verarbeitet. Als "Drucker" (besser Ausgabegerät) dient der "Flexowriter" von FRIDEN, im Prinzip eine elektrische Schreibmaschine mit Lochstreifenzusatz.</p><br>
 
 <div class="center">
 		<img src="/shared/photos/rechnertechnik/lgp30-1.jpg" alt="LGP 30 Anlage" width="850" height="380" />
 		<p class="center">	
 		<b>LGP 30 Röhrenrechner vor der Restauration</b>
 		</p></div>
-		
-<p>Bei unserem Rechner fehlen die Verbindungskabel zu den Lochstreifengeräten und zum Flexowriter. Daher suchen wir Steckverbindungen, siehe. Ansonsten ist der Zustand altersgemäß. Die Bleche müssen neu lackiert und die Mechanik wieder gangbar gemacht werden. Erst danach wird sich zeigen welche Fehler die Elektronik aufweist. Eine echte Herausforderung.</p>
+	
+<p>Bei unserem Rechner fehlen die Verbindungskabel zu den Lochstreifengeräten und zum Flexowriter. Daher suchen wir spezielle Steckverbindungen, siehe
+<a class="go" href="/de/wir-suchen.php" target="_blank"> "gesuchte Stecker"</a>. Ansonsten ist der Zustand altersgemäß. Die Bleche müssen neu lackiert und die Mechanik wieder gangbar gemacht werden. Erst danach wird sich zeigen welche Fehler die Elektronik aufweist. Eine echte Herausforderung.</p>
 
 		
-	<div class="desc-left borderless no-copyright">
+	<div style="width: 350px;" class="desc-left borderless no-copyright">
     <a class="popup" href="/shared/photos/rechnertechnik/lgp30-2.jpg">
-		<img src="/shared/photos/rechnertechnik/lgp30-2.jpg" alt="Frontansicht, offen" width="350" height="266" /></a>	
+		<img src="/shared/photos/rechnertechnik/lgp30-2.jpg" alt="Frontansicht, offen" height="266" width="350"></a>	
 		<div class="bildtext">	
 			<p><b>Vorderansicht, ohne Verkleidung</b></p> <a class="popup" href="/shared/photos/rechnertechnik/lgp30-2.jpg"> Bild vergrößern</a> 
 		</div></div>	
 				
-		<div class="desc-right borderless no-copyright">
+		<div style="width: 350px;" class="desc-right borderless no-copyright">
     <a class="popup" href="/shared/photos/rechnertechnik/lgp30-3.jpg">
-		<img src="/shared/photos/rechnertechnik/lgp30-3.jpg" alt="Rückansicht, offen" width="350" height="262" /></a>	
+		<img src="/shared/photos/rechnertechnik/lgp30-3.jpg" alt="Rückansicht, offen" height="262" width="350"></a>	
 		<div class="bildtext">	
 			<p><b>Rückansicht, ohne Verkleidung</b></p> <a class="popup" href="/shared/photos/rechnertechnik/lgp30-3.jpg"> Bild vergrößern</a> 
 		</div></div>	
+
+<p class="clear">Bei so einem historischen Rechner sind die technischen Daten durchaus interessant:<br>
+Der Trommelspeicher hat eine Rotationsgeschwindigkeit von 3600 U/Min., der Spurabstand der feststehenden Köpfe beträgt 2mm, die Spurbreite 1mm und der Kopfabstand zur Trommel 25 µm. Die Speicherkapazität ist mit 4.096 Worten (Wortlänge 32 Bit!) für damalige Zeiten recht groß.<br>
+Mit einer Taktfrequenz von 120 kHz beträgt die typische Zugriffszeit für eine Addition (Subtraktion): 0,23 ms, für eine Multiplikation bzw. Division 15 ms.<br>
+Die Elektronik besteht aus 113 Langlebensdauer-Röhren und 1450 Germaniumdioden in 34 Modulen (12 verschiedene Einschübe).<br>
+Peripherie:<br>
+Der Lochstreifenleser liest 200 Zeichen/Sek. der Lochstreifenstanzer kann 50 Zeichen/Sek. stanzen und der Flexowriter schafft 10 Zeichen/Sek.<br>
+Das Gewicht des LGP30 beträgt stolze 350 kg (ohne Peripherie).	
 
 <p class="clear">Hier ein paar Daten zu unserem LGP-30. Der Verbleib bis 1962 ist nicht geklärt. Von 1962 bis 1980 wurde er im Landesvermessungsamt wie folgt eingesetzt:
 <ol>
@@ -41,5 +50,6 @@ Zum Speichern von Programmen und Daten werden 1"-Lochstreifen verwendet, die ein
 <li> Landesvermessung: Ausgleich trigonometrischer Netze, Auswertung von Aerotriangulationen.
 </ol>
 
-
+Es wurden durchaus anspruchsvolle Berechnungen die mit dem kleinen Rechner ausgeführt, auch wenn z.B. eine trigonometrische Punktausgleichung mit 3 Punkten 3-4 Minuten Rechenzeit in Anspruch nahmen.<br><br>
+Der Versuch der Instandsetzung des LGP-30 erfolgt in den nächsten Monaten. Wir berichten dann weiter.
 

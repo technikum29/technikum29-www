93853bf1764bcb86d4a84f58cde3e68e3d1b10ae
heribert <heribert@svn-users.technikum29.de>
2015-11-04 16:51:34 +0000

    neuen Termin eingefügt
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@873 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/index.php
+++ de/index.php
@@ -121,6 +121,14 @@
 	
 	<div class="box termin clear-after">		
 	<div class="left">
+	<p class="date"><em>2015</em> 13. Nov.</p></div>
+	<h4>Computer-History + Experimental-Workshop</h4> 
+	Termin: Freitag, der <b>13. November</b> um 15:00 Uhr <br>
+	First Data Gruppe
+	</div>
+	
+	<div class="box termin clear-after">		
+	<div class="left">
 	<p class="date"><em>2015</em> 28. Nov.</p></div>
 	<h4>Computer-History (Führung)</h4> 
 	Termin: Samstag, der <b>28. November</b> um 14:00 Uhr <br>

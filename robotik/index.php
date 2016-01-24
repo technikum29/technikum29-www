7d1109a7c699b873888bcfd70388a2d3e5d67047
heribert <heribert@svn-users.technikum29.de>
2016-01-24 08:51:05 +0000

    kleine Änderungen
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@900 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- robotik/index.php
+++ robotik/index.php
@@ -7,7 +7,7 @@ $sidebar_content = <<<SIDEBAR_ENDE
 
 <div class="text-block alertbox">
 	Seite zuletzt aktualisiert:
-	<br>1.1.2016
+	<br>24.1.2016
 </div>
 
 <div class="spacing"></div>
@@ -46,7 +46,7 @@ $sidebar_content = <<<SIDEBAR_ENDE
 <div class="text-block">
 	<strong>Robotik in der Schule:</strong>
 	<p>Das technikum29 sponsert je einen Robotik-Workshop an folgenden Schulen: Eichendorffschule (EDS), Kelkheim sowie Albert-Einstein-Schule (AES), Schwalbach. Hier werden für die Klassen 6/7 zwei Stunden pro Woche angeboten und von den Schülern mit Begeisterung angenommen! <br>
-	Diese Kurse sind total ausgebucht: An der AES gab es z.B. 41 Anmeldungen aus den 6. Klassen.<br><br>
+	Diese Kurse sind total ausgebucht: An der AES gab es über 40 Anmeldungen aus den 6. Klassen.<br><br>
 	Siehe auch: <a href="http://www.aesmtk.de/cms/index.php/49-ueber-uns/mint/765-robotik-workshop-begeistert-sechstklaessler-der-aes"> Robotik-Workshop begeistert Sechstklässler der AES</a> </p>
 </div>
 
@@ -69,7 +69,7 @@ SIDEBAR_ENDE;
 
 <div class="leftcol">
 
-<p>Fun for Future: Roboter werden in Zukunft eine immer größere Bedeutung haben. Diese zu bauen und programmieren macht Spaß, da man hier richtig kreativ "spielen" kann. In dem <b>Ferienworkshop</b> lernst du mit einer grafischen Methode, wie man Programme erstellt.
+<p><b>Fun for Future:</b> Roboter werden in Zukunft eine immer größere Bedeutung haben. Diese zu bauen und programmieren macht Spaß, da man hier richtig kreativ "spielen" kann. In dem <b>Ferienworkshop</b> lernst du mit einer grafischen Methode, wie man Programme erstellt.
 <br>Du erfährst, wie der Roboter exakt deine Anweisungen ausführt. Wenn alle Sensoren im Einsatz sind, kann sich der Roboter mühelos in einem fremden Umfeld bewegen. Er findet sich sogar in einem Labyrinth zurecht!<br>
 
 <p>
@@ -117,7 +117,7 @@ Die Kursgebühr für den Ferienkurs (10 Zeitstunden) beträgt 40 Euro.</p>
 <p>In den nächsten Osterferien bieten wir wieder einen Robotik-Workshop an.<br>
 Entweder von Di. 29.3. bis Sa. 2.4.2016 oder von Mo. 4.4. bis Fr. 8.4.2016<br>
 Es wird der Termin gewählt an welchem genügend Anmeldungen erfolgen. Eventuell finden beide Termine statt.
-Eine rechtzeitige Vormerkung ist empfehlenswert.
+Eine rechtzeitige Vormerkung ist empfehlenswert. Einen Fortsetzungskurs wird in der letzten Woche der Sommerferien angeboten.
 </p>
 
 </div><!-- /rightcol -->

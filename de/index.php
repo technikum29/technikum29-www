2695ab4ce2ef0bdb40a7c2a74dc0d30eb162eca3
sven <sven@svn-users.technikum29.de>
2016-02-14 17:09:18 +0000

    Robotik-Link auf Startseite korrigiert.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@905 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/index.php
+++ de/index.php
@@ -122,7 +122,7 @@
 	<div class="left">
 	<p class="date"><em>2016</em></p></div>
 	<h4>Robotik-Workshop in den Ferien für Kids von 11-13 Jahren.</h4> 
-	Hochinteressante Kurse in welchen Roboter der neuesten Generation gebaut und programmiert werden. Weitere Informationen unter <a class="go" href="http:/robotik"> Robotik-Workshop</a><br>
+	Hochinteressante Kurse in welchen Roboter der neuesten Generation gebaut und programmiert werden. Weitere Informationen unter <a class="go" href="/robotik"> Robotik-Workshop</a><br>
 	Termine: Osterferien 2016 
 	</div>
 	

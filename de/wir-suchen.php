08c84ae3c7e82d0af31594656d606a0c202dfa00
sven <sven@svn-users.technikum29.de>
2016-11-14 17:44:07 +0000

    "Wir suchen"-Seite gegliedert.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1044 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/wir-suchen.php
+++ de/wir-suchen.php
@@ -6,37 +6,38 @@
 	require "../lib/technikum29.php";
 ?>
 
-   <h2>Was (wen) das Museum noch sucht</h2><br>
+   <h2>Was (wen) das Museum noch sucht</h2>
+
+	<h3>Menschen</h3>
    
   Immer gesucht aber schwerlich zu finden sind Leute mit starkem technischem Interesse, die hier mitmachen möchten. Arbeiten Sie sich in eines unserer Computer-Systeme ein, es macht wirklich Spaß und ist ein ungewöhnliches und anspruchsvolles Hobby. Dabei könnten Sie Schwerpunkte im Software- oder Hardware-Bereich setzen. Sie sollten jedoch im näheren Umkreis des Museums wohnen. 
-   <br>
-   
-	
-		
-	<h3>Geräte, Manuals und Bauteile</h3><br>
-	
-    <div class="left">
-		<img src="/shared/photos/rechnertechnik/card-5806223.jpg" alt="Plotter-Interface" width="350" height="483" />
-	</div>
-	Gesucht werden Manuals (Schaltpläne) für das <b>AMPEX TMZ</b> Magnetbandlaufwerk (Digital Tape Memory System) sowie Unterlagen zum Interface von WDV (München), siehe IBM 1130<br>
-	<hr><br>
-	Für unsere IBM 1130 suchen wir ferner die Plotterinterface-Karte mit der Nummer 5806223 oder das Schaltbild hiervon. Der abgebildete Schaltplan gehört zu einer anderen Karte. Das nebenstehende Bild lässt sich vergrößern: Auf die rechte Maustaste klicken und Grafik anzeigen.<br><br>
-	<hr>
 
-     
-<hr id="stecker">
-	  <p>Wir sind nunmehr im Besitz eines <b>LGP 30</b> Röhrenrechners Bj. 1958-61. Leider fehlen einige Verbindungskabel. Daher suchen wir 37-polige Rundstecker(-buchsen) die von Cannon gebaut wurden, siehe Bild unten. Der Durchmesser beträgt ca. 40mm.<br>
-	  Auch andere alte 40-polige Steckverbindungen würden uns eventuell weiter helfen.</p><br>
-    
-<div class="left">
-		<img src="/shared/photos/rechnertechnik/cannon-stecker.jpg" alt="Cannon-Steckverbindung" width="627" height="338" />
+		
+	<h3>Geräte, Manuals und Bauteile</h3>
+
+	<h4>Schaltpläne für AMPEX Magnetbandlaufwerk</h4>
+	Gesucht werden Manuals (Schaltpläne) für das <b>AMPEX TMZ</b> Magnetbandlaufwerk (Digital Tape Memory System) sowie Unterlagen zum Interface von WDV (München), siehe <a class="go" href="/de/rechnertechnik/ibm1130.php">IBM 1130</a>
+	
+	<h4>Plotterinterface-Karte für IBM 1130</h4>
+	<div class="left clear-after">
+		<a href="/shared/photos/rechnertechnik/card-5806223.jpg"><img src="/shared/photos/rechnertechnik/card-5806223.jpg" alt="Plotter-Interface" width="350" height="483" /></a>
+		<p class="bildtext">
+			Für unsere IBM 1130 suchen wir ferner die Plotterinterface-Karte mit der Nummer 5806223 oder das Schaltbild hiervon. Der <a href="/shared/photos/rechnertechnik/card-5806223.jpg">abgebildete Schaltplan</a> gehört zu einer anderen Karte. Das nebenstehende Bild lässt sich durch Anklicken vergrößern.
+		</p>
 	</div>
+
+	<h4 id="stecker">Cannon-Verbindungsstecker</h4>
+	<div class="left clear-after">
+		<img src="/shared/photos/rechnertechnik/cannon-stecker.jpg" alt="Cannon-Steckverbindung" width="627" height="338" />
+		<p class="bildtext">Wir sind nunmehr im Besitz eines <b>LGP 30</b> Röhrenrechners Bj. 1958-61. Leider fehlen einige Verbindungskabel. Daher suchen wir 37-polige Rundstecker(-buchsen) die von Cannon gebaut wurden, vgl <a href="/shared/photos/rechnertechnik/cannon-stecker.jpg">das abgebildete Foto</a>.
+	</div>
+	<p> Der Durchmesser der Rundstecker beträgt ca. 40mm. Auch andere alte 40-polige Steckverbindungen würden uns eventuell weiter helfen.</p>
 	<br>
 	
 	<div class="left">
 		<img src="/shared/photos/rechnertechnik/cannon-40polig.jpg" alt="Cannon-Steckverbindung" width="497" height="566" />
+		<p>Die links abgebildete Steckverbindung wird ebenfalls gesucht: 40-polig von Cannon.
 	</div>
-	Die unten abgebildete Steckverbindung wird ebenfalls gesucht: 40-polig von Cannon.
 	
 	
 	

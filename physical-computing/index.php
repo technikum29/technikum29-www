15feac36782203d86b01360d1a5dd09afa7347a3
heribert <heribert@svn-users.technikum29.de>
2016-12-29 17:39:03 +0000

    Rechtschreibkorrekturen von Sylvia
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1078 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- physical-computing/index.php
+++ physical-computing/index.php
@@ -44,16 +44,16 @@ SIDEBAR_ENDE;
 
 <p><h3>Grundsätzliches </h3> <br>
 
-Schon immer üben Roboter eine Faszination auf Jugendliche und Kinder aus. Diese Faszination sollte man nutzen, um einen altersgerechten Einstieg in die Programmierung zu ermöglichen.
-Im Vergleich zu den ausschließlich am Bildschirm dargestellten virtuellen Simulationen, die im üblichen Informatikunterricht ablaufen, werden hier physikalisch angreifbare Objekte bewegt. Diese interessanten Anwendungen sind für Jugendliche äußerst motivierend. Spielerisches Lernen wird hier Realität.<br>
+Schon immer üben Roboter eine Faszination auf Kinder und Jugendliche aus. Diese Faszination sollte man nutzen, um einen altersgerechten Einstieg in die Programmierung zu ermöglichen.
+Im Vergleich zu den ausschließlich am Bildschirm dargestellten virtuellen Simulationen, wie sie im üblichen Informatikunterricht ablaufen, werden hier physikalisch anfassbare Objekte bewegt. Diese interessanten Anwendungen sind für Jugendliche äußerst motivierend. Spielerisches Lernen wird hier Realität.<br>
 
-Im Workshop "Physical-Computing & Robotics" wird ein Microcontroller mit der physikalischen Außenwelt durch Sensoren und Aktoren verbunden. Diese Controller steuern z.B. intelligent einen Roboter. Bei der Roboter-Programmierung hat man sofort ein greifbares Feedback an dem Jugendliche erkennen können ob das implementierte Programm läuft oder nicht. Da kann schon mal ein Roboter sprichwörtlich "gegen die Wand fahren".
+Im Workshop "Physical-Computing & Robotics" wird ein Microcontroller mit der physikalischen Außenwelt durch Sensoren und Aktoren verbunden. Diese Controller steuern z.B. intelligent einen Roboter. Bei der Roboter-Programmierung hat man sofort ein greifbares Feedback, an dem Jugendliche erkennen können, ob das implementierte Programm läuft oder nicht. Da kann schon mal ein Roboter sprichwörtlich "gegen die Wand fahren".
 	<div class="box left">
 	<img src="yannik.jpg" width="180"  height="230"/> 
 </div>
  Im Laufe des Kurses werden die Aufgaben immer freier lösbar, dadurch wird das eigenverantwortliche Lernen adressiert. Erfahrungsgemäß macht den meisten das Lösen der recht offen gestalteten Gesamtaufgaben großen Spaß, da sie hier ihrer Kreativität freien Lauf lassen können.<br>
- Für besonders begabte Schüler ergibt sich immer die Möglichkeit zusätzlich schwierigere Programmteile einzubauen. Das erhöht das Selbstwertgefühl und schafft eine natürliche ungezwungene Binnendifferenzierung.<br>
-Ein weiterer Vorteil ist, dass Schüler sowohl mit Hard- als auch Software konfrontiert werden wobei die Grenzen dieser Welten zunehmend verschwimmen.<br>
+ Für besonders begabte Schülerinnen und Schüler ergibt sich immer die Möglichkeit zusätzlich schwierigere Programmteile einzubauen. Das erhöht das Selbstwertgefühl und schafft eine natürliche ungezwungene Binnendifferenzierung.<br>
+Ein weiterer Vorteil ist, dass Schüler sowohl mit Hard- als auch Software konfrontiert werden, wobei die Grenzen dieser beiden Welten zunehmend verschwimmen.<br>
 Es ist unübersehbar, dass dieser Themenbereich ein hohes Maß an Abwechslung, einen kreativen Freiraum sowie das Erlernen von Teamplaying (Zweierteams) bietet und zudem absolut "up-to-date" ist, eben ein HIGHLIGHT einer Schule.
 
 </div><!--/leftcol -->
@@ -68,13 +68,13 @@ In diesem Workshop erlernen wir das Programmieren von Arduino-Microcontrollern. 
 </div>
 
 Wie in dieser Materie üblich beginnen wir mit der blinkenden LED (Leuchtdiode). Anhand solcher einfachen Sketche (Arduino-Programme) versteht man schnell die Methoden dieser Programmierung. Die Aufgabenstellungen werden durch die Einführung vieler neuer Sensoren immer interessanter, schließlich soll unser Roboter intelligent interagieren können.<br>
-Durch die Einbindung von sogenannten "Libraries" (Programm-Bibliotheken) können wir verblüffende Effekte erzielen, der Spaßfaktor steigt kontinuierlich an. Wer immer mit Erfolg daran teilnimmt kann von sich behaupten in der Entwicklung unserer digitalen Welt als Schüler ganz vorne zu sein, etwas Besonderes zu leisten und eventuell den Grundstein für ein tolles, anspruchsvolles Hobby oder gar den späteren Beruf zu setzen.
+Durch die Einbindung von sogenannten "Libraries" (Programm-Bibliotheken) können wir verblüffende Effekte erzielen, der Spaßfaktor steigt kontinuierlich an. Wer immer mit Erfolg daran teilnimmt, kann von sich behaupten, in der Entwicklung unserer digitalen Welt als Schüler ganz vorne zu sein, etwas Besonderes zu leisten und eventuell den Grundstein für ein tolles, anspruchsvolles Hobby oder gar den späteren Beruf zu setzen.
 
 <div class="box right">
 	<img src="arduino.jpg" width="220"  height="171"/> 
 </div>
 
-Um den Teilnehmern die Möglichkeiten zu geben, auch Zuhause Aufgaben, Analysen und Skteche aus dem Unterricht nachlesen zu können, werden wir diese hier mit einem Link veröffentlichen. Für alle "Fremdleser" sei bemerkt, dass dieser Kurs am Entstehen ist. Noch ist nicht alles perfekt.
+Um den Teilnehmern die Möglichkeiten zu geben, auch Zuhause Aufgaben, Analysen und Sketche aus dem Unterricht nachlesen zu können, werden wir diese hier mit einem Link veröffentlichen. Für alle "Fremdleser" sei angemerkt, dass dieser Kurs am Entstehen ist. Noch ist nicht alles perfekt.
 Ferner findet man hier Links auf andere Seiten, die gegebenenfalls zur Vertiefung oder zum Nacharbeiten gedacht sind.
 </p>
 

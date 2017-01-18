9353e918b42c80800c4c4a15af190fec3a5e6d64
sven <sven@svn-users.technikum29.de>
2017-01-18 20:25:03 +0000

    Passwort-Demo: Barriere durch Zaun ersetzt. Damit lassen sich mehrere eingesperrte Textbereiche pro Seite machen und dahinter kann auch Text stehen.
    Layout weiter minimalisiert, auf eine Zeile reduziert.
    
    
    
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1100 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- lib/simplepassword.php
+++ lib/simplepassword.php
@@ -1,5 +1,108 @@
 <?php
 /**
+ * Simpler Passwortschutz von Textbereichen in einer t29-Seite.
+ * Prinzipiell ist dieser Passwortschut wirkungsvoll, sofern HTTPS
+ * verwendet wird. Die primitive Übermittlung per GET (oder POST)
+ * sorgt aber dafür, dass die Benutzererfahrung schwach ist (das
+ * Passwort wird etwa plain-text in der URL angezeigt oder ein einfaches
+ * Neuladen einer ansonsten statischen Seite führt stets zu den POST-
+ * Warnungen).
+ *
+ * In der Praxis auf t29 ist dieser Passwortschutz allerdings schon alleine
+ * deswegen völlig wirkungslos, weil die Files alle öffentlich per 
+ * SVN einsehbar sind. Aber Heribert wollte es ja so. Ist nur ein
+ * Demonstrator.
+ *
+ * Beispiel einer verwenden Seite gibt es weiter unten.
+ *
+ * Es gibt zwei Varianten des Schutzes, wobei der t29FencedContent
+ * der allgemeinere ist. Die t29PasswordBarrier hingegen geht nur,
+ * wenn die Seite danach aufhört.
+ *
+ * Aktuelle Demo zu t29FencedContent gibt es unter
+ * /physical-computing/geschuetzte-demo.php
+ *
+ **/
+
+class t29FencedContent {
+	public $password;
+	public $paramkey = 't29-pwd-barrier';
+	public $id; // used in form #id
+
+	function __construct($id, $password=Null) {
+		$this->id = $id;
+		$this->password = $password;
+	}
+
+	function isAuthenticated() {
+		return (isset($_GET[$this->paramkey]) &&
+			$_GET[$this->paramkey] == $this->password);
+	}
+
+	function failedToAuthenticate() {
+		return (isset($_GET[$this->paramkey]) &&
+			$_GET[$this->paramkey] != $this->password);
+	}
+
+	function printAnchor() {
+		print '<!-- t29FencedContent anchor -->';
+		print '<a id="'.$this->id.'"></a>';
+	}
+
+	function printMiniForm() {
+		// Print small inline "login/logut" form, Heribert style
+		if( $this->failedToAuthenticate()) {
+			?>
+			Passwort falsch, bitte nochmal probieren:
+			<?php
+			$this->printLoginForm();
+		} else if($this->isAuthenticated()) {
+			$this->printLogoutForm();
+		} else {
+			?>
+			Passwort:
+			<?php
+			$this->printLoginForm();
+		}
+	}
+
+	function printLoginForm() {
+		?>
+		<form method="get" style="display: inline-block;"
+		   action="#<?php echo $this->id; ?>">
+			<input type="password" name="<?= $this->paramkey; ?>">
+			<input type="submit" value="Anmelden">
+		</form>
+		<?php
+	}
+
+	function printLogoutForm() {
+		?>
+		<form method="get" class="inline">
+			<input type="submit" value="Abmelden">
+		</form>
+		<?php
+	}
+
+	function start() {
+		if(!$this->isAuthenticated()) {
+			// buffer (thus hide) content
+			ob_start();
+		}
+	}
+
+	function end() {
+		if(!$this->isAuthenticated()) {
+			// delete buffer content, thus hide content
+			ob_end_clean();
+		}
+	}
+
+}
+
+
+
+/**
  * Simpler Passwortschutz im Sinne einer "Barriere", die inmitten einer
  * t29-Seite stehen kann.
  * Prinzipiell ist dieser Passwortschut wirkungsvoll, sofern HTTPS
--- physical-computing/geschuetzte-demo.php
+++ physical-computing/geschuetzte-demo.php
@@ -4,8 +4,10 @@
 
 	$dynamischer_inhalt = true;
 	require "../lib/simplepassword.php";
-	$barriere = new t29PasswordBarrier();
-	$barriere->password = "huhn";
+	$zaun1 = new t29FencedContent('SchuleABC-Geheimnisse');
+	$zaun2 = new t29FencedContent('SchuleDEF-Geheimnisse');
+	$zaun1->password = "hahn";
+	$zaun2->password = "hundi";
 
 	require "../lib/technikum29.php";
 ?>
@@ -23,18 +25,42 @@
 mehr. Damit das nicht jeder lesen kann, wird ein einfaches Passwort
 benötigt.
 
-<?php
-	if(! $barriere->isAuthenticated()) {
-		$barriere->printAuthForm();
-		return;
-	} else {
-		$barriere->printSuccessForm();
-	}
-?>
-
-Der aktuelle Stand unserer Arbeiten: Jonas hatte Kopfschmerzen und
-Julia wurde strafversetzt.
+<?php $zaun1->printAnchor(); ?>
+<div class="panel panel-default">
+<div class="panel-heading">
+	<strong>Kurs an der ABC-Schule</strong>
+	<?php $zaun1->printMiniForm(); ?>
+</div>
+<?php $zaun1->start(); ?>
+<div class="panel-body">
+<p>Der aktuelle Stand unserer Arbeiten: Jonas hatte Kopfschmerzen und
+Julia wurde strafversetzt.</p>
 
 <h3>Neue Übungsblätter</h3>
 
 <p>blablabla
+
+</div><!--/panel-body -->
+<?php $zaun1->end(); ?>
+</div><!--/panel -->
+
+<!-- zweiter Zaun: -->
+
+<?php $zaun2->printAnchor(); ?>
+<div class="panel panel-default">
+<div class="panel-heading">
+	<strong>Kurs an der DEF-Schule</strong>
+	<?php $zaun2->printMiniForm(); ?>
+</div>
+<?php $zaun2->start(); ?>
+<div class="panel-body">
+<p>Hier stehen ganz andere Geheimnisse drin.
+<p>Hier stehen ganz andere Geheimnisse drin.
+<p>Hier stehen ganz andere Geheimnisse drin.
+<p>Hier stehen ganz andere Geheimnisse drin.
+</div><!--/panel-body -->
+<?php $zaun2->end(); ?>
+</div><!--/panel -->
+
+
+Hinter diesen Boxen kann wieder ganz normaler Text stehen.

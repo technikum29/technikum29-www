b071cc35ad1f2c78617eb04b4b97800326211907
heribert <heribert@svn-users.technikum29.de>
2017-01-25 08:08:27 +0000

    ersetzt....
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1113 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ physical-computing/ping-pong-led/ping-pong-led.ino
@@ -0,0 +1,54 @@
+// ping-pong-LED mit bitSet und outShift (inkl. Toene)
+// Aufgabe 1a/b von Blatt 5
+
+int clockPin = 8;
+int latchPin = 9;
+int dataPin = 10;
+
+int leds = 0;
+
+void setup() 
+{
+ pinMode(latchPin, OUTPUT);
+ pinMode(dataPin, OUTPUT);  
+ pinMode(clockPin, OUTPUT);
+}
+
+void loop() 
+{
+  int frequenz = 200;
+  
+  for (int n = 0; n< 8; n++)
+  {
+    bitSet(leds, n);
+    updateShiftRegister();
+    tone(7, frequenz, 100);
+    delay(100); 
+    frequenz=frequenz+50;
+    leds=0;
+    updateShiftRegister();    
+  }
+  
+frequenz=1000;
+
+  for (int n = 7; n>=0; n--)
+  {
+    bitSet(leds, n);
+    updateShiftRegister();  
+    tone(7, frequenz, 100);
+    delay(100);
+    frequenz=frequenz-50;
+    leds=0;
+    updateShiftRegister();
+        
+  }
+}
+
+void updateShiftRegister()    
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, MSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
+
+

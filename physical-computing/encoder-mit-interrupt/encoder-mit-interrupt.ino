fa7467b4a18ef5ef6584251dc91c34cf5eacb0db
heribert <heribert@svn-users.technikum29.de>
2017-03-20 21:26:20 +0000

    Encoder-Programm korrigiert
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1172 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- physical-computing/encoder-mit-interrupt/encoder-mit-interrupt.ino
+++ physical-computing/encoder-mit-interrupt/encoder-mit-interrupt.ino
@@ -1,7 +1,9 @@
 /* 
  * Rotary-Encoder Sketch mit Interrupt Nr. 2
  * beachte: Bei schnellen Vorgaengen verlangsamt der Serielle-Monitor den Sketch!
- *  
+ * Wenn der Button (Switch) zum Rueckstellen ins Programm eingebaut wird, muss
+ * man den INPUT mit PULLUP versehen! 
+ * 
  * read a rotary encoder with interrupts
    Encoder hooked up with common to GROUND,
    encoder0PinA to pin 2, encoder0PinB to pin 3 
@@ -14,7 +16,6 @@
 
 #define encoder0PinA  2
 #define encoder0PinB  3
-#define buttonPin 4
 
 volatile int encoder0Pos = 0;
 
@@ -22,11 +23,10 @@ void setup() {
   
   pinMode(encoder0PinA, INPUT); 
   pinMode(encoder0PinB, INPUT); 
-  pinMode(buttonPin, INPUT_PULLUP);
-
+  
   attachInterrupt(0, doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
   Serial.begin (9600);
-  Serial.println("Start");               
+  Serial.println("Start des Programms");               
 } 
 
 void loop(){    
@@ -38,13 +38,13 @@ void doEncoder() {
    * forward. If they're different, it's going backward.
    */
   if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
-    encoder0Pos--;
-  } else {
     encoder0Pos++;
+  } else {
+    encoder0Pos--;
   }
    Serial.print("    ");
   Serial.println (encoder0Pos);
 }
 
-  // you don't want serial slowing down your program if not needed
+ 
 

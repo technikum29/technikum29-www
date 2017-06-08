0e310be3a9fee5b707aad304bb03f598d8f2710e
sven <sven@svn-users.technikum29.de>
2017-06-08 15:39:26 +0000

    Englische Startseite neu formuliert.
    Lose an der deutschen Fassung orientiert, etwas mehr einordnend.
    
    
    
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1230 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- en/index.php
+++ en/index.php
@@ -9,65 +9,70 @@
     <div class="cols startseiten-boxen">
        <div class="leftcol">
          <div class="besonderes">
-           <h2>What's special about it?</h2>
+           <h2>Operational computer history</h2>
            <ul>
-               <li>Our museum shows the interesting development of technology
-                   that strongly influences our life</li>
-               <li>In contrast to similar museums that normally only exhibit
-                   the history of technology without showing their functionality,
-                   almost all devices in the technikum29 are operational and are
-                   demonstrated during the guided tours. So you get a totally
-                   different picture of that time. This is quite absolutely
-                   unique </li>
+               <li>Almost all exhibits in our museum are restored and fully functional:
+                   They can be turned on and take the user back to a time of
+                   oil, mechanics, shaky buttons and glowing bulbs.
+               <li>This is very different to popular computer history and science
+                   museums. The operation of our devices are always demonstrated
+                   during the guided tours. Visitors
+                   get a completely different impression of the circumstances of
+                   that time. Most likely, this is unique worldwide.
+               <li>We concentrate on the first devices which were called "computers",
+                   i.e. a time span of roughly 1880 until 1970. We do not cover
+                   personal computers (late 1970s and beyond) at all. On principle,
+                   almost all of our machines are big and bulky, they can be opened
+                   and one can watch the machine do computational steps. From our
+                   best knowledge, we are the only living computer museum covering
+                   this early period of rising electric computing.
            </ul>
           </div>
 
         <div class="wen">
-            <h2>Target group</h2>
+            <h2>Didactics and labs</h2>
             <ul>
-                <li>"Old veterans" who might become enthusiastic while
-                    actually using devices from their former days</li>
-                <li>Interested people who want to trace the process of
-                    origin of today's technolgy. You don't have to be
-                    an expert: Almost everybody can find an interesting
-                    part in our broad spectrum of possibilities.</li>
-                <li>Student groups (at least 10 years old), firms outgoing, etc.</li>
+                <li>Naturally, our museum attracks "veterans" who worked with
+                    these machines in the past. We recognize the knowledge and
+                    work together to restore the machines.
+                <li>Our most pleasant target group are young kids, school classes
+                    and students.
+                    We organize workshops to build and program both old and new
+                    technology.
+                <li>On a regular level, we carry out guided tours for everybody.
+                    We attract people throughout Germany and neighbouring contries.
+                    <!--However, our tours are only held in German.-->
+                <li>We also host cultural events in the scope of broadcasting
+                    our message. We frequently invite speakers from science and
+                    industry in order to highlight the development and importance
+                    of computer history.
             </ul>
         </div>
       </div><!--leftcol-->
 
       <div class="rightcol">
-        <div class="wo">
-            <h2>Location</h2>
-            <ul>
-                <li>The museum is located in Germany, close to Frankfurt am Main
-                    (more precisely: 65779 Kelkheim). It is privately owned
-                    and run.</li>
-                <li>See <a class="go" href="contact.php">contact data</a></li>
-            </ul>
-        </div>
-
         <div class="foto">
                 <img src="/shared/photos/start/museum.jpg" alt="Photo of the museum building" width="338" height="228" />
                 <p>The Museum</p>
         </div>
 
-        <div class="wann">
-            <h2>Opening hours</h2>
+        <div class="wo">
+            <h2>Location</h2>
             <ul>
-		<li>Guides are available only in German language.
-                <li>We accept only group bookings. Minimum number of participants 8, maximum 20, ideal 10-16.
-                    Students have free admission.
-                </li>
+                <li>The museum is located in the center of Germany, close to
+                    Frankfurt am Main. It is privately owned and run.
+                <li>For details, see <a class="go" href="contact.php">contact data</a></li>
             </ul>
         </div>
 
-        <div class="erwartung">
-            <h2>Provided services</h2>
+        <div class="wann">
+            <h2>Opening hours</h2>
             <ul>
-                <li>Visits are always connected to a guided tour and at least one
-                   presentation on a particular topic. Student groups get an
-                   educational tour, divided in three modules.
+                <li>We don't have regular opening hours. Instead, we accept only
+                    group bookings for guided tours. Minimum number of participants 8,
+                    maximum 20, ideal 10-16.
+		<li>Guides are available only in German language.
+                <li>Students have free admission.
                 </li>
             </ul>
         </div>

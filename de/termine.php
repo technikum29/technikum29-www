9d50530e7bb7e0ff8bcfce8d792df2437036bde3
heribert <heribert@svn-users.technikum29.de>
2017-06-11 20:56:21 +0000

    Termine aktualisiert
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1233 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/termine.php
+++ de/termine.php
@@ -53,6 +53,12 @@
 		<td>Interaktive Führung
 		<td>Rossertschule <br>
 		
+		<tr>
+		<td>Do, <b>29. Juni</b><br>9:00 + 10:15 Uhr
+		<td>Computer-History<br>
+		<td>Interaktive Führung
+		<td>Eichendorffschule G5b<br>
+		
 	<!--	<font color="#FF0000">>>ausgebucht!</font>  -->
 	
 		

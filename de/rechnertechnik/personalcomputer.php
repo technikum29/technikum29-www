f0d679f29e4deb841b13cef43f962a6907eebc11
heribert <heribert@svn-users.technikum29.de>
2017-12-19 22:56:08 +0000

    kleine Änderungen
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1396 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/personalcomputer.php
+++ de/rechnertechnik/personalcomputer.php
@@ -8,11 +8,20 @@
  
  <header class="teaser seitenstart">
 	<img src="/shared/photos/rechnertechnik/header-wang2200.jpg" width="940"  height="545">
-	<h2>Historische Personalcomputer</h2>
+	<h2>Historische Personal-Computer</h2>
 </header>
 
-<br>.....diese Seite befindet sich im Aufbau.......<br><br>
+<br>.....Diese Seite befindet sich im Aufbau und wird in Kürze erweitert.<br><br>
 
-<h3 id="wang2200"><b>WANG 2200</b> mit umfangreicher Peripherie</h3>
-    <p>Weiterhin ist das erste System angeschlossen, was schon so ähnlich wie heutige Computer aussieht: <a class="go" name="backlink-wang2200" href="/de/geraete/wang2200.php">WANG 2200</a>, Bj. 1973. Vermutlich einmalig in Deutschland ist dieser Computer mit so vielen peripheren Ger&auml;ten. So z.B. Lochstreifenleser, Stapelkartenleser, 8-Zoll dreifach Diskettenlaufwerk, Plattensystem mit 38cm gro&szlig;en Scheiben (alleine 100kg schwer und 24.000,-DM teuer speichert es gerade 5 MB), spezial BASIC-Tastatur usw.</p>
-    <p>WANG erkannte schon sehr früh, dass die Zukunft der Computer nur mit Bildschirm denkbar ist. Bis 1975 baute dagegen der Konkurrent HP seine Rechner nur mit einer einzeiligen Matrix-LED-Anzeige.</p>
+<h3 id="wang2200"><b>WANG 2200A/B</b> mit umfangreicher Peripherie</h3>
+
+<p>"Was ist ein Personal-Computer", eine mühselige Frage, die wir so beantworten würden:<br>
+	Ein Personal-Computer (PC) ist ein Einzelplatzsystem mit einem Datensichtgerät (Röhrendisplay) sowie geeigneter Peripherie zum Speichern und zur Ausgabe von Daten/Programmen. Es ist mühelos transportabel.<br>
+	Mit dieser Festlegung sind z.B. der Olivetti P101 sowie auch der IBM 1130 keine PCs.<br>
+	Einer der allerersten PCs ist der WANG 2200A/B aus dem Jahre 1973. WANG hat sehr früh erkannt, dass ein mehrzeiliges Sichtgerät zukunftweisend ist. Zu dieser Zeit arbeiteten die HP-Rechner noch mit einem einzeiligen LED-Display. Für den WANG 2200 und dessen Nachfolger gab es eine Unzahl von Programmbeispielen und Peripheriegeräte, welche jeweils den Bedürfnissen der wissenschaftlichen und kommerziellen Anwendung gerecht wurden. Eine Maschinensprache oder Assembler gab es nicht. Der Rechner arbeitet unmittelbar in der einfach erlernbaren Programmiersprache <b>BASIC</b>.<br>
+	
+    Vermutlich einmalig in Deutschland ist diese funktionsfähige Computer-Anlage mit so vielen peripheren Geräten: Lochstreifenleser (2203), Stapelkartenleser (2234), Markierungskartenleser (2214), 8-Zoll dreifach Diskettenlaufwerk (2243) und einem Plattensystem mit 38cm großen Scheiben. Das Gewicht des Plattensystems (eine Festplatte und eine Wechselplatte) beträgt ca. 100kg und der Preis damals stolze 24.000,- DM. Damit konnte man immerhin bereits komfortable 5 MB speichern.</p>
+	<br>
+	
+	Fortsetzung folgt.....
+   

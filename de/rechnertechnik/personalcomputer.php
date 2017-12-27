dc5f57b16e4bf460dddc2249820cf70667525508
heribert <heribert@svn-users.technikum29.de>
2017-12-27 09:08:16 +0000

    Text aktualisiert. Fortsetzung folgt...
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1401 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/personalcomputer.php
+++ de/rechnertechnik/personalcomputer.php
@@ -13,16 +13,16 @@
 
 <br>.....Diese Seite befindet sich im Aufbau und wird fortlaufend erweitert.<br>
 
-<h3 id="wang2200"><b>WANG 2200 B</b> mit umfangreicher Peripherie</h3>
+<h3 id="wang2200"><b>WANG 2200B</b> mit umfangreicher Peripherie</h3>
 
 <p>"Wie definiert man einen Personal-Computer?", eine oft gestellte Frage, die wir so beantworten würden:<br>
 	Ein Personal-Computer (PC) ist ein Einzelplatzsystem mit einem Datensichtgerät (damals ein Röhren-Monitor) sowie geeigneter Peripherie zum Speichern und zur Ausgabe von Daten/Programmen. Es ist mühelos transportabel.<br>
-	Einer der allerersten PCs ist der WANG 2200 A/B aus dem Jahre 1973. WANG hat sehr früh erkannt, dass nur ein mehrzeiliger Monitor zukunftweisend ist. Zu dieser Zeit arbeiteten die HP-Rechner noch mit einem einzeiligen LED-Display. Hier gibt es einen schönen <a href="http://www.wang2200.org/docs/internal/HP_Competitive_Analysis_9830_vs_Wang_2200B_Mar_1974.pdf"> Vergleich von HP 9830 und WANG 2200 (PDF)</a> aus dem Jahre 1974, 15 Seiten in engl. Sprache.  <br> 
-	Für dieses System und deren Nachfolger gab es eine Unzahl von Programmbeispielen und Peripheriegeräte, welche jeweils den Bedürfnissen der kommerziellen und vor allem der wissenschaftlichen Anwendung gerecht wurden. Eine Maschinensprache bzw. einen Assembler gab es nicht. Der Rechner arbeitet unmittelbar in der einfach erlernbaren Programmiersprache <b>BASIC</b>.<br>
+	Einer der allerersten PCs ist der WANG 2200 A/B aus dem Jahre 1973. WANG hat sehr früh erkannt, dass nur ein mehrzeiliger Monitor zukunftweisend ist. Zu dieser Zeit arbeiteten die HP-Rechner noch mit einem einzeiligen LED-Display. Hier gibt es einen schönen <a href="http://www.wang2200.org/docs/internal/HP_Competitive_Analysis_9830_vs_Wang_2200B_Mar_1974.pdf" target="_blank"> Vergleich zwischen HP 9830 WANG 2200 (PDF)</a> als Verkaufsargumente für HP-Händler aus dem Jahre 1974, 15 Seiten in engl. Sprache. WANG und HP waren <u>die</u> Konkurrenten der 70er Jahre im Bereich der wissenschaftlichen Tischrechner und der ersten PCs.  <br> 
+	Für das System 2200 und dessen Nachfolger gab es eine Unzahl von Peripheriegeräte und Programmbeispielen, welche jeweils den Bedürfnissen der kommerziellen und vor allem der wissenschaftlichen Anwendung gerecht wurden. Eine Maschinensprache bzw. einen Assembler gab es nicht. Der Rechner arbeitet unmittelbar in der einfach erlernbaren Programmiersprache <b>BASIC</b>.<br>
 	
     Einmalig in Deutschland ist diese funktionsfähige Computer-Anlage mit so vielen peripheren Geräten: Lochstreifenleser (2203), Stapelkartenleser (2234), Markierungskartenleser (2214), 8-Zoll dreifach Diskettenlaufwerk (2270-3) und einem Plattensystem (2230-1) mit 38cm großen Scheiben. Das Gewicht des Plattensystems (eine Festplatte und eine Wechselplatte) beträgt ca. 100kg und der Preis damals stolze 24.000,- DM. Damit konnte man immerhin bereits komfortable 5 MB Daten/Programme speichern.</p>
 	
-	<h3 id="wang2200s"><b>WANG 2200 S</b></h3>
+	<h3 id="wang2200s"><b>WANG 2200S</b></h3>
 	
 	<div class="box left"> 
 	<img src="/shared/photos/rechnertechnik/wang-2200s.jpg" width="350"  height="304" />
@@ -31,11 +31,10 @@
 	<img src="/shared/photos/rechnertechnik/wang-2200s-detail.jpg" width="429"  height="304" />
 	</div>
 	
-	<p>Der Neuzugang (12.2017) zeigt die etwas verkleinerte Ausführung des 2200 aus dem Jahre 1974/75. "S" steht wohl für "small". Das zusätzliche Kassettenlaufwerk 2217 hat eine Kapazität von 1,7 KB pro Meter Band und lässt einen Transfer von 326 Zeichen/Sekunde zu. Es ist mit drei Motoren ausgestattet und kann in beide Laufrichtungen lesen/schreiben. Dabei werden immer Blöcke mit 256 Bytes gelesen/geschrieben. Dieses langsame Speichermedium war damals halt konkurrenzlos preiswert.<br>
-	Interessant ist auch die dazugehörige Kugelkopfmaschine (2202) als Ausgabegerät. Ausgestattet mit Schrittschaltmotoren kann die Maschine auch plotten. Das Modell ist identisch mit dem des <a class="go" href="/de/rechnertechnik/ic-technik.php#wang700" target="_blank">WANG 700 Rechners.</a></p>
-	<br>
-	
-	
+	<p>Der Neuzugang (12.2017) zeigt die etwas verkleinerte Ausführung des 2200 aus dem Jahre 1974/75. "S" steht wohl für "small". Mit dem zusätzlichen Kassettenlaufwerk 2217 lassen sich pro Meter Magnetband 1,7 KB speichern bei einem Transfer von 326 Zeichen/Sekunde. Es ist mit drei Motoren ausgestattet und kann in beide Laufrichtungen lesen/schreiben. Dabei werden immer Blöcke mit 256 Bytes gelesen/geschrieben. Dieses langsame Speichermedium war damals halt konkurrenzlos preiswert.<br>
+	Interessant ist auch die dazugehörige Kugelkopfmaschine als Ausgabegerät. Ausgestattet mit Schrittschaltmotoren kann die Maschine auch plotten. Das Modell 2202 ist identisch mit dem des am <a class="go" href="/de/rechnertechnik/ic-technik.php#wang700" target="_blank">WANG 700 Rechner</a> angeschlossenen Modells "702".</p>
+	 <p class="small">Wir bedanken uns bei der Westfälischen Wilhelms-Universität Münster für die Überlassung der Anlage.</p> 
+	 <br>
 	
 	Fortsetzung folgt.....
     

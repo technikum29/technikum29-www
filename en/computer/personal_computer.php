95b08e071c4721199e502f7b9c9dcdb6a5368ff0
heribert <heribert@svn-users.technikum29.de>
2017-12-28 18:00:16 +0000

    Neuen Ordner für die englische Seite hoch geladen. Ich kann ihn aber nicht öffnen, da er nicht in der Navigation erscheint :-( Die restliche Übersetzung folgt demnächst.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1405 388e7c26-a98e-49f6-8d48-46b9a15b27c2

---
+++ en/computer/personal_computer.php
@@ -0,0 +1,100 @@
+<?php
+	$seiten_id = 'programmierbare';
+	$version = '$Id: programmable.php 914 2016-03-12 20:54:26Z heribert $';
+	$title = 'Exemplary programmable second-generation calculators';
+	
+	require "../../lib/technikum29.php";
+?>
+   <header class="teaser seitenstart">
+	<img src="/shared/photos/rechnertechnik/header-wang2200.jpg" width="940"  height="545">
+	<h2>Historical Personal Computers</h2>
+</header>
+
+<h3 id="wang2200"><b>WANG 2200B</b> with an extensive set of peripherals</h3>
+
+<p>An often posed question is that of how to define the notion of a "personal
+computer". We would answer like this: "A personal computer is a single-user
+system with attached display (back then typically a CRT) and a suitable 
+complement of peripherals to store and retrieve data and for input/output of
+data and programs. It is also easily transportable."
+
+One of the first personal computers, PC for short, is the WANG 2200 A/B dating
+back to 1973. Wang realized pretty early that a key component for such a device
+was an electronic display capable of displaying multiple lines at once.
+Comparable HP systems from that time only had a single line LED-display. A nice
+comparison between these two types of machines can be found here:
+<a href="http://www.wang2200.org/docs/internal/HP_Competitive_Analysis_9830_vs_Wang_2200B_Mar_1974.pdf" target="_blank"> HP competitive Analysis 9830 vs WANG 2200B (PDF)</a> 
+This is, in fact, a comparison performed by HP and was suited to aid the
+salesperson when he or she faced a situation where HP had to compete agains a
+Wang offer. HP and WANG were THE opponents in these days when it came to
+scientific calculators and early PCs.  <br> 
+	The system 2200 and its successors featured a plethora of peripheral devices as
+well as a vast library of programs for a variety of scientific and commercial
+problems. Though, there was no assembler or access to machine language at all.
+The system was programmed in a BASIC dialect.<br>
+	
+  The system on display here is unique in Germany with its set of peripheral
+devices: Paper tape reader (2203), card reader (2234), marker card reader
+(2214), triple 8 inch diskette drive (2270.3) and a disk subsystem (2230-1)
+with 14 inch disks. The weight of the disk subsystem alone is about 100 kg, and
+it cost a whopping 24 000 DM back in its days. Its total capacity is 5 MB for
+programs and data.</p>
+	
+	<h3 id="wang2200s"><b>WANG 2200S</b></h3>
+	
+	<div class="box left"> 
+	<img src="/shared/photos/rechnertechnik/wang-2200s.jpg" width="350"  height="304" />
+	</div>
+	<div class="box right clear-after"> 
+	<img src="/shared/photos/rechnertechnik/wang-2200s-detail.jpg" width="429"  height="304" />
+	</div>
+	
+	<p>This new acquisition (December 2017) is a smaller version of the WANG 2200B,
+built in 1974/75, the "S" probably denoting "small". The additional magnetic
+tape unit (2217) allows to store 1.7 kB of data on one meter of tape at a
+transfer rate of 326 characters/second. It is equipped with three independent
+motors and capable of reading/writing in both directions. The tape is always
+written/read in blocks of 256 bytes. As slow as it is, the low price of the
+storage media more than compensated for that. <br>
+
+Also interesting is the golf-ball typewriter as output device. Thanks to the
+unique stepper motor driven mechanics, the system can also be used to plot
+graphs.  The model 2202 is identical with the model 702 typically used in
+conjunction with a WANG 700 system.</p>
+
+	 <p class="small">We would like to express our thanks to the "Westfaelische Wilhelms-Universitaet
+Muenster" who donated this system.</p> 
+	
+	 
+	 <h3 id="cbm"><b>Commodore (CBM) PET 2001, 8096-SK</b></h3><br>
+	 
+	 ......excuse, further translation will follow shortly....<br><br>
+	 
+	 Auch wenn man über Commodore PCs viele Informationen im Netz findet, haben wir einige Geräte aus unserem Bestand dennoch aufgenommen.<br>
+	 Es war im Jahre 1977 als die meisten Kids an Schulen schon von dem neuen Medium "Computer" hörten und recht gierig darauf warteten, hier endlich loslegen zu können. 
+	 <div class="box left"> 
+	<img src="/shared/photos/rechnertechnik/ur-pet.jpg" width="300"  height="266" />
+	</div>
+	 <div class="box right"> 
+	<img src="/shared/photos/rechnertechnik/pet+floppy.jpg" width="500"  height="266" />
+	</div>
+	 Der PET 2001 wurde damals von Commodore für ganze 2000 DM angeboten und war damit auch für Schulen finanzierbar. Blitzschnell entstanden dort "Computerräume" die so stark belegt waren, dass man sich Tage vorher in Listen eintragen musste oder alternativ nur zu unüblichen Zeiten (nach 18 Uhr) Einlass bekam.<br><br>
+	 
+	 Mit anfangs ca. 7KB RAM und der sehr langsam arbeitenden "Datasette", im Prinzip ein simpler Kassettenrecorder der 70er Jahre, war das Programmieren in BASIC zwar etwas mühsam aber immerhin möglich. 
+	 <br>Schon zu Beginn war klar: Der Computer ist auch zum Spielen geeignet. In unseren Geburtstags-Workshops kommen sogar die alten Computerspiele der Anfangszeit gut an. Die bescheidenen Möglichkeiten faszinieren die hightec-Kids erstaunlicherweise. Hier spritzt kein Blut und die Distanz zur Realität ist noch riesig groß.<br>
+	 
+	 Im nächsten Modell wurde die "Micky-Maus-Tastatur", gerade gut für Kinderhände, durch eine ordentliche Tastatur ersetzt. Wer genügend Geld hatte investierte dies in ein 5,25" Doppelfloppy-Laufwerk das genauso teuer wie der PC selbst war. Hiermit konnte man jedoch endlich "vernünftig" arbeiten, so dass die Anlage auch für Kleinbetriebe interessant wurde.
+	 <div class="box left"> 
+	<img src="/shared/photos/rechnertechnik/commodore-8096.jpg" width="300"  height="276" />
+	</div>
+	 Im folgenden Entwicklungsschritt war u.a. das Design an der Reihe. Computer sollten nun auch elegant und "schön" sein, wie das Modell 8096-SK aus dem Jahre 1980, auch "Ei" genannt, zeigt. "80" steht für 80 Zeichen pro Zeile, "96" für 96 KB RAM und SK für "Separate Keyboard", die Tastatur ist abnehmbar. Der Bildschirm lässt sich kippen sowie drehen und der PC kann große und kleine Buchstaben generieren. Mit angeschlossenem Nadeldrucker hatte man so einen "richtigen" Arbeitsplatz.<br>
+	 Hilfreich waren spezielle Software-Pakete, welche die einfache Anwendung auch für relative Laien ermöglichte.
+	 Hier ein paar technische Daten:<br>
+	 Prozessor: MOS 6502<br>
+	 Taktfrequenz: 1 MHz<br>
+	 RAM: 96 KByte, ROM: 18 KByte<br>
+	
+	 Doch jetzt beginnt die Massenproduktion von PCs, weshalb wir auf die weitere Entwicklung hier nicht eingehen. Wir möchten unsere Besucher nur mit seltenen Raritäten der Computer-Frühzeit beglücken.
+	 
+	
+    

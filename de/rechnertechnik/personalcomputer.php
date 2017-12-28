04938ca076e3cdf72cceae238dc960c56e79d774
heribert <heribert@svn-users.technikum29.de>
2017-12-28 17:13:36 +0000

    Personalcomputer aufgestockt. Einige Formatierungen sind noch verbesserungsbedürftig.
    git-svn-id: https://www.technikum29.de/svn/technikum29-www@1404 388e7c26-a98e-49f6-8d48-46b9a15b27c2

--- de/rechnertechnik/personalcomputer.php
+++ de/rechnertechnik/personalcomputer.php
@@ -17,7 +17,7 @@
 
 <p>"Wie definiert man einen Personal-Computer?", eine oft gestellte Frage, die wir so beantworten würden:<br>
 	Ein Personal-Computer (PC) ist ein Einzelplatzsystem mit einem Datensichtgerät (damals ein Röhren-Monitor) sowie geeigneter Peripherie zum Speichern und zur Ausgabe von Daten/Programmen. Es ist mühelos transportabel.<br>
-	Einer der allerersten PCs ist der WANG 2200 A/B aus dem Jahre 1973. WANG hat sehr früh erkannt, dass nur ein mehrzeiliger Monitor zukunftweisend ist. Zu dieser Zeit arbeiteten die HP-Rechner noch mit einem einzeiligen LED-Display. Hier gibt es einen schönen <a href="http://www.wang2200.org/docs/internal/HP_Competitive_Analysis_9830_vs_Wang_2200B_Mar_1974.pdf" target="_blank"> Vergleich zwischen HP 9830 WANG 2200 (PDF)</a> als Verkaufsargumente für HP-Händler aus dem Jahre 1974, 15 Seiten in engl. Sprache. WANG und HP waren <u>die</u> Konkurrenten der 70er Jahre im Bereich der wissenschaftlichen Tischrechner und der ersten PCs.  <br> 
+	Einer der allerersten PCs ist der WANG 2200 A/B aus dem Jahre 1973. WANG hat sehr früh erkannt, dass nur ein mehrzeiliger Monitor zukunftsweisend ist. Zu dieser Zeit arbeiteten die HP-Rechner noch mit einem einzeiligen LED-Display. Hier gibt es einen schönen <a href="http://www.wang2200.org/docs/internal/HP_Competitive_Analysis_9830_vs_Wang_2200B_Mar_1974.pdf" target="_blank"> Vergleich zwischen HP 9830 WANG 2200 (PDF)</a> als Verkaufsargumente für HP-Händler aus dem Jahre 1974, 15 Seiten in engl. Sprache. WANG und HP waren <u>die</u> Konkurrenten der 70er Jahre im Bereich der wissenschaftlichen Tischrechner und der ersten PCs.  <br> 
 	Für das System 2200 und dessen Nachfolger gab es eine Unzahl von Peripheriegeräte und Programmbeispielen, welche jeweils den Bedürfnissen der kommerziellen und vor allem der wissenschaftlichen Anwendung gerecht wurden. Eine Maschinensprache bzw. einen Assembler gab es nicht. Der Rechner arbeitet unmittelbar in der einfach erlernbaren Programmiersprache <b>BASIC</b>.<br>
 	
     Einmalig in Deutschland ist diese funktionsfähige Computer-Anlage mit so vielen peripheren Geräten: Lochstreifenleser (2203), Stapelkartenleser (2234), Markierungskartenleser (2214), 8-Zoll dreifach Diskettenlaufwerk (2270-3) und einem Plattensystem (2230-1) mit 38cm großen Scheiben. Das Gewicht des Plattensystems (eine Festplatte und eine Wechselplatte) beträgt ca. 100kg und der Preis damals stolze 24.000,- DM. Damit konnte man immerhin bereits komfortable 5 MB Daten/Programme speichern.</p>
@@ -34,7 +34,35 @@
 	<p>Der Neuzugang (12.2017) zeigt die etwas verkleinerte Ausführung (Bauj. 1974/75) des 2200B, dabei steht "S" wohl für "small". Mit dem zusätzlichen Kassettenlaufwerk 2217 lassen sich pro Meter Magnetband 1,7 KB speichern bei einem Transfer von 326 Zeichen/Sekunde. Es ist mit drei Motoren ausgestattet und kann in beide Laufrichtungen lesen/schreiben. Dabei werden immer Blöcke mit 256 Bytes gelesen/geschrieben. Dieses langsame Speichermedium war damals halt konkurrenzlos preiswert.<br>
 	Interessant ist auch die dazugehörige Kugelkopfmaschine als Ausgabegerät. Ausgestattet mit Schrittschaltmotoren kann die Maschine auch plotten. Das Modell 2202 ist identisch mit dem des am <a class="go" href="/de/rechnertechnik/ic-technik.php#wang700" target="_blank">WANG 700 Rechner</a> angeschlossenen Modells "702".</p>
 	 <p class="small">Wir bedanken uns bei der Westfälischen "Wilhelm-Universität Münster" für die Überlassung der Anlage.</p> 
-	 <br>
 	
-	Fortsetzung folgt.....
+	 
+	 <h3 id="cbm"><b>Commodore (CBM) PET 2001, 8096-SK u.a.</b></h3><br>
+	 
+	 Auch wenn man über Commodore PCs viele Informationen im Netz findet, haben wir einige Geräte aus unserem Bestand dennoch aufgenommen.<br>
+	 Es war im Jahre 1977 als die meisten Kids an Schulen schon von dem neuen Medium "Computer" hörten und recht gierig darauf warteten, hier endlich loslegen zu können. 
+	 <div class="box left"> 
+	<img src="/shared/photos/rechnertechnik/ur-pet.jpg" width="300"  height="266" />
+	</div>
+	 <div class="box right"> 
+	<img src="/shared/photos/rechnertechnik/pet+floppy.jpg" width="500"  height="266" />
+	</div>
+	 Der PET 2001 wurde damals von Commodore für ganze 2000 DM angeboten und war damit auch für Schulen finanzierbar. Blitzschnell entstanden dort "Computerräume" die so stark belegt waren, dass man sich Tage vorher in Listen eintragen musste oder alternativ nur zu unüblichen Zeiten (nach 18 Uhr) Einlass bekam.<br><br>
+	 
+	 Mit anfangs ca. 7KB RAM und der sehr langsam arbeitenden "Datasette", im Prinzip ein simpler Kassettenrecorder der 70er Jahre, war das Programmieren in BASIC zwar etwas mühsam aber immerhin möglich. 
+	 <br>Schon zu Beginn war klar: Der Computer ist auch zum Spielen geeignet. In unseren Geburtstags-Workshops kommen sogar die alten Computerspiele der Anfangszeit gut an. Die bescheidenen Möglichkeiten faszinieren die hightec-Kids erstaunlicherweise. Hier spritzt kein Blut und die Distanz zur Realität ist noch riesig groß.<br>
+	 
+	 Im nächsten Modell wurde die "Micky-Maus-Tastatur", gerade gut für Kinderhände, durch eine ordentliche Tastatur ersetzt. Wer genügend Geld hatte investierte dies in ein 5,25" Doppelfloppy-Laufwerk das genauso teuer wie der PC selbst war. Hiermit konnte man jedoch endlich "vernünftig" arbeiten, so dass die Anlage auch für Kleinbetriebe interessant wurde.
+	 <div class="box left"> 
+	<img src="/shared/photos/rechnertechnik/commodore-8096.jpg" width="300"  height="276" />
+	</div>
+	 Im folgenden Entwicklungsschritt war u.a. das Design an der Reihe. Computer sollten nun auch elegant und "schön" sein, wie das Modell 8096-SK aus dem Jahre 1980, auch "Ei" genannt, zeigt. "80" steht für 80 Zeichen pro Zeile, "96" für 96 KB RAM und SK für "Separate Keyboard", die Tastatur ist abnehmbar. Der Bildschirm lässt sich kippen sowie drehen und der PC kann große und kleine Buchstaben generieren. Mit angeschlossenem Nadeldrucker hatte man so einen "richtigen" Arbeitsplatz.<br>
+	 Hilfreich waren spezielle Software-Pakete, welche die einfache Anwendung auch für relative Laien ermöglichte.
+	 Hier ein paar technische Daten:<br>
+	 Prozessor: MOS 6502<br>
+	 Taktfrequenz: 1 MHz<br>
+	 RAM: 96 KByte, ROM: 18 KByte<br>
+	
+	 Doch jetzt beginnt die Massenproduktion von PCs, weshalb wir auf die weitere Entwicklung hier nicht eingehen. Wir möchten unsere Besucher nur mit seltenen Raritäten der Computer-Frühzeit beglücken.
+	 
+	
     

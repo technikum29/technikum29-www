ac6a6998ed838c955f0b4a39343951edb3727ab6
Sven <sven@timothy.local>
2019-08-15 14:05:58 +0200

    Blog: Veröffentlichung des im Februar geschriebenen Artikels über Organisationsformen.
    War nie so ganz ausformuliert, will ich aber jetzt auch nicht auf der Festplatte verrotten lassen.

---
+++ blog/2019-02-21-Organisationsformen.php
@@ -0,0 +1,74 @@
+<?php
+	$blog_title = "Organisationsformen";
+	$blog_author = "Sven";
+	$blog_date = "2019-02-21";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+Wie in unserem letzten Blogbeitrag 
+<a href="2019-02-04-the-heritage.php" class="go">The Heritage</a> erläutert, steht
+das Museum vor einer oragnisatorischen Herausforderung. Hier listen wir kurz auf,
+welche Aspekte dabei zum Tragen kommen. Diese öffentliche Darstellung ist eine
+Kurzform von Vorbereitungen, wie wir sie uns intern überlegen.
+
+<h3>Anforderungen an eine Organisation</h3>
+<p>Eine zu gründende Organisationsform (im Sinne von "juristischer Person" o.ä.)
+müsste wahrscheinlich folgene Eigenschaften aufweisen:
+
+<ul>
+  <li>Eigentümerschaft an den Geräten und damit einhergehend Kuration der Ausstellung
+    und Gestaltung des Depots &ndash; dazu gehört das Abstoßen (idealerweise
+    in Form eines Verkaufes) von Geräten sowie die Neueinwerbung (in der Regel durch
+    Schenkung).
+  <li>Symbolischer Mieter der Räumlichkeiten, dadurch langfristige Sicherheit für ein
+    grundsätzliches Bestehen. Andernfalls besteht keinerlei Gewissheit, dass die
+    Sammlung am nächsten Tag ein Dach über dem Kopf hat.
+  <li>Gemeinnützigkeit: Dazu gehört das grundsätzliche Interesse, das Museumsthema ohne
+    Profitinteresse für die Öffentlichkeit aufzuarbeiten und die Bereitschaft,
+    Interessenten an der Museumsarbeit mitmachen zu lassen.
+  <li>Gemeinsames Treffen von Entscheidungen. Das Zusammentragen des Wissens von Vielen
+    soll Relevanz und richtige Entscheidungen sicherstellen. Demokratische Strukturen
+    sind essentiell, um die Bereitschaft zum Mitmachen hochzuhalten.
+</ul>
+
+<h3>Finanzieller Rahmen</h3>
+<ul>
+  <li>Es gibt leider keine finanzielle Basis. Dennoch können geringfügige Ausgaben 
+    durch Mitgliederbeiträge oder Spenden gedeckt werden.
+  <li>Die Räumlichkeiten werden hoffentlich auch weiterhin gestellt, sodass
+    deren Unterhalt (etwa Strom-, Wasser-, Heiz-, Instandhaltungskosten) nicht bezahlt
+    werden muss. Konkret ist die Erbengemeinschaft im Begriff, eine Absichtserklärung
+    zu formulieren und zumindest für einige Jahre den Status Quo beizubehalten.
+</ul>
+
+<h3>Mögliche Organisationsformen</h3>
+<ul>
+  <li>Gemeinnützige Stiftung. Hat den Vorteil, dass sie eine juristische Person ist
+    und ohne Weiteres Inhaber der Sammlung sein kann. Das Wort "Stiftung" kann auch
+    allgemein als Prädikat in der Museen- und Mäzenenlandschaft gelten und hilfreich
+    bei Bewerbungen auf staatliche Förderungen sein. Organe der Stiftung sind etwa der
+    Vorstand (Aktivie) und das Kuratorium. Stiftungen können auch mit geringem
+    Kapital gegründet werden, so werden etwa 
+    <a href="https://de.wikipedia.org/wiki/B%C3%BCrgerstiftung">Bürgerstiftungen</a>
+    auch mit wenigen Zehntausend Euro Mindestkapital gegründet.
+  <li>Gemeinnütziger Verein. Hat den Vorteil, dass hierin organisiert eine Gruppe von Menschen
+    das "Tagesgeschäft" machen kann, also Führungen, Reperaturen, Katalogisierung,
+    Website, An-/Verkäufe, Fundraising, usw. Ist aber leider keine juristische Person
+    und genießt meines Erachtens nicht das Ansehen einer Stiftung. Hier gibt es als
+    Organe etwa den Vorstand und die Mitgliederversammlung.
+  <li>Gemeinnütziger Förderverein, der z.B. eine gemeinnützige Stiftung unterstützt.
+      Eine Ausgestaltungsmöglichkeit ist hier ein frei wählbarer regelmäßiger Mitgliedsbeitrag,
+      der eine Mindestsumme nicht unterschreiten soll, z.B. 500€ pro Jahr. Zum "Freikaufen"
+      könnte es dann die Möglichkeit geben, als Ersatz eine gewisse Anzahl an Stunden
+      Dienstleistungen für den Verein zu erbringen (z.B. 25 Stunden pro Jahr).
+</ul>
+
+<p>Derzeit ist am wahrscheinlichsten, dass ein gemeinnütziger Verein gegrünet wird.
+
+
+<?php print_author_info(); ?>
+
+
+

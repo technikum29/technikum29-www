e483ecfc1ce0b9082107eb7bef7e34793f8ce71d
Sven <sven@timothy.local>
2019-10-31 17:44:40 +0100

    Neue Seite: FAQ (testweise)

---
+++ de/faq.php
@@ -0,0 +1,33 @@
+<?php
+	$seiten_id = 'faq';
+	$version = '$Id$';
+	$titel = 'FAQ';
+	
+	require "../lib/technikum29.php";
+?>
+
+<h2>FAQ: Frequently Asked Questions</h2>
+<p>Wir bekommen derzeit häufig ähnliche Fragen gestellt, die wir hier veröffentlichen
+und auch gleich beantworten möchten.
+
+
+<dl class="faq">
+
+<dt>
+Ich habe Interesse an einer Führung (Besichtigung).
+<dd>
+Wir befinden uns derzeit in einer Neugründungsphase und haben daher weder regelmäßige
+Öffnungszeiten, noch bieten wir Führungen an. In Einzelfällen führen wir derzeit in
+den Museumsräumen Gespräche mit Menschen, die beim technikum29 mitmachen möchten.
+
+
+<dt>
+Ich habe etwas, was ich dem <a href="/">technikum29</a>
+   verkaufen oder verschenken möchte.
+<dd>
+Wir befinden uns derzeit in einer Neugründungsphase und können daher im Allgemeinen
+   nichts annehmen.
+
+</dl>
+
+<em>Wird ergänzt...</em>

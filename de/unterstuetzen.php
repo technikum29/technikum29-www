e87ab6b98b23cf8d6089af82dd19f803b88b03bf
Sven <sven@timothy.local>
2019-11-19 22:53:33 +0100

    Adding a fundraising page.
    Well this is something I wanted to wait until we have the nonprofit organisation, but whatever. People want to donate money :-D

---
+++ de/unterstuetzen.php
@@ -0,0 +1,70 @@
+<?php
+	$seiten_id = 'support';
+	$version = '$Id$';
+	$titel = 'Das technikum29 unterstützen';
+	
+	require "../lib/technikum29.php";
+?>
+
+<h2>Wir suchen Unterstützer</h2>
+
+<p>Das technikum29 befindet sich derzeit in der Neugründung, die mit der
+Gründung eines gemeinnützigen eingetragenen Vereins einen vorläufigen
+Höhepunkt erreichen soll. Wir suchen Unterstützer jeder Art: Ob persönlich
+vor Ort, über das Netz, ideell oder auch finanziell.
+
+<h3>Persönlich aushelfen: Mitarbeit</h3>
+<p>Wer Zeit zur Verfügung hat und im Rhein-Main-Gebiet wohnt, kann schnell
+in den Museumsräumen sein und hat im Museum eine Vielzahl an Tätigkeiten,
+die bewältigt werden müssen: Neben Restauration und Publikumsbetrieb (Führungen)
+haben wir immer noch eine Inventarisierung, Aufarbeitung, Veräußerungen
+und Abstoßungen von Sammlungsteilen, aber auch die Instandhaltung der Räume,
+und vieles mehr.
+
+<p>Wer nicht vor Ort wohnt, kann auf unserer umfassenden Online-Präsenz aktiv
+werden: Wir haben eine große Homepage und
+<a href="https://github.com/technikum29">viel Code</a>, der entwickelt werden
+will. Von entfernt lassen sich auch organisatorische oder rechtliche Dinge
+erledigen, auch Verkäufe, Öffentlichkeitsarbeit oder E-Mails gehen von überall.
+
+<p>Wir führen derzeit eine Liste von Interessenten; wenn Sie sich für die
+Museumsthemen oder die Idee des technikum29 begeistern können, dann
+<a href="/de/kontakt.php">schreiben Sie uns</a>.
+
+<h3>Finanziell helfen: Spende</h3>
+<p>Das technikum29 bewohnt derzeit eigene Räume, die aber trotzdem unterhalten
+werden müssen. Pro Monat fallen alleine mehrere Hundert Euro Nebenkosten (Strom, Heizung)
+an, von Umlagen ganz abgesehen; auf eine kostendeckende Kaltmiete verzichten die Eigentümer
+derzeit noch komplett. Auch entstehen Kosten für Material, Öffentlichkeitsarbeit
+oder Museumsbetrieb. Unsere Mitstreiter arbeiten hingegen ehrenamtlich und zahlen
+auch Fahrtkosten zum Museum oder zu anderen Orten aus eigener Tasche.
+
+<p>Da wir bislang bereits oft gefragt wurden, ob man uns etwas <strong>spenden</strong>
+kann, haben wir vorab ein Konto eingerichtet. Wir empfangen Zahlungen bei derzeit
+auf folgendem Bankkonto:
+
+<blockquote>
+   IBAN: <tt>DE82 5001 0517 5430 0710 38</tt>
+   <br>BIC: <tt>INGDDEFFXXX</tt>
+   <br>Kreditinstitut: ING-Diba
+</blockquote>
+
+Außerdem kann man uns <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=kontakt%40technikum29.de&item_name=Spende+zum+Museumsbetrieb&currency_code=EUR&source=url">auf unser Paypal-Konto Geld spenden</a>:
+
+<p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_donations" />
+<input type="hidden" name="business" value="kontakt@technikum29.de" />
+<input type="hidden" name="item_name" value="Spende zum Museumsbetrieb" />
+<input type="hidden" name="currency_code" value="EUR" />
+<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Spenden mit dem PayPal-Button" />
+<img alt="" border="0" src="https://www.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
+</form>
+
+
+
+
+
+
+
+

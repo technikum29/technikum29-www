bff15157145d603e871129257fb03c76dcc885ab
RolandLangfeld <54443215+RolandLangfeld@users.noreply.github.com>
2019-12-05 12:32:20 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -77,7 +77,48 @@ Januar 2020 wieder Führungen an. Die Liste der Termine wird ständig ergänzt.
 		
 	</dl>
 </div><!--/.termin -->
+
+<div class="box termin clear-after">		
+	<p class="date left"><em>2020</em> 25. Jan.</p>
+	<h4>Führung "Computergeschichte"</h4> 
+	<p>In dieser Führung wird die Computertechnik des Museums vorgestellt.
+
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 15. Februar 2020
+		<dt>Uhrzeit
+		<dd>14:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dt>Eintritt
+		<dd>8€ pro Person
+		<dt>
+		<dd>noch 15 freie Plätze
+		<dt>Anmeldung
+		<dd class="anmelden"><a class="button anmeldung-btn" href="#">Bitte zum Termin anmelden</a>
+	</dl>
+</div><!--/.termin -->
 	
+<div class="box termin clear-after">		
+	<p class="date left"><em>2020</em> 25. Jan.</p>
+	<h4>Führung "Computergeschichte"</h4> 
+	<p>In dieser Führung wird die Computertechnik des Museums vorgestellt.
+
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Sonntag, 16. Februar 2020
+		<dt>Uhrzeit
+		<dd>14:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dt>Eintritt
+		<dd>8€ pro Person
+		<dt>
+		<dd>noch 15 freie Plätze
+		<dt>Anmeldung
+		<dd class="anmelden"><a class="button anmeldung-btn" href="#">Bitte zum Termin anmelden</a>
+	</dl>
+</div><!--/.termin -->
 	
 <div class="box termin clear-after">		
 	<p class="date left"><em>2020</em> 25. Feb.</p>

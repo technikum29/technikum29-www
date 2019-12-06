1a9e6685e7a726ee01e4ddc49340bb059c5ff9ec
Sven Sveto <sven@sveto>
2019-12-06 15:57:15 +0100

    Transparenzbericht hinzugefÃgt.
    (noch nicht verlinkt/eingebunden).

---
+++ de/transparenz.php
@@ -0,0 +1,126 @@
+<?php
+	$seiten_id = 'transparenz';
+	$titel = "Transparenz und Mittelherkunft";
+	require "../lib/team.php";
+?>
+
+<h2>Transparenz: Struktur und Finanzierung des Museums</h2>
+<p>Auf dieser Seite legen wir die Struktur und Finanzierung des technikum29-Computermuseums
+offen. Es handelt sich um eine Auflistung im Sinne der
+<a href="https://www.transparency.de/mitmachen/initiative-transparente-zivilgesellschaft/">Initiative transparente Zivilgesellschaft</a>. Wir hoffen
+durch die Transparenz <a href="/de/unterstuetzen.php">UnterstützerInnen</a> zu finden.
+  
+  
+<h3>1. Name, Sitz, Anschrift und Gründungsjahr</h3>
+<p>Wir sind das <strong>technikum Computermuseum</strong> (eigentlich <em>technikum29</em>),
+mit Sitz in Kelkheim/Taunus, die genaue Anschrift steht in unserem <a href="/de/impressum.php">Impressum</a>.
+Das Museum wurde unter diesem Namen 2005 privat gegründet.
+
+<h3>2. Vollständige Satzung sowie Angaben zu den Organisationszielen</h3>
+<p>Derzeit sind wir immernoch ein <em>privat</em> geführtes Museum. Es gibt aber bereits einen Satzungsentwurf
+für einen gemeinnützigen Verein, der gegründet werden soll; außerdem Kandidaten für die Vorstandspositionen.
+In diesem Sinn ist das Museum möglicherweise im Zustand eines <em>Vorvereins</em>.
+
+<p>Das Organisationziel liest sich in der Präambel so:
+
+<blockquote>
+Der technikum-Verein hat das Ziel, den Stand der Rechen- und Computertechnik des letzten Jahrhunderts
+mit Schwerpunkt von den 30er bis 70er Jahren (Sammlungsgebiet) durch Erhalt und Pflege von Exponaten
+der Technikgeschichte zu dokumentieren und ein breites Publikum durch Vorführungen und Vorträge mit
+den Wurzeln unserer heutigen digitalen Welt bekannt zu machen.
+<p>
+Kernaufgaben des Vereins ist der Betrieb eines Museums, darunter fallen u.a. die Inventarisierung, die
+Reparatur und Instandhaltung der Exponate, die Präsentation der Sammlung durch Vorführungen und
+Veröffentlichungen sowie die Unterstützung von Kunst und Kultur auf dem Sammlungsgebiet in Deutschland
+u.a. durch Vorträge und zeitweiliger Präsentation anderer Sammlungen (Wanderausstellungen).
+<p>
+Der Verein wird hierzu in angemieteten Räumlichkeiten sowohl Leihgaben als auch eigene Exponate
+beherbergen. Die Sammlung des 2018 verstorbenen Gründers des Privatmuseums „technikum29“ wird von
+dessen Erben als Dauerleihgabe in den Verein übertragen.
+</blockquote>
+
+<p>Der Vereinszweck wird wie folgt definiert:
+
+<blockquote>
+Zweck des Vereins ist die Förderung von Kunst, Kultur, Wissenschaft und Forschung auf dem o.g.
+Sammlungsgebiet im Rahmen der in der Präambel abgesteckten Zielsetzung.
+
+<p>Der Vereinszweck wird insbesondere verwirklicht durch
+<ul>
+<li>Dokumentation der Sammlung durch Inventarisierung.
+<li>Erweiterung der Sammlung durch Anwerbung von Leihgaben und Ankauf.
+<li>Präsentation der Sammlung durch Vorführungen und Veröffentlichungen.
+<li>Öffentlichkeitsarbeit auf dem Sammlungsgebiet, z.B. durch eigene und eingeladene Vorträge
+und Präsentation von Sammlungen Dritter (Wanderausstellungen).
+<li>Unterstützung der Technikwissenschaften auf dem Sammlungsgebiet, z.B. durch Verleih von
+Exponaten, Austausch von Dokumenten und wissenschaftliche Veröffentlichungen.
+</ul>
+</blockquote>
+
+<h3>3. Angaben zur Steuerbegünstigung</h3>
+<p>Das Museum ist derzeit in Privathand. Deswegen können wir <strong>keine</strong>
+steuerbegünstigten Spendenquittungen ausstellen.
+
+Die Museumsräume genießen gemäß Beschluss des hessischen Ministeriums für Wissenschaft und Kunst
+von 2005 eine Grundsteuerreduktion (ca. 100 EUR weniger im Quartal).
+
+<!--Der zu gründende Verein wird ausschließlich und unmittelbar gemeinnützige Zwecke verfolgen
+(im Sinne der Abgabenordnung).-->
+
+<h3>4. Name und Funktion wesentlicher Entscheidungsträger</h3>
+Hier haben wir auf <a href="/de/team.php">unserer Teamseite</a> viele Mitarbeiter aufgelistet.
+Wesentliche Entscheidungsträger sind derzeit Roland Langfeld und Sven Köppel.unterstützt
+
+<h3>5. Tätigkeitsbericht</h3>
+<p>Im Jahr 2019 haben wir intensiv an der Aufarbeitung gearbeitet. Viele unserer Tätigkeiten
+<a href="/blog">sind im Blog beschrieben</a>.
+
+<h3>6. Personalstruktur</h3>
+<p>Unsere Helfer arbeiten alle Ehrenamtlich. Viele Mitarbeiter kommen von weit her.
+Derzeit besitzen nur wenige Personen eine Schließberechtigung für die Räumlichkeiten, daher
+muss man sich zum Mitmachen immer an uns wenden.
+<br>Die Erbengemeinschaft Heribert Müller unterstützt das Museum durch <em>Duldung</em>, also
+Verfügungstellung von Räumen.
+
+<h3>7. Angaben zur Mittelherkunft</h3>
+<p>Derzeit finanziert sich das Museum auf folgende Weise:
+<ul>
+  <li>Geldspenden: Wir haben bislang Zuwendungen in Höhe von ca. 300 EUR erhalten.
+  <li>Sachspenden: Wir haben verschiedene Geräte erhalten (siehe Blog).
+  <li>Verkäufe: Wir haben restaurierte Geräte aus Depots an interessierte Sammler abgetreten und
+      dabei bislang Einnahmen von knapp 2.000 EUR erzielt.
+  <li>Miet- und Nebenkostennachlass: Im Jahr 2019 wurden sämtliche Kosten das letzte mal von der
+      Erbengemeinschaft getragen. Ab 2020 soll das Museum zumindest durch den Betrieb enstehende
+      laufende Kosten (Heizung, Strom, Umlagen) begleichen können.
+</ul>
+
+<p>Wir versuchen derzeit, ein erstes Jahresbudget von etwa 6.000 EUR zu erwirtschaften.
+
+<h3>8. Angaben zur Mittelverwendung</h3>
+<p>Die <strong>jährlichen Ausgaben des Museums</strong> erwarten wir für 2020 wie folgt:
+
+<ul>
+  <li>Sachmittel (Putzmittel, Verpflegung bei öffentlichen Veranstaltungen): ca. 2.000 EUR
+  <li>Betriebsmittel für Restaurationstätigkeiten (Werkzeug, Bauteile, usw): ca. 3.000 EUR
+  <li>Nebenkosten (Heizung, Strom, Umlagen) ca. 3.000 EUR
+  <li>Symbolische Kaltmiete für die Museumsräume ab 3.000 EUR
+</ul>
+
+<p>Zur reinen Bewirtung der Museumsräume werden wir jährlich ca. 6.000 EUR aufbringen müssen.
+Für den Museumsbetrieb erwarten wir Kosten um etwa 5.000 EUR pro Jahr. Ohne Mitgliedsbeiträge
+eines Vereines gehen wir davon aus, dass Sach- und Betriebsmittel weiterhin vom Museumsteam
+getragen werden müssen.
+
+<h3>9. Gesellschaftsrechtliche Verbundenheit mit Dritten</h3>
+<p>Das Museum war stets stolz darauf, unabhängig und in eigenen Räumlichkeiten seine
+Ziele zu verfolgen. Es gibt einzelne Sachspenden von Firmen, etwa gesponserte Transporte von BULL,
+die in den letzten 15 Jahren stattgefunden haben und denen <a href="/">auf der Website</a>
+gedankt wird.
+
+<p>Das Museum stellt eine Hand voll Dauerleihgaben vom <a href="https://www.fitg.de/">Förderkreis
+Industrie- und Technikgeschichte e.V.</a> aus, einem gemeinnützigen Verein in Frankfurt am Main.
+
+<h3>10. Namen von Personen, deren jährliche Zahlungen mehr als 10 % des Gesamtjahresbudgets ausmachen</h3>
+<p>Keine
+
+

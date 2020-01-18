25daa1a4efbf24829de4670ef14350ac310b88b6
RolandLangfeld <54443215+RolandLangfeld@users.noreply.github.com>
2020-01-18 09:51:31 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -31,10 +31,8 @@ Januar 2020 wieder Führungen an. Die Liste der Termine wird ständig ergänzt.
 		<dt>Eintritt
 		<dd>8€ pro Person
 		<dt>
-		<dd>nur noch ein freier Platz !!
-		<dt>Anmeldung
-		<dd class="anmelden"><a class="button anmeldung-btn" href="#">Bitte zum Termin anmelden</a>
-		
+		<dd>ausgebucht
+			
 	</dl>
 </div><!--/.termin -->
 

404da9a8edb7966d51ca5b28d77786075a43434a
RolandLangfeld <54443215+RolandLangfeld@users.noreply.github.com>
2020-02-07 13:50:53 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -113,7 +113,7 @@ Januar 2020 wieder Führungen an. Die Liste der Termine wird ständig ergänzt.
 		<dt>Eintritt
 		<dd>8€ pro Person
 		<dt>
-		<dd>noch 4 freie Plätze
+		<dd>nur noch ein freier Platz !
 		<dt>Anmeldung
 		<!--<dd class="anmelden"><a class="button anmeldung-btn" href="#">Bitte zum Termin anmelden</a>-->
 		<dd>Bitte per Mail an <a href="mailto:kontakt@technikum29.de">kontakt@technikum29.de</a>

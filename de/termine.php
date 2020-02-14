78d6609bd09809b4278e4cefa8e3205b313b5f2d
RolandLangfeld <54443215+RolandLangfeld@users.noreply.github.com>
2020-02-14 17:55:04 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -9,8 +9,7 @@
 
 <h2>Kalender und Termine</h2>
 
-<p>Es folgt ist eine Auflistung aktueller und jüngerer ehemaliger Termine im technikum29.
-Durch den <a href="/heribert-mueller/">Verlust von Heribert Müller</a> im April 2018
+<pDurch den <a href="/heribert-mueller/">Verlust von Heribert Müller</a> im April 2018
 kam der öffentliche Museumsbetrieb über ein Jahr lang zum Erliegen. Wir bieten ab
 Januar 2020 wieder Führungen an. Die Liste der Termine wird ständig ergänzt.
 <p>Sie möchten als Gruppe (aus Sicherheitsgründen max 15 Teilnehmer) einen eigenen Termin vereinbaren ? Dann melden sie sich bitte per Mail an <a href="mailto:kontakt@technikum29.de">kontakt@technikum29.de</a>

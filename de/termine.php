2b173b9b0d8def75394a66d55e00506b4991649e
RolandLangfeld <54443215+RolandLangfeld@users.noreply.github.com>
2020-02-22 13:17:28 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -119,6 +119,26 @@ Januar 2020 wieder Führungen an. Die Liste der Termine wird ständig ergänzt.
 		
 	</dl>
 </div><!--/.termin -->
+	
+<div class="box termin clear-after">		
+	<p class="date left"><em>2020</em> 25. April.</p>
+	<h4>Führung "Computergeschichte im technikum29"</h4> 
+	<p>In dieser Führung werden die Anfänge der Computertechnik von der Lochkartentechnik über mechanische Rechenmaschinen zu den frühen Computern der 50er - 70er Jahre vorgestellt 
+
+
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Mittwoch, 16. September 2020
+		<dt>Uhrzeit
+		<dd>10:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dt>
+		<dd>geschlossene Gesellschaft - Kursfabrik Wiesbaden
+		
+		
+	</dl>
+</div><!--/.termin -->
 
 </div><!--/#termine -->
 

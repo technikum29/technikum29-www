a071c8ed19c567d6545b3a6837878fa3d123e7a8
Sven Sveto <sven@sveto>
2020-03-03 08:36:40 +0100

    Neuer Blogartikel über das jüngst vergangene Arbeitswochende

---
+++ blog/2020-03-03-Nachlese-Arbeitswochenende.php
@@ -0,0 +1,68 @@
+<?php
+	$blog_title = "Erfolgreiches Treffen der Unterstützer des technikum29 am 28.02.2020";
+	$blog_title_kurz = "Bericht: Erfolgreiches Treffen der Unterstützer des technikum29 am 28.02.2020";
+	$blog_author = "Roland";
+	$blog_date = "2020-03-03";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>Sehr reges Treiben herrschte im technikum29 am letzten Samstag: mehr als 15 Unterstützer des
+technikum29 trafen sich im Museum, um verschiedensten Aktivitäten zum Erhalt der Sammlung
+fortzuführen, zu diskutieren bzw. zu starten:
+
+<ul>
+<li>Die Reparaturarbeiten an den <a href="/de/rechnertechnik/personalcomputer.php">WANG 2200</a>-Systemen (Christian, Georg, Martin und Michael) und der <a href="/de/rechnertechnik/lgp30.php">LGP30</a> (Jürgen M) gingen weiter! Die WANGs zeigen wieder ein Lebenszeichen, die LGP30 widersetzt sich unseren Reparaturversuchen weiterhin hartnäckig.
+
+<li>Unsere <a href="/de/rechnertechnik/ibm1130.php">IBM1130</a> druckt wieder und ist damit wieder für Vorführungen einsatzbereit (Roland, Reinhard&nbsp;F)!
+
+<li>Zwei neue Mitstreiter (Detlef, Thorsten) kümmerten sich um unsere <a href="/de/kommunikationstechnik/faxtechnik.php#Fernschreibtechnik">Fernschreiber</a> und werden diese in Kürze mit dem <a href="https://www.i-telex.net/">weltweiten Fernschreibnetz i-Telex</a>, das Enthusiasten im Internet betreiben, verbinden. Das wird ein neues Highlight für Führungen werden.
+
+<li>Eine <a href="/de/rechnertechnik/programmierbare.php#101">Olivetti Programma P101</a> wurde erfolgreich repariert, die Arbeiten an unserer großen <a href="/de/rechnertechnik/kommerzielle.php#olivetti-p203">Programma P201</a> gehen voran (Jürgen S).
+
+<li>Mit unseren langjährigen UNIVAC-Unterstützern wurde die Wiederinstandsetzung des <a href="/de/rechnertechnik/univac9400.php">Univac 9400-Rechenzentrums</a> nach langer Pause besprochen (Hrn. Barnowski &amp; Schale).
+
+<li>Zwei junge Unterstützer (David, Alexander) möchten sich um ein Inventarisierungssystem kümmern und ggf Patenschaften für einige Computer übernehmen.
+
+<li>Reinhard M will sich um unser großes TMZ-Bandlaufwerk (IBM 1130) kümmern
+
+<li>Unsere Infrastruktur wurde verbessert: wir haben jetzt im Museum einen Internetanschluß und einen WLAN-Hotspot (Sven, Thilo).
+
+<li>Ralf hatte sich schon vor einiger Zeit angeboten, sich um die Haustechnik zu kümmern. Aktuell wird die Klimasituation im Museum erfasst, um Feuchteschäden durch die unregelmäßige Heizsituation zu vermeiden.
+
+<li>Christan kümmert sich mit Sven um ein Whitepaper, um die Diskussion zur Zukunft des Museums voranzubringen.
+
+<li>Roland hat mittlerweile ungefähr <a href="/de/termine.php">zehn Führungen</a> erfolgreich durchgeführt, unser Gästebuch füllt sich mit vielen begeisterten Kommentaren unserer Besucher.
+
+<li>Reges Treiben herrschte auch in den Depots: Dutzende Kisten mit Ersatzteilen und viele Geräte wurden ins technikum29 transferiert (Sven und Helfer)
+
+</ul>
+
+<p>Wir waren überrascht von der Resonanz auf unsere Einladung und werden in Kürze möglichst
+in engerer Folge weitere  Möglichkeiten zu gemeinsamen Arbeiten im Museum anbieten.
+Aktuelle Termine werden auf unserer Webseite unter <a href="/de/termine.php" class="go">Termine</a> bekannt gegeben.
+
+<p>Wir möchten an der Stelle auch auf <a href="https://twitter.com/technikum29">unseren neuen Twitter-Account @technikum29</a>
+hinweisen, der es einfacher macht, den Aktivitäten und Ankündigungen des Museums zu folgen.
+
+<p>Allen Helfern ein <strong>Riesen-Dankeschön</strong> für ihren Einsatz!
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2020-03-03-Arbeitstreffen/univac9400-konsole.jpg" class="popup">
+      <img src="/shared/photos/blog/2020-03-03-Arbeitstreffen/univac9400-konsole.jpg" width="420">
+   </a>
+   <a  href="/shared/photos/blog/2020-03-03-Arbeitstreffen/personen-an-der-ibm1130.jpg" class="popup">
+      <img src="/shared/photos/blog/2020-03-03-Arbeitstreffen/personen-an-der-ibm1130.jpg" width="420">
+   </a>
+   <p class="bildtext small">
+     <em>Links:</em> Die Farbrolle des Univac9400-Terminals wurde mit einer selbst angerührten Farbmischung bestückt
+     und ist total vertrocknet. Ohne Terminal lässt sich der Rechner nicht steuern, daher wollen wir hier
+     baldmöglichst einen Ersatz anschließen.
+     <em>Rechts:</em> Die IBM-1130 wird repariert, während die nächste Generation an die Architektur des
+     Rechners herangeführt wird.
+   </p>
+</div>
+
+<?php print_author_info(); ?>

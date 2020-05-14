544ce66457bb6c4fa7e06aec29750eb4f337ef45
RolandLangfeld <54443215+RolandLangfeld@users.noreply.github.com>
2020-05-14 10:44:16 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -15,7 +15,7 @@
   <p>Kein Hobby kann so wichtig sein, daß wir unsere Gesundheit oder die anderer in Gefahr bringen! 
   <p>Aus diesem Grund <strong>sagen wir alle geplanten Touren und Helfertage im Museum vorerst ab</strong>
   <p>Update 14.5.2020: 
-  <p>Einige Museen in Hessen öffnen wieder unter Auflagen aus einem strengen Hygienekonzept: 
+  <p>Einige Museen in Hessen öffnen wieder unter Auflagen nach einem strengen Hygienekonzept: 
 	  keine Gruppenführungen, es darf nichts  angefasst werden u.a. Das Konzept des technikum29 kann
 	  unter diesen Bedingungen nicht fortgesetzt werden, unser Ansatz lebt von der Interaktivität in
 	  Gruppen und dem "Begreifen" der Exponate. 

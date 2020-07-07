3a684913360fd038fd92c13a2dca572ede7faf80
Sven <sven@timothy.local>
2020-07-07 16:08:46 +0200

    Entwurf für Blogeintrag.

---
+++ blog/2020-07-07-Nachlese-Helfertag.php
@@ -0,0 +1,50 @@
+<?php
+	$blog_title = "Erfolgreicher Helfertag am 04.07.2020";
+	$blog_title_kurz = "Bericht: Erfolgreicher Helfertag während Corona technikum29 am 04.07.2020";
+	$blog_author = "Roland";
+	$blog_date = "2020-03-03";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>In Zeiten der Corona-Pandemie blieben die letzten Monate auch die Pforten unseres Museums
+für die Öffentlichkeit geschlossen. Eine Versuchsführung mit einer kleinen Gruppe vor einigen
+Wochen (seit <a
+href="https://www.hessen.de/fuer-buerger/corona-hessen/interviews-reden-und-mehr/corona-massnahmen-der-landesregierung"
+>Ende März</a> in Hessen wieder erlaubt) hat uns keinen großen Spaß gemacht — mit Schutzmaske
+und Sicherheitsabstand ist lebendige Computertechnik nur schwer vorzuführen. Zumal unser Konzept
+auch auf dem Benutzen von Exponaten beruht, was Museen seinerzeit noch landesweit behördlich
+untersagt war.
+
+<p>Mittlerweile ist der <em>Lockdown</em> aber an vielen Stellen weiter zurückgerückt. Geschäfte
+des täglichen Bedarfs mit Atemmaske zu betreten gehört mittlerweile zum Alltag, genauso wie
+corona-bedingte Besucherlisten in Cafes und die obligatorischen Handdesinfektions-Pumpen.
+
+<p>Mit dieser Grundausstattung haben wir am vergangenen Samstag also auch bei uns 
+
+<!--
+<p>Wir möchten an der Stelle auch auf <a href="https://twitter.com/technikum29">unseren neuen Twitter-Account @technikum29</a>
+hinweisen, der es einfacher macht, den Aktivitäten und Ankündigungen des Museums zu folgen.
+
+<p>Allen Helfern ein <strong>Riesen-Dankeschön</strong> für ihren Einsatz!
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2020-03-03-Arbeitstreffen/univac9400-konsole.jpg" class="popup">
+      <img src="/shared/photos/blog/2020-03-03-Arbeitstreffen/univac9400-konsole.jpg" width="420">
+   </a>
+   <a  href="/shared/photos/blog/2020-03-03-Arbeitstreffen/personen-an-der-ibm1130.jpg" class="popup">
+      <img src="/shared/photos/blog/2020-03-03-Arbeitstreffen/personen-an-der-ibm1130.jpg" width="420">
+   </a>
+   <p class="bildtext small">
+     <em>Links:</em> Die Farbrolle des Univac9400-Terminals wurde mit einer selbst angerührten Farbmischung bestückt
+     und ist total vertrocknet. Ohne Terminal lässt sich der Rechner nicht steuern, daher wollen wir hier
+     baldmöglichst einen Ersatz anschließen.
+     <em>Rechts:</em> Die IBM-1130 wird repariert, während die nächste Generation an die Architektur des
+     Rechners herangeführt wird.
+   </p>
+</div>
+-->
+
+<?php print_author_info(); ?>

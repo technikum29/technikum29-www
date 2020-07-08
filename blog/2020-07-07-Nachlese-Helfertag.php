652934a82cda17e6bd8de23ac8e92d91568e0172
Sven Sveto <sven@sveto>
2020-07-08 14:25:16 +0200

    Blogeintrag über letzten Helfertag fertiggeschrieben.

--- blog/2020-07-07-Nachlese-Helfertag.php
+++ blog/2020-07-07-Nachlese-Helfertag.php
@@ -1,13 +1,24 @@
 <?php
 	$blog_title = "Erfolgreicher Helfertag am 04.07.2020";
-	$blog_title_kurz = "Bericht: Erfolgreicher Helfertag während Corona technikum29 am 04.07.2020";
-	$blog_author = "Roland";
-	$blog_date = "2020-03-03";
+	$blog_title_kurz = "Bericht aus der Corona-Zeit: Erfolgreicher Helfertag am 04.07.2020";
+	$blog_author = "Sven";
+	$blog_date = "2020-07-07";
 
 	require "blog.php";
 	
 	print_blog_title();
 ?>
+
+<div class="box desc-right auto-bildbreite">
+   <a href="/shared/photos/blog/2020-07-04-Helfertag/Corona-Covid-Eingang-technikum29.jpg" class="popup">
+      <img src="/shared/photos/blog/2020-07-04-Helfertag/Corona-Covid-Eingang-technikum29.jpg" width="420">
+   </a>
+   <p class="bildtext">
+     Besucherliste und Handdesinfektion am Eingang, Verpflegung unter freiem Himmel im Museumsgarten:
+     So öffneten wir am Morgen des ersten Helfertages seit der Corona-Pandemie unsere Türen.
+     Das Bild erschien <a href="https://twitter.com/technikum29/status/1279338714196516864">life bei Twitter</a>.
+   </p>
+</div>
 
 <p>In Zeiten der Corona-Pandemie blieben die letzten Monate auch die Pforten unseres Museums
 für die Öffentlichkeit geschlossen. Eine Versuchsführung mit einer kleinen Gruppe vor einigen
@@ -21,30 +32,52 @@ untersagt war.
 <p>Mittlerweile ist der <em>Lockdown</em> aber an vielen Stellen weiter zurückgerückt. Geschäfte
 des täglichen Bedarfs mit Atemmaske zu betreten gehört mittlerweile zum Alltag, genauso wie
 corona-bedingte Besucherlisten in Cafes und die obligatorischen Handdesinfektions-Pumpen.
+Mit dieser Grundausstattung haben wir am vergangenen Samstag also auch bei uns geöffnet
+(siehe rechts).
 
-<p>Mit dieser Grundausstattung haben wir am vergangenen Samstag also auch bei uns 
+<p>Im Laufe des Tages kamen knapp 10-15 angemeldete Helfer, um sich um diverse Geräte zu kümmern.
+Sie verteilten sich bei den <a href="/de/rechnertechnik/personalcomputer.php">WANG-Rechnern</a>,
+aber auch bei unseren
+<a href="/de/kommunikationstechnik/faxtechnik.php#Fernschreibtechnik">Fernschreibern</a>. Auch
+unsere am besten laufenden Rechner, wie die <a class="go"
+href="https://twitter.com/technikum29/status/1279440406888034306">DEC PDPs</a> und die 
+<a class="go" href="https://twitter.com/technikum29/status/1279421502564126720">Univac 9200er</a>
+liefen.
 
-<!--
-<p>Wir möchten an der Stelle auch auf <a href="https://twitter.com/technikum29">unseren neuen Twitter-Account @technikum29</a>
-hinweisen, der es einfacher macht, den Aktivitäten und Ankündigungen des Museums zu folgen.
 
-<p>Allen Helfern ein <strong>Riesen-Dankeschön</strong> für ihren Einsatz!
-
-<div class="box center">
-   <a  href="/shared/photos/blog/2020-03-03-Arbeitstreffen/univac9400-konsole.jpg" class="popup">
-      <img src="/shared/photos/blog/2020-03-03-Arbeitstreffen/univac9400-konsole.jpg" width="420">
-   </a>
-   <a  href="/shared/photos/blog/2020-03-03-Arbeitstreffen/personen-an-der-ibm1130.jpg" class="popup">
-      <img src="/shared/photos/blog/2020-03-03-Arbeitstreffen/personen-an-der-ibm1130.jpg" width="420">
-   </a>
-   <p class="bildtext small">
-     <em>Links:</em> Die Farbrolle des Univac9400-Terminals wurde mit einer selbst angerührten Farbmischung bestückt
-     und ist total vertrocknet. Ohne Terminal lässt sich der Rechner nicht steuern, daher wollen wir hier
-     baldmöglichst einen Ersatz anschließen.
-     <em>Rechts:</em> Die IBM-1130 wird repariert, während die nächste Generation an die Architektur des
-     Rechners herangeführt wird.
-   </p>
+<div class="box auto-bildbreite center">
+  <a href="/shared/photos/blog/2020-07-04-Helfertag/Kathodenstrahlroehre-Test.jpg" class="popup">
+    <img src="/shared/photos/blog/2020-07-04-Helfertag/Kathodenstrahlroehre-Test.jpg" width="700">
+  </a>
+  <p class="bildtext">
+  Reinhard M. nahm sich mit seinem Sohn unserer exotischen
+  <a href="/de/kommunikationstechnik/messtechnik.php">Experimentaltechnik-Sammlung</a> an. Unter
+  anderem brachte er die abgebildete Kathodenstrahlroehre zum Leuchten.
+  </p>
 </div>
--->
+
+<p>Bemerkenswert ist, dass sich sogar Helfer einfanden, um unseren kleinen Museumsgarten
+zurückzuschneiden. Der hat zur Zerstreuung und zum Gewinnen von Abstand gute Dienste geleistet.
+Besonderer Dank gilt hier Ralf K., Markus S. und Sylvia K..
+Von Besuchern geschossene Fotos finden sich
+dazu auch auf der von Facebook geführten
+<a href="https://www.facebook.com/pages/Technikum29/2065486283675504">Seite über
+das technikum29</a>.
+
+<h3>Wie geht es während Corona weiter im technikum29?</h3>
+
+<p>Wir halten unsere <a class="go" href="/de/termine.php">Terminseite</a> stetig aktuell und
+informieren dort, sobald wir wieder regelmäßiger die Tore öffnen können. Für September ist
+eine reguläre Führung geplant, sodenn es keinen erneuten Lockdown gibt.
+
+<p>Wir planen auch vorher weitere Helfertage in kleineren Kreisen, die auf einzelne Themen
+spezialisiert sind. Dazu gehören etwa ein Tag, der der unserer größten Anlage, der
+<a href="/de/rechnertechnik/univac9400.php">Univac 9400</a>, gewidmet ist, sowie ein Tag,
+an dem das erste mal dediziert Erfahrungen mit Videoaufnahmen gemacht werden sollen.
+
+<p>Die nächsten Helfertage kündigen wir in erster Linie über unseren kleinen E-Mail-Verteiler
+an. Wer auf der Liste eingetragen werden möchte, kann sich formlos
+<a href="/de/impressum.php">bei uns melden</a>.
+
 
 <?php print_author_info(); ?>

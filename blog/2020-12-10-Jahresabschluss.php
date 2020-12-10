0181708287d43d601f093a6e9fbf089bef7f55e0
SvenK <git+sveto@svenk.org>
2020-12-10 20:03:24 +0100

    Jahresabschluss-Mail hinzugefügt

---
+++ blog/2020-12-10-Jahresabschluss.php
@@ -0,0 +1,67 @@
+<?php
+	$blog_title = "technikum29 wünscht schöne Feiertage — wir fassen zusammen, was 2020 geschah";
+	$blog_title_kurz = "technikum29 wünscht schöne Feiertage";
+	$blog_author = "Sven";
+	$blog_date = "2020-12-10";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>Liebe Freunde und Unterstützer des technikum29,
+
+<p>
+ein herausforderndes Jahr neigt sich dem Ende zu und wir möchten es nicht versäumen, allen Unterstützern und Helfern für ihre Arbeit und Unterstützung in 2020 zu danken. Wir wünschen euch, euren Familien und Lieben gesunde, ruhige Festtage und ein erfolgreiches Neues Jahr!
+
+<h3>Was geschah 2020?</h3>
+
+<h4>Führungen</h4>
+<p>Das Jahr 2020 war geprägt durch Covid-19 und den damit einhergehenden Einschränkungen. Zu Beginn des Jahres konnten wir noch über <strong>10 Führungen</strong> mit über 150 Teilnehmern absolvieren. Unsere Gäste waren durchweg begeistert und füllten das Gästebuch mit Kommentaren und unser Sparschwein mit dem einen oder anderen Schein. Aber im März zeichnete sich ab, dass wir keine Führungen zu Corona-Bedingungen anbieten können: das Abstandsgebot und stundenlanges Maskentragen lassen sich nicht durchhalten. Von daher ruht der Führungsbetrieb seit März bis auf weiteres auch ins Jahr 2021 hinein - leider !
+
+<h4>Organisatorisches</h4>
+<p>Eine mögliche <strong>Vereinsgründung</strong> ist zunächst aufgeschoben, einerseits wegen der Beschränkungen durch Corona, andererseit wg. Diskussionen um die geeignete rechtliche Form für das Museum. Technikum29 ist also nach wie vor eine private Sammlung, was dem Schwung der Helfer und auch der einen oder anderen Spende keinen Abbruch getan hat.
+
+<p>
+Trotz aller Beschränkungen konnten wir doch einige Erfolge erzielen: wir konnten Anfang des Jahres noch 2 Helfertage abhalten, bei dem bis zu 20 Helfer Maschinen reparierten. Nach den Corona-Beschränkungen kamen einzelne Helfer ins Museum, und mit großem Abstand wurde das eine oder andere Gerät weiter instand gesetzt bzw. erstmalig wieder in Betrieb genommen. <strong>Wir danken den betreffenden Helfern hier summarisch</strong>, um in der Einzelaufzählung niemenden zu vergessen.
+
+<h4>Depots</h4>
+<p>
+In den <strong>Depots</strong> wurde kontinuierlich aufgeräumt, hin- und hertransportiert und
+Platz geschaffen. Aber es ist eine Sisyphusarbeit!
+Die Fortschritte sind noch gering, und Überschwemmungen im Folge eines Gewitters
+im Sommer haben die Lage nicht verbessert. <a href="/blog/2020-09-03-Lagersuche.php">Wir suchen dringend bezahlbaren
+Lagerraum auf Zeit, um das Sortieren der vielen Geräte(fragmente) voranzutreiben.</a>
+Wer immer etwas weiss - bitte melden!
+
+<h3>Was geschah technisch in 2020 (alphabetisch gereiht)?</h3>
+
+<ul>
+<li>die <a href="/de/rechnertechnik/tabelliermaschine.php">BULL Tabelliermaschine</a> läuft wieder, führt Konten und auch das berühmte „Wurzelprogramm“ (siehe unsere Webseite) ist wieder vorführbereit
+<li>die Wiederinbetriebnahme der <a href="/de/rechnertechnik/gamma55.php">BULL GE55</a> wurden aufgenommen, Defekte im Netzteil beseitigt
+<li>zu unserer <a href="/blog/2019-12-05-cpt-8800.php">CPT8800</a> erhielten wir umfangreiche Ersatzteile, Broschüren und Handbücher sowie Originaldisketten - sie ist damit komplett und vorführbereit
+<li>Drei <a href="/de/kommunikationstechnik/faxtechnik.php#Fernschreibtechnik">Fernschreiber</a> wurden überholt und mit einer modernen Vermittlungsstelle versehen. Somit kann der frühere Wählbetrieb vorgeführt werden. Sogar <a href="/blog/2020-07-27-iTelex.php">über das Internet</a> kann man unsere Fernschreiber erreichen.
+<li>unser <strong>Garten</strong> wird von Ralf beispielhaft in Schuß gehalten, er war auch zur Stelle, als wir Anfang des Jahres einen Wassereinbruch im Keller wg einer defekten Pumpe hatten und ist auch sonst als Hausmeister unentbehrlich!
+<li>der Drucker der <a href="/de/rechnertechnik/ibm1130.php">IBM1130</a> wurde gereinigt, Probleme im Memory wurden beseitigt und sie ist wieder vorführbereit.
+<li>unsere <em>IBM5100</em> konnte mit großer Unterstützung des IBM-Museums (Stuttgart Böblingen) wieder in Gang gesetzt werden, zur Zeit wird der Drucker <em>IBM5103</em> repariert.
+<li>das technikum29 <a href="https://twitter.com/technikum29/status/1228333243818237953">hat jetzt Internetanschluß und WLAN</a> und vieles mehr.
+<li>die <em>Inventarisierung</em> vor allem von Ersatzteilen, Broschüren, Software und Schaltplänen wurde deutlich vorangetrieben, 
+<li>die Restaurierung der <a href="/de/rechnertechnik/lgp30.php">LGP30</a> ging weiter: die <em>Trommelreplika</em> konnte erfolgreich an der Maschine der Freunde vom Computermuseum der Informatik Uni Stuttgart getestet werden - weitere Arbeiten bei uns blieben ruhten wg. Corona und werden die Wochen wieder aufgenommen
+<li>zu unseren zwei <em>LGP21</em> wurde mit einer Bestandsaufnahme zwecks Restaurierung begonnen (hier unterstütze uns schon 2019 das ISER in Erlangen). Wir begannen, die große Sammlung an Originalprogrammen der Maschine elektronisch zu sichern.
+<li>eine weitere <a href="/de/rechnertechnik/kommerzielle.php#olivetti-p203">Olivetti P101</a> läuft wieder
+<li>die <a href="/de/rechnertechnik/fruehe-computer.php#pdp8i">PDP-8i</a> und die <a href="/de/rechnertechnik/fruehe-computer.php#pdp12">PDP-12</a> sind wieder vorführbereit
+<li>für unsere <a href="/de/geraete/pianola-funktionserklaerung/">Pianola</a> konnte ein Musiker gefunden werden, mit dem wir spezielle Musikvorführungen planen, sobald Corona es wieder erlaubt
+<li>die Reparatur der <a href="/de/rechnertechnik/personalcomputer.php">WANG</a>-Computer ging voran - auch hier Stopp wg. der Coronasituation
+</ul>
+
+
+
+<p>
+Soweit der Stand zum technikum29 und unser Dank!
+<p>
+<a href="/de/team.php#roland">Roland</a> &amp; <a href="/de/team.php#sven">Sven</a>
+
+
+<?php //print_author_info();
+?>
+

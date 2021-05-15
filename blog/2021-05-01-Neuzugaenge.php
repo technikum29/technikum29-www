f598117263ab56b6b3d5652409b7ccb911b6b930
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2021-05-15 18:32:49 +0200

    Create 2021-05-01-Neuzugaenge.php

---
+++ blog/2021-05-01-Neuzugaenge.php
@@ -0,0 +1,91 @@
+<?php
+	$blog_title = "Neuzugänge";
+	$blog_title_kurz = "Geräte-Neuzugänge in 2021";
+	$blog_author = "Roland";
+	$blog_date = "2021-05-01";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>
+In den letzten Monaten erreichten das technikum29 eine Vielzahl von interessanten neuen Exponaten, 
+die wir hier kurz vorstellen möchten:
+
+<p>
+Das älteste Exponat übereignete uns freundlicherweise Frau Sigrun Schuster aus Kronberg: eine Staffelwalzen-
+Rechenmaschine Typ <strong> „XxX“ von Seidel und Naumann</strong> (Dresden). Dieses Modell wurde von 1906 bis 1927 hergestellt, 
+eine genauere Zuordnung des Herstelljahrs zu unserer Maschine ist uns im Augenblick nicht möglich.
+
+<p>
+Nach oberflächlicher Reinigung und etwas Ölen funktionierte die Maschine auf Anhieb in ihren Grundfunktionen. 
+Einige Details sind noch im Zuge einer späteren Komplettüberholung zu erledigen. Einige fehlende Tastenkappen 
+wurden uns freundlicherweise von Florian Ehmann per 3-D-Druck ersetzt (im Bild auf der linken Seite der Volltastatur 
+zu sehen, Gravur fehlt noch).
+  
+<p>
+Die Maschine wird wegen ihrer guten Zugänglichkeit bei Führungen zu mechanischen Rechenmaschinen vorgeführt werden.
+
+<p>
+Mit Sicherheit das schwerste der Neuzugänge ist eine komplette <strong>IBM5320 System/32</strong>. Sie wurde uns von Edwin Miller 
+(Rammingen) gespendet. Die 5320 ist ein 16-Bit-Einzelplatzcomputer, der von 1975-1984 von IBM vertrieben (bzw. 
+vermietet) wurde.
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2021-05-01-Neuzugaenge/IBM5320-Herkunft.jpg" class="popup">
+      <img src="/shared/photos/blog/2021-05-01-Neuzugaenge/IBM5320-Herkunft.jpg" width="400">
+   </a>
+   <a  href="/shared/photos/blog/2021-05-01-Neuzugaenge/IBM5320-Abtransport.jpg" class="popup">
+      <img src="/shared/photos/blog/2021-05-01-Neuzugaenge/IBM5320-Abtransport.jpg" width="400">
+   </a>
+</div>
+   
+<p>
+Das Gerät ist nicht zerlegbar und mußte mit seinen 290 kg durch das Zimmerfenster im ersten Stock mit Hilfe eines 
+Hubwagens abtransportiert werden. Fleißige Helfer halfen dann in Kelkheim, das gute Stück in das Museum zu bringen. 
+Zu dem Gerät erhielten wir mehrere Kisten voller technischer Dokumentation und Software, so dass wir zuversichtlich 
+sind, die IBM/32 bald wieder in Betrieb nehmen zu können - wir werden berichten !.
+
+<p>
+Beim Transport der IBM/32 führte der Weg unseres Museumstransporters noch in Neckargemünd vorbei, wo wir eine Schenkung 
+von Günter Ruschitzka entgegennehmen konnten: einen <strong>DEC-Writer LA36</strong> in betriebsbereitem Zustand. Er wird unsere 
+Präsentation von DEC-Rechnern ergänzen (im Bild vorne rechts)!
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2021-05-01-Neuzugaenge/DEC-LA36.jpg" class="popup">
+      <img src="/shared/photos/blog/2021-05-01-Neuzugaenge/DEC-LA36.jpg" width="400">
+   </a>
+   <a  href="/shared/photos/blog/2021-05-01-Neuzugaenge/PDP8E-Neuzugang.jpg" class="popup">
+      <img src="/shared/photos/blog/2021-05-01-Neuzugaenge/PDP8E-Neuzugang.jpg" width="400">
+   </a>
+</div>
+  
+<p>
+Herr Simon Gleissner (Bad Homburg) vermittelte uns als Vermächtnis eine <strong>DEC PDP8E<strong>, die wir im Raum Hannover abholen 
+durften. Das technikum29 zeigt zwar eine Reihe von PDP-8-Rechnern, eine vorzeigbare 8E fehlt uns aber noch. Zunächst 
+war der Schreck groß: die Maschine wurde lange Zeit kopfüber in einem feuten Keller gelagert, eine erste Inspektion 
+zeigte aber, dass dies wohl nur kosmetische Spuren auf dem Gehäusedeckel hinterlassen hat. Die Elektronikist komplett 
+und optisch in gutem Zustand - mehr wird eine vorsichtige Inbetriebnahme zeigen.
+
+
+<p>
+Anläßlich eines Besuchs im Museum überließ uns Bernd Nagel (Frankfurt) zwei wunderschöne Exponate mit den herrlichen 
+Nixie-Röhrenanzeigen: einen 20 MHz-Universalzähler der Firma Hewlett Packard Typ hp5325B von 1969 und einen wunderschönen 
+Tischrechner Olympia CD401 von 1972 - dafür nochmals vielen Dank !
+  
+<div class="box center">
+   <a  href="/shared/photos/blog/2021-05-01-Neuzugaenge/hp5325B-Neuzugang.jpg" class="popup">
+      <img src="/shared/photos/blog/2021-05-01-Neuzugaenge/hp5325B-Neuzugang.jpg" width="400">
+   </a>
+   <a  href="/shared/photos/blog/2021-05-01-Neuzugaenge/CD401-Neuzugang.jpg" class="popup">
+      <img src="/shared/photos/blog/2021-05-01-Neuzugaenge/CD401-Neuzugang.jpg" width="400">
+   </a>
+</div>
+
+<p>
+Und schließlich erhielten wir aus einem größeren Platinenfundus 4 Ersatzplatinen für unsere BULL Gamma 10 - hier geht 
+der Dank an Frank Kabjoll (Siegen).
+  
+<?php print_author_info(); ?>

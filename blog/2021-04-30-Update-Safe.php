d75c3f58d2516bf325ee26b95516c2385e8096bd
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2021-05-15 17:01:39 +0200

    Update 2021-04-30-Update-Safe.php
    Link eingefügt, Fehlerkorrektur

--- blog/2021-04-30-Update-Safe.php
+++ blog/2021-04-30-Update-Safe.php
@@ -11,15 +11,18 @@
 ?>
 
 <p>
-Wir hatten über das Problem der verschlossenen Revisionstür unseres Tresors im Museum berichtet. 
+Wir hatten über das <a href="/blog/2020-02-05-Safetuer.php"> Problem der verschlossenen Revisionstür</a> 
+unseres Tresors im Museum berichtet. 
 Leider hatten die Corona-Einschränkungen verhindert, dass Helfer zu uns ins Museum kommen konnten,
-umd die Tür zu öffnen.
+um die Tür zu öffnen.
 
+<p>
 Zufälligerweise fanden wir eines Tages beim Aufräumen die Schlüssel für die Revisionstür ! Die 
 Schlösser liessen sich öffnen, doch die Revisionstür ging nur ca 1 cm weit auf. Irgend etwas verhinderte
 das Aufschwenken. Letztlich fanden wir den entscheidenden Trick: Der Tresor muss mit der richtigen
 Kombination geöffnet sein und auf halben weg zwischen "Riegel zu" und "Riegel auf" wird die 
-Revisionstür freigegeben ! Damit hat sich unsere Anfrage erledigt.
+Revisionstür freigegeben ! Damit hat sich unsere Anfrage erledigt, hier der Blick auf die Rückseite
+der Tresortür mit geöffneter Revisionstür:.
 
 <div class="box center">
    <a  href="/shared/photos/blog/2020-02-05-Safe/bank-safe-tuer-geoeffnet.jpg" class="popup">

67830749871d2c91e4a79f9983f251c8c9b278b0
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2021-05-19 19:29:36 +0200

    Update 2021-05-19-MADAS.php

--- blog/2021-05-19-MADAS.php
+++ blog/2021-05-19-MADAS.php
@@ -11,13 +11,20 @@
 ?>
 
 <p>
-Text
-
-<video width="320" height="240" controls>
-  <source src="/shared/photos/blog/2021-05-19-MADAS/MADAS_rechnet.mov" type="video/mov">
-Your browser does not support the video tag.
-</video>
-
+Wer in den letzten Jahren das technikum29 besucht hat, kennt unsere mechanische Rechenmaschine
+	MADAS VII eTA. Die Maschine muss vor langer defekt geworden sein, sie blockiert, deshalb 
+	war sie auf einem Gestell montiert und diente als Schaustück für den Stand
+	der mechanischen Rechentechnik der 30er Jahre des letzten Jahrhunderts: ein Vollautomat,
+	der die 4 Grundrechenarten vollautomatisch durchführen konnte. 
+	
+<p>
+Mittlerweile haben wir in unseren depots das Gehäuse mit Motor wiedergefunden. Deshalb lag es nahe,
+	die Maschine wieder instand zu setzen. Sie läuft jetzt wieder, nur bei der Multiplikation
+	"hakt" es noch etwas. Hier in dem kleinen Filmchen sieht man, wie sie die Rechnung "1 geteilt 
+	durch 7" vollautomatisch auf Knopfdruck abarbeitet und am Schluß das Ergebnis auf 6 Stellen 
+	anzeigt: 0,142857 !
+	
+BAUSTELLE: Film folgt in Kürze ...
 
 
 <?php print_author_info(); ?>

c1fcd793cf4502598ffcb01c7dd9edbbf127c6f4
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-04-26 12:05:40 +0200

    Neuer Beitrag

---
+++ blog/2022-04-26-Wir-raeumen-um.php
@@ -0,0 +1,28 @@
+<?php
+	$blog_title = "Neues im Technikum29 - Wir räumen um !";
+	$blog_title_kurz = "Neues im Technikum29";
+	$blog_author = "Roland";
+	$blog_date = "2022-04-26";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>
+
+<p>
+
+<p>
+
+<p>
+
+<p>
+
+<p>
+
+  
+<?php print_author_info(); ?>
+
+

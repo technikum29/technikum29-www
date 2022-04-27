54fe28054380b5d5b97808413032cc8f118dc04f
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-04-27 18:57:59 +0200

    Create 2022-04-27-Neuzugaenge.php

---
+++ blog/2022-04-27-Neuzugaenge.php
@@ -0,0 +1,60 @@
+<?php
+	$blog_title = "Wir erwarten neue Exponate";
+	$blog_title_kurz = "Wir erwarten neue Exponate";
+	$blog_author = "Roland";
+	$blog_date = "2027-04-26";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>Das <a href="https://museum-kassel.de/de/museen-schloesser-parks/staatspark-karlsaue/astronomisch-physikalisches-kabinett"> 
+  Astronomisch-Physikalisches Kabinett </a>in Kassel muß einige seiner Depoträume räumen und bot uns an, einige seltene Exponate 
+  zu überlassen. Die Wahl fiel auf das technikum29, weil unser Konzept, historische Computertechnik möglich betriebsbereit und 
+  "zum Anfassen" zu präsentieren, ganz im Sinne des Kasseler Museums ist. Wir besprechen im Augenblick noch Details des 
+  Überlassungsvertrages und des Transports, möchten aber schon einmal die guten Stücke zeigen, wie sie im Kasseler Depot auf 
+  den Transfer warten:
+
+<p>Hier eine MAEL 4000 und eine Triumpf Adler TA1000:
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2022-04-26-Neuzugaenge/MAEL4000.jpg" class="popup">
+      <img src="/shared/photos/blog/2022-04-26-Neuzugaenge/MAEL4000.jpg" width="400">
+   </a>
+   <a  href="/shared/photos/blog/2022-04-26-Neuzugaenge/TA1000.jpg" class="popup">
+      <img src="/shared/photos/blog/2022-04-26-Neuzugaenge/TA1000.jpg" width="400">
+   </a>
+</div>
+
+<p>Ferner einen UNIVAC 1710-Stanzer und ein elektromechanischer Buchungsautomat Mercedes Addelektra:
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2022-04-26-Neuzugaenge/UNIVAC1710.jpg" class="popup">
+      <img src="/shared/photos/blog/2022-04-26-Neuzugaenge/UNIVAC1710.jpg" width="400">
+   </a>
+   <a  href="/shared/photos/blog/2022-04-26-Neuzugaenge/Addelektra.jpg" class="popup">
+      <img src="/shared/photos/blog/2022-04-26-Neuzugaenge/Addelektra.jpg" width="400">
+   </a>
+</div>
+
+
+<p>Zuguterletzt ein für uns recht neuer Computer: ein Q1 der Firma Q1 Inc:
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2022-04-26-Neuzugaenge/Q1.jpg" class="popup">
+      <img src="/shared/photos/blog/2022-04-26-Neuzugaenge/Q1.jpg" width="400">
+   </a>
+  
+</div>
+
+<p>Technische Details und Art und Umfang der begleitenden Dokumentation kennen wir noch
+  nicht. Wenn alles klappt sind die Teile im Mai oder Juni im technikum29. Natürlich werden 
+  die Geräte dann ausführlich dokumentiert und wir werden versuchen, sie wieder vorführbereit 
+  zu bekommen. Wir werden die neuen Exponate dann hier auch ausführlich vorstellen.
+
+
+<?php print_author_info(); ?>
+
+

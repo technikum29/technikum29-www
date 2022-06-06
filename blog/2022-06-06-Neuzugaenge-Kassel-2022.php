d3d931c67c06f09c8fab1a82122f9f65d908ea5d
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-06-06 13:14:20 +0200

    Create 2022-06-06-Neuzugaenge-Kassel-2022.php

---
+++ blog/2022-06-06-Neuzugaenge-Kassel-2022.php
@@ -0,0 +1,39 @@
+<?php
+	$blog_title = "5 neue Exponate im technikum29 eingetroffen";
+	$blog_title_kurz = "5 neue Exponate im technikum29 eingetroffen";
+	$blog_author = "Roland";
+	$blog_date = "2022-06-06";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>wir hatten <a href="https://technikum29.de/blog/2022-04-27-Neuzugaenge.php"> im April </a>
+	angekündigt, dass wir erwarten. Es sind Schenkungen des 
+	<a href="https://museum-kassel.de/de/museen-schloesser-parks/staatspark-karlsaue/astronomisch-physikalisches-kabinett"> Astronomisch-Physikalischen Kabinetts  </a> in Kassel,
+das uns die seltenen Stücke als Schenkung überließ. 
+
+<p>
+
+<div class="box center">
+   <a  href="/shared/photos/blog/2022-06-06-Neuzugaenge-Kassel/Kassel1.jpeg" class="popup">
+      <img src="/shared/photos/blog/2022-06-06-Neuzugaenge-Kassel/Kassel1.jpeg" width="400">
+   </a>
+   <a  href="/shared/photos/blog/2022-06-06-Neuzugaenge-Kassel/Kassel2.jpeg" class="popup">
+      <img src="/shared/photos/blog/2022-06-06-Neuzugaenge-Kassel/Kassel2.jpeg" width="400">
+   </a>
+</div>
+
+
+<p>... wird fortgesetzt ....
+
+<p>
+
+<p>
+
+<p>
+
+  
+<?php print_author_info(); ?>

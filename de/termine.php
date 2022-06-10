5ace9cce546006bc79f64e275e4a2120700cfec4
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-06-10 10:59:12 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -53,7 +53,7 @@ an. Die Liste der Termine wird fortlaufend ergänzt.
 
 	
 <div class="box termin clear-after">		
-	<p class="date left"><em>2022</em> 25. Juli</p>
+	<p class="date left"><em>2022</em> 23. Juli</p>
 	<h4>allgemeiner Helfertag</h4> 
 	<p>An diesem Termin ist ein Betreuer von 10:00-18:00 vor Ort, Unterstützer können gerne jederzeit
 		dazustossen und an ihren Projekten (weiter-)arbeiten. Zur besseren Planung bitten wir unbedingt
@@ -61,7 +61,7 @@ an. Die Liste der Termine wird fortlaufend ergänzt.
 
 	<dl class="daten">
 		<dt>Tag
-		<dd class="termin">Samstag, 25. Juli 2022
+		<dd class="termin">Samstag, 23. Juli 2022
 		<dt>Uhrzeit
 		<dd>10:00 - 18:00 Uhr
 		<dt>Leitung

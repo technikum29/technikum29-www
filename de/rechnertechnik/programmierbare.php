a837e23e6d8d450ecf99698f80499b8b51c8e21e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-07-01 11:03:16 +0200

    Update programmierbare.php

--- de/rechnertechnik/programmierbare.php
+++ de/rechnertechnik/programmierbare.php
@@ -82,10 +82,10 @@ Dieses Verfahren war sehr umständlich und zeitraubend.
 
     <div class="box center">
    <a  href="/shared/photos/hp9100A.jpg" class="popup">
-      <img src="/shared/photos/hp9100A.jpg" width="400">
+      <img src="/shared/photos/hp9100A.jpg" width="350">
    </a>
    <a  href="/shared/photos/rechnertechnik/hp-9100.jpg" class="popup">
-      <img src="/shared/photos/rechnertechnik/hp-9100.jpg" width="400">
+      <img src="/shared/photos/rechnertechnik/hp-9100.jpg" width="350">
    </a>
 </div>
 

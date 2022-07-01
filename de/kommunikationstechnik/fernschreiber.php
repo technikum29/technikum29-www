919ac760740e2dda5976f5031a48bcf00e874809
Detlef Gerhardt <feedback@dgerhardt.de>
2022-07-01 18:40:21 +0200

    Update fernschreiber.php

--- de/kommunikationstechnik/fernschreiber.php
+++ de/kommunikationstechnik/fernschreiber.php
@@ -6,57 +6,82 @@
 	require "../../lib/technikum29.php";
 ?>
 
-<h2 >Fernschreibtechnik</h2>
+    <h2>Fernschreibtechnik</h2>
 
-<div id="t32" class="box left">
-	  <a href="/shared/photos/kommunikationstechnik/siemens-t32.jpg"><img src="/shared/photos/kommunikationstechnik/siemens-t32.jpg" width="435" height="415"/></a>
+    <div class="box center clear-after">
+        <a class="popup" href="/shared/photos/kommunikationstechnik/siemens-t32.jpg" title="Siemens">
+        <img src="/shared/photos/kommunikationstechnik/siemens-t32.jpg" alt="Siemens T32" width="435" height="415" /></a>
+        <p class="bildtext"><b>Siemens Springschreiber T32</b><br/>(zum Vergrößern anklicken)</p>
+    </div>
+
+	<p>Der ratternde Fernschreiber bestimmte Jahrzehnte das Bild der schnellen Nachrichtenübermittlung. 1930 erstmals von Siemens & Halske vorgestellt, wurde er am 16. Oktober 1933 von
+    der Deutschen Reichspost eingeführt. Die erste amtliche Verbindung bestand zwischen Berlin und Hamburg. Für das Fernschreib- Telexnetz mussten separate Leitungen verlegt werden.<br/>
+    Von 21 Teilnehmern 1933 stieg die Zahl auf 1500 im Jahre 1939 und schließlich auf über 90.000 um 1975. Der mechanische Fernschreiber wurde nun auf elektronische umgestellt.<br>
+    Die Blütenzeit der Mechanik in der Nachrichtentechnik bestand u.a. in den verschiedenen Fernschreibertypen. Der relativ seltene Springschreiber T32 wurde ab den 30er Jahren bis in die
+    50er hergestellt. Es ist ein Streifenschreiber mit überschaubarem Aufwand aber ungeheuer stabil aufgebaut. Im Buch von F. Schiweck "Fernschreibtechnik" (1942) wird hervorgehoben:</p>
+    <blockquote>"Der Fernschreiber ist heute eins der hervorragendsten Erzeugnisse der Feinwerktechnik, indem er in sich höchste Kunst der Getriebetechnik, Werkstoffbehandlung und Herstellung
+    vereinigt. Seine Betriebssicherheit ist nach dem Urteil der Praxis außerordentlich groß".</blockquote>
+    <p>In diesem voll mechanisiertem Zeitalter benötigten die "Büromaschinenmechaniker" ein gefühlvolles Händchen, gute Ohren um eventuelle Störgeräusche einordnen zu können und jede Menge
+    Ölkännchen mit verschiedenen Ölsorten. In den 80er Jahren ist dieser Beruf schließlich ausgestorben.</p>
+
+    <h2 id="lorenz-lo15b">Lorenz Blattschreiber Lo15b</h2>
+		
+	<div class="box center clear-after">
+		<img src="/shared/photos/dummy.jpg" width="520" height="536" alt="Lorenz Lo15b" />		
 		<p class="bildtext">
-		    Siemens Springschreiber T32 (zum Vergrößern anklicken)
-		</p></div>
-		 <p>Der ratternde Fernschreiber bestimmte Jahrzehnte das Bild der schnellen Nachrichtenübermittlung. 1930 erstmals von Siemens & Halske vorgestellt, wurde er am 16. Oktober 1933 von der Deutschen Reichspost eingeführt. Die erste amtliche Verbindung bestand zwischen Berlin und Hamburg. Für das Fernschreib- Telexnetz mussten separate Leitungen verlegt werden. <br/>
-	    Von 21 Teilnehmern 1933 stieg die Zahl auf 1500 im Jahre 1939 und schließlich auf über 90.000 um 1975. Der mechanische Fernschreiber wurde nun auf elektronische umgestellt (Telex). <br>
+            <b>Lorenz Fernschreiber ca. Bj. 1960</b> 
+		</p>
+	 </div>
+     <p>Der Blattschreiber Lo15b gehört zur zweiten Generation von Fernschreibern der Lo15-Serie, die von der Firma Lorenz seit 1930 als Lizenzbau der Teletype
+     (damals noch Kleinschmidt-Morkrum) Model 14 und 15 gebaut wurden. Unser Modell stammt von 1960 und war laut dem original erhaltenen Kennungsgeber noch bis 1983 bei der Firma
+     Rudolf Hoffmann in Nürnberg im Einsatz. Die mechanischen Fernschreiber aus dieser Zeit waren bei regelmäßiger Wartung nahezu unverwüstlich und über viele Jahre im Einsatz.<br/> 
+    Der grundsätzliche mechanische Aufbau des Lo15 wurde über alle Gerätegenerationen kaum verändert.<br/>
+    Wie auch die Siemens Fernschreiber wurde der Lo15 in den unterschiedlichsten Gehäuseformen angeboten. Unser Holzstandgehäuse befindet sich, wie der Fernschreiber selbst,
+    in einem sehr guten Zustand.<br/>
+    Über das rechts neben dem Fernschreiber angeordnete Fernschaltgerät FZGT 564 war der Fernschreiber mit der TW39-Vermittlung verbunden (TW = Teilnehmerwahl). Das FZGT 564 hat einen
+    Lokalmoduls, also eine lokale Linienstromversorgung, um ohne Verbindung zur Vermittlungsstelle Lochstreifen erstellen und drucken zu können.</br>
+    Um Zeit und damit Geld bei der Übermittlung der Nachrichten zu sparen, besitzt der Lo15b einen integrierten 5-Kanal-Lochstreifenstanzer und -sender, um Nachrichten "offline"
+    vorzustanzen und dann direkt vom Lochstreifen zusenden.</p>
 
-Die Blütenzeit der Mechanik in der Nachrichtentechnik bestand u.a. in den verschiedenen Fernschreibertypen. Der relativ seltene Springschreiber T32 wurde ab den 30er Jahren bis in die 50er hergestellt. Es ist ein Streifenschreiber mit überschaubarem Aufwand aber ungeheuer stabil aufgebaut. Im Buch von F. Schiweck "Fernschreibtechnik" (1942) wird hervorgehoben:</p>
-
-		<blockquote>"Der Fernschreiber ist heute eins der hervorragendsten Erzeugnisse der Feinwerktechnik, indem er in sich höchste Kunst der Getriebetechnik, Werkstoffbehandlung und Herstellung vereinigt. Seine Betriebssicherheit ist nach dem Urteil der Praxis außerordentlich groß".</blockquote>
+	 <h2 id="siemens-t37">Siemens Blattschreiber T37</h2>
 		
-		<p>In diesem voll mechanisiertem Zeitalter benötigten die "Büromaschinenmechaniker" ein gefühlvolles Händchen, gute Ohren um eventuelle Störgeräusche einordnen zu können und jede Menge Ölkännchen mit verschiedenen Ölsorten. In den 80er Jahren ist dieser Beruf schließlich ausgestorben.</p>
-		
-	
-	
-   
-		
-	<div class="box center auto-bildbreite">
+	<div class="box center clear-after">
 		<img src="/shared/photos/kommunikationstechnik/fernschreiber.jpg" width="520" height="536" alt="Fernschreiber" />		
 		<p class="bildtext">
 	        <b>Siemens Fernschreiber ca. Bj. 1952</b> 
 		</p>
 	 </div>
+	<p>Dieses Gerät war authentisch der erste Fernschreiber des Zeitungsverlages "Darmstädter Echo". Unten ist ein Lochstreifen-Sender zu sehen. Mit ihm konnte man vorher langsam geschriebene
+    Texte schnell versenden, ähnlich wie heute eine SMS erzeugt und gesendet wird. Die Anlage ist natürlich noch voll funktionsfähig.
+    <!-- Im Rahmen des <a href="/de/lehrerinfo.php#experimente">Experimental-Workshop</a> kann man mit diesem Gerät und einem Lorenz-Fernschreiber Chatten.</p> -->
 	 
-	 <p>Dieses Gerät war authentisch der erste Fernschreiber des Zeitungsverlages "Darmstädter Echo". Unten ist ein Lochstreifen-Sender zu sehen. Mit ihm konnte man vorher langsam geschriebene Texte schnell versenden, ähnlich wie heute eine SMS erzeugt und gesendet wird. Die Anlage ist natürlich noch voll funktionsfähig. Im Rahmen des <a href="/de/lehrerinfo.php#experimente">Experimental-Workshop</a> kann man mit diesem Gerät und einem Lorenz-Fernschreiber Chatten.</p>
-	 
-	 <div class="box left">
-		<img src="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg" width="414" height="391" alt="Fernschreiber" />
+	<h2>Der 5-Bit-Fernschreibcode</h2>
+
+	<div class="box center auto-bildbreite">
+        <img src="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg" width="414" height="391" alt="Fernschreiber" />
 		<p class="bildtext">
-	        <div class="bildtext">	
-	<p><b id="decoder">5-Bit-Fernschreibcode </b> <a class="popup" href="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg"> Bild vergrößern</a>
-			</div>
-			
-			Diesen sehr großen (70cm x 60cm) und schönen "Decoder", der den 5-Bit-Code in das internationale Telegrafenalphabet übersetzt, haben wir vor der Entsorgung an einer Technischen Universität gerettet.<br>
-			Er ist von den dortigen Werkstätten ca. in der Zeit zwischen 1937 und 1949 als aufwändiges Demonstrationsgerät für Vorlesungen und Praktika gebaut worden.<br>
-			Man stellt eine beliebige 5-Bit-Zahl an den groß dimensionierten Schaltern ein, dreht die Codierscheibe und erhält an einer bestimmten Stelle das dazugehörige, oben aufleuchtende Zeichen. Hier würde z.B. bei der Einstellung 10101 der Buchstabe "Y" leuchten.<br>
-			Was damals fortschrittliche Nachrichtentechnik war, dient heute im technikum29 als sogenannte "Dechiffriermaschine". In unserem <a href="/de/lehrerinfo.php#experimente"> Experimental-Workshop</a> können Kids und Jugendliche damit Binärtexte entschlüsseln und weiter verarbeiten.
-			</p>
-			<div class="clear"></div>
-			
-			<div class="box center auto-bildbreite">
+            <b id="decoder">5-Bit-Fernschreibcode </b> <a class="popup" href="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg"> Bild vergrößern</a>
+        </p>
+	</div>
+    <p>Diesen sehr großen (70cm x 60cm) und schönen "Decoder", der den 5-Bit-Code in das internationale Telegrafenalphabet übersetzt, haben wir vor der Entsorgung an einer
+    Technischen Universität gerettet.<br>
+    Er ist von den dortigen Werkstätten ca. in der Zeit zwischen 1937 und 1949 als aufwändiges Demonstrationsgerät für Vorlesungen und Praktika gebaut worden.<br>
+    Man stellt eine beliebige 5-Bit-Zahl an den groß dimensionierten Schaltern ein, dreht die Codierscheibe und erhält an einer bestimmten Stelle das dazugehörige, oben aufleuchtende
+    Zeichen. Hier würde z.B. bei der Einstellung 10101 der Buchstabe "Y" leuchten.<br>
+    Was damals fortschrittliche Nachrichtentechnik war, dient heute im technikum29 als sogenannte "Dechiffriermaschine". In unserem <a href="/de/lehrerinfo.php#experimente">
+    Experimental-Workshop</a> können Kids und Jugendliche damit Binärtexte entschlüsseln und weiter verarbeiten.</p>
+    <div class="clear"></div>
+
+    <h2>Hellschreiber</h2>
+    
+	<div class="box center auto-bildbreite">
 	    <img src="/shared/photos/kommunikationstechnik/hellschreiber.jpg" width="629" height="245"  />
 		<p class="bildtext">
 		    "Ohne Worte". Was es mit dieser merkwürdigen Anordnung auf sich hat können wir Ihnen nur im technikum29 vorführen!
 		</p>
 	</div>
 	 
-	 <h2>Schreibautomaten</h2>
+    <h2>Schreibautomaten</h2>
 	 
     <div class="box center auto-bildbreite">
 	    <img src="/shared/photos/kommunikationstechnik/olympia-schreibautomat.jpg" width="629" height="242" alt="Schreibautomat" />

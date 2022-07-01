012418aca4bc084977aaa113e8714fabc7e043d2
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-07-01 19:57:49 +0200

    Update personalcomputer.php

--- de/rechnertechnik/personalcomputer.php
+++ de/rechnertechnik/personalcomputer.php
@@ -76,7 +76,7 @@
 	<h3 id="kim1"><b>KIM 1</b></h3><br>
 
 	<div class="box center"> 
-	<img src="/shared/photos/dummy.jpg" width="500" height="275"/>
+	<img src="/shared/photos/rechnertechnik/KIM.jpg" width="321" height="427"/>
 	</div>
 
 	<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam

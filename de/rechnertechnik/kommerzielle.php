32f883e5ceb092f0b877a8afa04b78c6d09272bf
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-07-01 21:58:17 +0200

    Update kommerzielle.php

--- de/rechnertechnik/kommerzielle.php
+++ de/rechnertechnik/kommerzielle.php
@@ -111,8 +111,8 @@
 		<p>Gemeinsam mit Tandberg stellte Siemens 1979 diesen Bürorechner vor: Er basiert auf einer CPU 8080A mit 2 MHz Takt, hat 56 kB RAM und 7 kB ROM 
 			für ein Monitorprogramm. Ein Monochrombildschirm mit 24 Zeilen zu je 80 Zeichen und eine 8"-Floppystation sind in das Gehäuse
 			integriert. Als Betriebssystem wird AMBOSS (BS1S Rev 4.09/D) von Floppy geladen.<br>
-			Zu unserem Rechner haben wir umfangreiche Schulungshandbücher, Serviceunterlagen und Software, u.a. COBOL, BASIC, Assembler und Programme für die Finanzbuchghaltung, das 
-			Personalwesen und zur Datenkommunikation mit Großrechnern. Ein besobderer Glücksfall ist auch , dass wir einen Servicekoffer für den Außendienstservice
+			Zu unserem Rechner haben wir umfangreiche Schulungshandbücher, Serviceunterlagen und Software, u.a. COBOL, BASIC, Assembler und Programme für die Finanzbuchhaltung, das 
+			Personalwesen und zur Datenkommunikation mit Großrechnern. Ein besonderer Glücksfall ist auch , dass wir einen Koffer für den Außendienstservice
 			zu dem Gerät haben, er enthält alle Steckkarten und Baugruppen nochmal als Ersatzteile !</p>
 
 	<h3 id="cpt8800">CPT 8800</h3>

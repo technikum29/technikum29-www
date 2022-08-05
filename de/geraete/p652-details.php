2a3f4c4e4f001a2aa6a5cdd1bd4af8f09d4a206e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-08-05 14:30:15 +0200

    Update p652-details.php

--- de/geraete/p652-details.php
+++ de/geraete/p652-details.php
@@ -8,14 +8,24 @@
 
 <h2><?php print $title; ?></h2>
 
+<p>Im Bestand des technikum29 haben wir 3 Olivetti P652, 2 Schreibsysteme ST4, 2 Magnetbandeinheiten MLU600, eine Festplatte DAS600, 2 Doppel-Cassettenlaufwerke
+CTU1000 sowie einen Lochstreifenstanzer PN20. Wir werden versuchen, aus dem Material eine funktionsfähige Komplettanlage zusammenzustellen. 
+Über den Fortschritt bei der Restaurierung werden wir hier berichten.</p>
+
+<p>In der Ausstellung präsentieren wir z. Zt. eine kleine Kombination aus P652, ST4 und CTU1000
+
+
 <div class="box center">
     <img src="/shared/photos/dummy.jpg" width="594" height="446" alt="P652 Details 1" />
-    <p>In Arbeit...
+    <p>Unsere erste P652 ist in sehr gutem Zustand, sie rechnet und druckt. Die Magnetkarteneinheit muss noch überprüft werden.
     </p>
 </div>
 
 <div class="box center">
 	<img src="/shared/photos/dummy.jpg" width="594" height="424" alt="P652 Details 2" />
-	<p>In Arbeit...
+	<p>Eine der Ersatz-P652en hat eine Erweiterung "GOLU MLU600" eingebaut, sie ist Voraussetzung, um eines der Magnetlaufwerke MLU600
+		in Betrieb nehmen zu können. Leider suchen wir noch nach den passenden Magnetbandcassetten.
     </p>
 </div>
+
+<p> wird fortgesetzt ... </p>

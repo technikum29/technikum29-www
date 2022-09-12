46e66193c0a8d77e892647f0aeccfc33219b832b
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 20:18:11 +0200

    Update Nixdorf820_Module.php

--- de/geraete/Nixdorf820_Module.php
+++ de/geraete/Nixdorf820_Module.php
@@ -7,3 +7,21 @@
 ?>
 
 <h3>Im technikum29 vorhandene Module zur Nixdorf 820</h3>
+
+<p>In unserem Fundus befinden sich folgende Module für die Nixdorf 820:
+</p>
+
+<table>
+	<tr>
+		<th>Bezeichnung</th>   <th>Funktion</th>
+	</tr>
+	<tr>
+		<td>LFI 17</td>   <td>?</td>
+	</tr>	
+	<tr>
+		<td>LFI 170</td>   <td>Fädel-ROM</td>
+	</tr>		
+	<tr>
+		<td>LFI 177</td>   <td>Fädel-ROM</td>
+	</tr>		
+</table>

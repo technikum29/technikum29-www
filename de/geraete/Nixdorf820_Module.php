0fdcb90682e8faeb3cbcf824e4ab17038a171f66
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 16:35:21 +0200

    Create Nixdorf820_Module.php

---
+++ de/geraete/Nixdorf820_Module.php
@@ -0,0 +1,9 @@
+<?php
+	$seiten_id = 'Nixdorf 820 Module';
+	$version = '$Id';
+	$titel = 'Nixdorf 802 Module';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h3>Im technikum29 vorhandene Module zur Nixdorf 820</h3>

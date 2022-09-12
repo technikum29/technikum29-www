c26bca8d4a5c3a0b64508dd50003e8320f0b9ddf
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 20:37:44 +0200

    Update Nixdorf820_Module.php

--- de/geraete/Nixdorf820_Module.php
+++ de/geraete/Nixdorf820_Module.php
@@ -28,39 +28,42 @@
 		<td>LFI 152,154,158</td>   <td>Prozessoren</td>
 	</tr>	
 	<tr>
-		<td>LFI 161,162,163,166,1607</td>   <td>Kernspeichermodule</td>
+		<td>LFI 161,162,163,166    </td>   <td>Kernspeichermodule</td>
 	</tr>		
 	<tr>
-		<td>LFI 184</td>   <td>EA-Karte (D10) für Drucker, Lochkarten und Lochstreifen</td>
+		<td>LFI 1607</td>          <td>Kernspeichermodule</td>
 	</tr>	
 	<tr>
-		<td>LFI 186</td>   <td>EA-Karte (D9) für Magnetkonten</td>
+		<td>LFI 184</td>           <td>EA-Karte (D10) für Drucker, Lochkarten und Lochstreifen</td>
 	</tr>	
 	<tr>
-		<td>0191</td>   <td>Netzteil</td>
+		<td>LFI 186</td>           <td>EA-Karte (D9) für Magnetkonten</td>
+	</tr>	
+	<tr>
+		<td>LFI 0191</td>          <td>Netzteil</td>
 	</tr>
 	<tr>
-		<td>201, 202</td>   <td>batteriegepufferte Halbleiterspeicher RAM</td>
+		<td>201, 202</td>          <td>batteriegepufferte Halbleiterspeicher RAM</td>
 	</tr>
 	<tr>
-		<td>LFI 037</td>   <td>?</td>
+		<td>LFI 037</td>           <td>?</td>
 	</tr>	
 	<tr>
-		<td>LFI 310</td>   <td>EA-Karte (D5) </td>
+		<td>LFI 310</td>          <td>EA-Karte (D5) </td>
 	</tr>	
 	<tr>
-		<td>LFI 318</td>   <td>EA-Karte für 2. Magnetbandcassette MBC</td>
+		<td>LFI 318</td>          <td>EA-Karte für 2. Magnetbandcassette MBC</td>
 	</tr>	
 	<tr>
-		<td>LFI 333</td>   <td>Recheneinheit für den Centronix-Drucker</td>
+		<td>LFI 333</td>          <td>Recheneinheit für den Centronix-Drucker</td>
 	</tr>
 	<tr>
-		<td>LFI 402</td>   <td>Umschaltplatte</td>
+		<td>LFI 402</td>          <td>Umschaltplatte</td>
 	</tr>	
 	<tr>
-		<td>LFI 5200, 571</td>   <td>?</td>
+		<td>LFI 5200, 571</td>    <td>?</td>
 	</tr>	
 	<tr>
-		<td>LFI 972,973,974,978</td>   <td>?</td>
+		<td>LFI 972,973,974,978   </td>   <td>?</td>
 	</tr>
 </table>

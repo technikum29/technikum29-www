452c8e26ce4e5744a18265cdd546a5f4598a156e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 15:55:16 +0200

    Update Nixdorf820_Details.php

--- de/geraete/Nixdorf820_Details.php
+++ de/geraete/Nixdorf820_Details.php
@@ -10,8 +10,21 @@
 
 	<p>
 	*** ACHTUNG BAUSTELLE ! ***
-	</p>
+</p>
 
 	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer Nixdorf 820 und die Wiederinbetriebnahme der Maschine.
 	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
-	</p>
+</p>
+
+<h3>Herkunft der NIXDORF 820</h3>
+
+	<p> Unsere Nixdorf 820 war im VW-Autohaus am Kurfürstenplatz (Frankfurt Bockenheim) im Einsatz. Neben der Hardwre wurden dem
+		technikum29 mehrere Regalmeter mit Schulungsunterlagen und Beispielen zu den verschiedenen Anwendungen mitgegeben. 
+		Viele Pakete mit blanko Magnetkonenkarten zeugen noch von dieser Zeit.
+</p>
+
+<h3>aktueller Bestand zur Nixdorf 820</h3>
+
+	<p>Zur Hardware besitzen wir umfangreiche technische Unterlagen und Serviceanleitungen, die wir 
+		<a class="go" href="/de/geraete/Nixdorf820_Manuals.php">hier</a> auflisten und zukünftig zum Download anbieten werden.
+</p>

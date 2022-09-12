5b09090b0effa687d5f98f2160ff4f5856747509
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 15:57:42 +0200

    Create Nixdorf820_Manuals.php

---
+++ de/geraete/Nixdorf820_Manuals.php
@@ -0,0 +1,10 @@
+<?php
+	$seiten_id = 'Nixdorf 820 technische Unterlagen';
+	$version = '$Id';
+	$titel = 'Nixdorf 802 technische Unterlagen';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h3>Im technikum29 vorhandene Manuals zur Nixdorf 820</h3>
+

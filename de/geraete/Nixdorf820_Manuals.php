158086ab480341103bd246afb12c341a48e20cd1
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 19:35:24 +0200

    Update Nixdorf820_Manuals.php

--- de/geraete/Nixdorf820_Manuals.php
+++ de/geraete/Nixdorf820_Manuals.php
@@ -8,5 +8,24 @@
 
 <h3>Im technikum29 vorhandene Manuals zur Nixdorf 820</h3>
 
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_1_und_2.jpg" width="400"  height="256"/>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_1_und_2_Inhalt.jpg" width="365"  height="256"/>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_3.jpg" width="175"  height="256"/>
+		<p class="bildtext"><b>Nixdorf 820 </b> in unserer Ausstellung (links), rechts der Lochkartenstanzer im Depot</p>
+	</div>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/" width="573"  height="325"/>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/" width="218"  height="325"/>
+		<p class="bildtext"><b>Nixdorf 820 </b> in unserer Ausstellung (links), rechts der Lochkartenstanzer im Depot</p>
+	</div>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/" width="573"  height="325"/>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/" width="218"  height="325"/>
+		<p class="bildtext"><b>Nixdorf 820 </b> in unserer Ausstellung (links), rechts der Lochkartenstanzer im Depot</p>
+	</div>
+
 	<p>zum Download unserer Scans geht es <a href="/de/geraete/Nixdorf820_Manuals_PDFs">hier</a>
 </p>

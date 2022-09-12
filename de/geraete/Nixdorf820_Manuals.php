23daa8800d9b17712b05900c20004b7799c88473
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 17:53:42 +0200

    Update Nixdorf820_Manuals.php

--- de/geraete/Nixdorf820_Manuals.php
+++ de/geraete/Nixdorf820_Manuals.php
@@ -8,5 +8,5 @@
 
 <h3>Im technikum29 vorhandene Manuals zur Nixdorf 820</h3>
 
-	<p>zum Download unserer Scans geht es <a href="lerncomputer">hier</a>
+	<p>zum Download unserer Scans geht es <a href="/de/geraete/Nixdorf820_Manuals_PDFs">hier</a>
 </p>

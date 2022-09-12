2469e0c9dedf288d153fb50de7f1b0c58f4ce046
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-12 19:44:20 +0200

    Update Nixdorf820_Manuals.php

--- de/geraete/Nixdorf820_Manuals.php
+++ de/geraete/Nixdorf820_Manuals.php
@@ -12,13 +12,14 @@
 		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_1_und_2.jpg" width="300"  height="205"/>
 		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_1_und_2_Inhalt.jpg" width="293"  height="205"/>
 		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_3.jpg" width="140"  height="205"/>
-		<p class="bildtext"><b>Magnetkontencomputer</b> Systembeschreibung Teil 1 - 3</p>
+		<p class="bildtext">Magnetkontencomputer Systembeschreibung Teil 1 - 3</p>
 	</div>
 
 <div class="box center"> 
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/" width="573"  height="325"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/" width="218"  height="325"/>
-		<p class="bildtext"><b>Nixdorf 820 </b> in unserer Ausstellung (links), rechts der Lochkartenstanzer im Depot</p>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_15_Service_Manual.jpg" width="148"  height="205"/>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_25_35_Service_Manual.jpg" width="148"  height="205"/>
+		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_30_tech_Handbuch.jpg" width="140"  height="205"/>
+		<p class="bildtext">Service Manual 820/15 und 820/25/35 sowie techn. Handbuch 820/30</p>
 	</div>
 
 <div class="box center"> 

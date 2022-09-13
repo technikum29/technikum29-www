9d927fd53727e7ff70f3159b1a8251547171b3a7
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-13 17:01:25 +0200

    Update Nixdorf820_Module.php

--- de/geraete/Nixdorf820_Module.php
+++ de/geraete/Nixdorf820_Module.php
@@ -110,7 +110,7 @@
 
 
 <div class="box left"> 
-		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_4.jpg" width="270"  height="105"/>
+		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_4.jpg" width="135"  height="52"/>
 		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_1.jpg" width="146"  height="210"/>
 		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_2.jpg" width="285"  height="210"/>
 		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_3.jpg" width="146"  height="210"/>

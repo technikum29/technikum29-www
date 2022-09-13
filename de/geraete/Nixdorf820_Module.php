fc3faeacdc81b7d762f0dd37c830197ac65809e1
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-13 17:09:45 +0200

    Update Nixdorf820_Module.php

--- de/geraete/Nixdorf820_Module.php
+++ de/geraete/Nixdorf820_Module.php
@@ -107,7 +107,7 @@
 <h3>Prozessormodul <b>LFI 154-02</b></h3>
 
 
-<div class="box left"> 
+<div class="box center"> 
 		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_5.jpg" width="270"  height="105"/>
 		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_1.jpg" width="146"  height="210"/>
 		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_2.jpg" width="294"  height="210"/>

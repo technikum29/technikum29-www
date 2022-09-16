b2ce90a28683a207eacd120fe98abfc50ec2a5ce
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-16 10:20:02 +0200

    Create Q1_lite_Details.php

---
+++ de/geraete/Q1_lite/Q1_lite_Details.php
@@ -0,0 +1,65 @@
+<?php
+	$seiten_id = 'Q1 lite';
+	$version = '$Id';
+	$titel = 'Q1 lite';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h2><?php print $titel; ?></h2>
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+</p>
+
+	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer Q1 lite und die Wiederinbetriebnahme der Maschine.
+	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
+</p>
+
+<h3>Herkunft der Q1 lite</h3>
+
+	<p> _____________________________________
+</p>
+
+<h3>einige Detailbilder unserer Q1</h3>
+
+	<p>_____
+</p>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jp" width="573"  height="325"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jpg" width="218"  height="325"/>
+		<p class="bildtext"><b>Q1 lite</b> ___________</p>
+	</div>
+
+	<p>______________________
+		<p/>
+<p>__________________________________-
+	<p/>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jpg" width="427"  height="320"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jp" width="427"  height="320"/>
+		<p class="bildtext">Q1 lite</b> ____________________________</p>
+	</div>
+
+<p>_____________________
+</p>
+
+
+
+<h3>Wiederinbetriebnahme der Anlage</h3>
+
+
+	<p>______________________
+		<p/>
+<p>__________________________________-
+	<p/>
+
+<h3>Broschüren und Manuals zum Download</h3>
+
+<p>zum Download unserer Scans geht es <a href="/de/geraete/Q1_lite/Q1_PDFs">hier</a>
+</p>
+
+	<p> *** wird fortgesetzt *** 
+</p>

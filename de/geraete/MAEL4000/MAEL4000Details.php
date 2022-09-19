4ef8f8721e7c3eb167be2a905f40254938646e9d
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-19 10:42:02 +0200

    Create MAEL4000Details.php

---
+++ de/geraete/MAEL4000/MAEL4000Details.php
@@ -0,0 +1,58 @@
+<?php
+	$seiten_id = 'MAEL 4000';
+	$version = '$Id';
+	$titel = 'MAEL 4000';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $titel; ?></h2>
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+</p>
+
+	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer MAEL und die Wiederinbetriebnahme der Maschine.
+	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
+</p>
+
+
+
+<h3>einige Detailbilder unserer MAEL</h3>
+
+	<p>Zum Vergrößern der Bilder "Rechtsklick", dann "in neuem Tab öffnen" wählen</p>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Blick_ins-Chassis.jpg" width="700"  height="434"/>
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Hauptplatine.jpg" width="300"  height="200"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_PCB_Display.jpg" width="382"  height="200"/>
+		<p class="bildtext"><b>Q1 lite</b> Hauptplatine (links) und Displayplatine (rechts)</p>
+	</div>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_links.jpg" width="350"  height="216"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_rechts.jpg" width="352"  height="216"/>
+		<p class="bildtext"><b>Q1 lite</b> Netzteilplatine linke und rechte Seite</p>
+	</div>
+
+
+
+<h3>Wiederinbetriebnahme der Anlage</h3>
+
+
+	<p>______________________
+		<p/>
+	<p> *** wird fortgesetzt *** 
+		</p>
+
+<h3>Broschüren und Manuals zum Download</h3>
+
+<p>zum Download unserer Scans geht es <a href="/de/geraete/MAEL4000/MAEL4000_PDFs">hier</a>
+</p>
+
+

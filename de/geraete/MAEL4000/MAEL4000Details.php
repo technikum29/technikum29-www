20cc31d8b6365ed846f6743672b63c30c7202b9f
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-19 11:37:58 +0200

    Update MAEL4000Details.php

--- de/geraete/MAEL4000/MAEL4000Details.php
+++ de/geraete/MAEL4000/MAEL4000Details.php
@@ -130,9 +130,8 @@ In einem 2. Rack, der MAEL Interface Unit 4001 SerNo 22735 befinden sich neben d
 </p>
 
 <div class="box center"> 
-		<img src="/de/geraete/MAEL4000/MAEL4000_Bilder/MAEL4000_4001_innen2.jpeg" width="250"  height="207"/>
-		<img src="/de/geraete/MAEL4000/MAEL4000_Bilder/MAEL4000_4001_innen3.jpeg" width="250"  height="66"/>
-		<img src="/de/geraete/MAEL4000/MAEL4000_Bilder/MAEL4000_4001_innen.jpeg" width="250"  height="152"/>
+		<img src="/de/geraete/MAEL4000/MAEL4000_Bilder/MAEL4000_4001_innen2.jpeg" width="180"  height="150"/>
+		<img src="/de/geraete/MAEL4000/MAEL4000_Bilder/MAEL4000_4001_innen3.jpeg" width="570"  height="150"/>
 		<p class="bildtext"><b>MAEL 4001</b> Blick in die Erweiterungseinheit</p>
 	</div>
 

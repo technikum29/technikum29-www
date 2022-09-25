12a03f9a3ea9870897a4f30d8d47e5f65a0b746c
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-25 14:21:01 +0200

    Add files via upload

---
+++ de/geraete/TA1000/TA1000_Module.php
@@ -0,0 +1,184 @@
+<?php
+	$seiten_id = 'Nixdorf 820 Module';
+	$version = '$Id';
+	$titel = 'Nixdorf 820 Module';
+	
+	require '../../lib/technikum29.php';
+?>
+
+<h3>Im technikum29 vorhandene Module zur Nixdorf 820</h3>
+
+<p>In unserem Fundus befinden sich folgende Module für die Nixdorf 820:
+</p>
+
+<table>
+	<tr>
+		<th>Modul - Bezeichnung   </th>     
+		<th>Funktion</th>
+	</tr>
+	<tr>
+		<td>LFI 017</td>   
+		<td>Ringkernspeicher-ROM für max 2048 Befehlsworte zu 12 Bit</td>
+	</tr>	
+	<tr>
+		<td>LFI 037</td>   
+		<td>Ringkernspeicher-ROM </td>
+	</tr>	
+	<tr>
+		<td>LFI 119</td>          
+		<td>Netzteil</td>
+	</tr>
+	<tr>
+		<td>LFI 152,154,158</td>   
+		<td>Prozessoren</td>
+	</tr>
+	<tr>
+		<td>LFI 1607</td>          
+		<td>Kernspeichermodul 4 K</td>
+	</tr>
+	<tr>
+		<td>LFI 161,162,163,166</td>   
+		<td>Kernspeichermodule mit 128, 64, 32 bzw. 512 Speicherworten</td>
+	</tr>		
+	<tr>
+		<td>LFI 177</td>   
+		<td>Stäbchenspeicher-ROM für max 4096 Befehlsworte zu 12 Bit</td>
+	</tr>	
+	<tr>
+		<td>LFI 184</td>           
+		<td>EA-Karte (D10, 7, 8) für Drucker, Lochkarten und Lochstreifen Erst- und Zweitgeräte</td>
+	</tr>	
+	<tr>
+		<td>LFI 186</td>           
+		<td>EA-Karte (D9) für Magnetkonten-Vorsteckeinrichtung MKE</td>
+	</tr>	
+	<tr>
+		<td>201, 202</td>          
+		<td>batteriegepufferter Halbleiterspeicher RAM 48 kBit aus 48*IM6508</td>
+	</tr>	
+	<tr>
+		<td>LFI 310</td>          
+		<td>EA-Karte (D5) 2*MBC 20732</td>
+	</tr>	
+	<tr>
+		<td>LFI 318</td>          
+		<td>EA-Karte für 2. Magnetbandcassette MBC</td>
+	</tr>	
+	<tr>
+		<td>LFI 333</td>          
+		<td>Recheneinheit für den Centronix-Drucker</td>
+	</tr>
+	<tr>
+		<td>LFI 380</td>          
+		<td>Programmträger (2048 Befehlsworte zu 12 Bit) für LFI 177</td>
+	</tr>
+	<tr>
+		<td>LFI 381</td>          
+		<td>Programmträger (2048 Befehlsworte zu 12 Bit) für LFI 177</td>
+	</tr>
+	<tr>
+		<td>LFI 420</td>          
+		<td>Umschaltplatte für Chassis 540 für Modell 820/35</td>
+	</tr>		
+	<tr>
+		<td>LFI 972</td>   
+		<td>Nadeldrucker Steuerelektronik SE 1</td>
+	</tr>
+	<tr>
+		<td>LFI 973</td>   
+		<td>Nadeldrucker Steuerelektronik SE 2</td>
+	</tr>
+	<tr>
+		<td>LFI 974</td>   
+		<td>Nadeldrucker Verstärkerelektronik</td>
+	</tr>
+</table>
+
+<h3>Fotos einiger Module:</h3>
+
+<p>Es folgen Fotos ausgesuchter Module aus unserer Nixdorf 820. Ausnahme ist der Prozessor LFI 152, noch in komplett diskreter Technologie,
+	der nicht in unserer Maschine im Einsatz war.</p>
+<p>Zum Vergrößern der Bilder "Rechtsklick", dann "in neuem Tab öffnen" wählen</p>
+
+<h3>Prozessormodul <b>LFI 152</b>, noch in komplett diskreter Technologie ohne ICs aufgebaut !</h3>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_152/LFI_152_6.jpg" width="200"  height="72"/>
+		<img src="/shared/photos/rechnertechnik/LFI_152/LFI_152_1.jpg" width="146"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_152/LFI_152_2.jpg" width="294"  height="210"/>
+	<p class="bildtext">  </p>
+	</div>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_152/LFI_152_3.jpg" width="280"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_152/LFI_152_4.jpg" width="285"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_152/LFI_152_5.jpg" width="141"  height="210"/>
+	<p class="bildtext"></p>
+	</div>
+
+<h3>Prozessormodul <b>LFI 154-02</b></h3>
+
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_5.jpg" width="200"  height="72"/>
+		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_1.jpg" width="146"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_2.jpg" width="294"  height="210"/>
+	<p class="bildtext">  </p>
+	</div>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_3.jpg" width="280"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_154_02/LFI_154_02_4.jpg" width="146"  height="210"/>
+	<p class="bildtext"></p>
+	</div>
+
+
+
+<h3>Kernspeicher <b>LFI 166</b> </h3>
+
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_4.jpg" width="200"  height="72"/>
+		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_1.jpg" width="146"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_2.jpg" width="285"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_166/LFI_166_3.jpg" width="146"  height="210"/>
+	<p class="bildtext">  </p>
+	</div>
+
+
+<h3>Batteriegepufferter Halbleiterspeicher <b>Nr. 202</b> </h3>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_202/202_3.jpg" width="200"  height="72"/>
+		<img src="/shared/photos/rechnertechnik/LFI_202/202_1.jpg" width="294"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_202/202_2.jpg" width="294"  height="210"/>
+	<p class="bildtext">  </p>
+	</div>
+
+<h3>Ringkernspeicher-ROM <b>LFI 017</b> </h3>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_017/017_1.jpg" width="200"  height="72"/>
+		<img src="/shared/photos/rechnertechnik/LFI_017/017_2.jpg" width="294"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_017/017_3.jpg" width="294"  height="210"/>
+	<p class="bildtext">  </p>
+	</div>
+
+<h3>Stäbchenspeicher-ROM <b>LFI 177</b> </h3>
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_177/177_1.jpeg" width="200"  height="72"/>
+		<img src="/shared/photos/rechnertechnik/LFI_177/177_2.jpeg" width="294"  height="210"/>
+		<img src="/shared/photos/rechnertechnik/LFI_177/177_3.jpeg" width="294"  height="210"/>
+	<p class="bildtext">mitte: Draufsicht auf den Stäbchenspeicher <b>LFI 177</b>, rechts: mit hochgeklapptem Programmträger <b>LFI 381</b> </p>
+	</div>
+
+
+<div class="box center"> 
+		<img src="/shared/photos/rechnertechnik/LFI_177/177_4.jpeg" width="200"  height="170"/>
+		<img src="/shared/photos/rechnertechnik/LFI_177/177_5.jpeg" width="203"  height="170"/>
+		<img src="/shared/photos/rechnertechnik/LFI_177/177_6.jpeg" width="320"  height="226"/>
+	<p class="bildtext">links und mitte: <b>LFI 381</b> Programmträger mit Fädelprogramm, rechts: Rückseite <b>LFI 177</b> </p>
+	</div>
+
+<p>*** wird fortgeführt ***</p>

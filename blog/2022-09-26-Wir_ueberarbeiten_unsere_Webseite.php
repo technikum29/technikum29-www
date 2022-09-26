ebe6dd778764ede23d6afc8a0143bf539b8dd363
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-26 11:56:01 +0200

    Add files via upload

---
+++ blog/2022-09-26-Wir_ueberarbeiten_unsere_Webseite.php
@@ -0,0 +1,31 @@
+<?php
+	$blog_title = "Wir überarbeiten unsere Webseite";
+	$blog_title_kurz = "Wir überarbeiten unsere Webseite";
+	$blog_author = "Roland";
+	$blog_date = "2022-09-26";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+
+
+<p>Unsere Webseite ist über viele Jahre entstanden. Nach dem Tod des Museumsgründers Heribert Müller in 2018 haben wir die Seite nur ganz behutsam an wenigen Stellen geändert. </p>
+
+<p>Wir haben die letzten Jahre genutzt, dem technikum29 einen neuen Sammlungsschwerpunkt zu geben: wir werden uns ausschließlich auf Rechen- und Rechnertechnik bis ca 1980 konzentrieren. Dazu kommt die Telegrafie, die Fernschreib- und FAXtechnik sowie das BTX: alles Techniken, die in einem direkten bezug zur Entwicklung der Computertechnik stehen. Nicht mehr im Fokus steht die Rundfunk- Magnetton und Fernsehtechnik. Aus Platzgründen musste sie aus der Ausstellung weichen.</p>
+
+<p>Um diese Veränderungen auch auf der Webseite zu dokumentieren, haben wir begonnen, diese sukzessive anzupassen. Obwohl von manchem Seitenbesucher gedrängt, doch auf eine ganz neue Webpräsenz mit modernster Technik zu setzen, haben wir beschlossen, das jetzige Seitenformat und die zugrundeliegende Technik beizubehalten - auch als Hommage an die Grundlagen, die Heribert mit dem Seitenkonzept gelegt hat.</p>
+
+<p>Was hat sich geändert bzw. wird sich ändern ?</p>
+<p>Seiten, die nicht mehr zur Ausstellung gehören, wandern in die Rubrik <a href="/de/archiv/rundfunk.php">Archiv</a>.</p>
+<p>Die aktuellen Neuzugänge wurden in die bestehende Seitenstruktur eingepflegt wie zB <a href="/de/rechnertechnik/personalcomputer.php#ibm5100">unsere IBM5100</a></p>
+<p>Viele Exponate bekamen in der Vorstellung einen weiterführenden Link wie zB. 
+ <a href="/de/geraete/Q1_lite/Q1_lite_Details.php">unsere Q1</a> oder <a href="/de/geraete/Nixdorf820_Details.php">die Nixdorf 820</a>. Diese Detailseiten zu den geräten zeigen Bilder, berichten über die Instandsetzung und enthalten einen Downloadbereich für SCans von Unterlagen aus dem Museum. Neben den o.g. Maschinen ist das bereits auch bei der MAEL4000, der Addelektra SR22 und der TA1000 angelegt ! </p>
+
+<p>Viel Spass beim Stöbern !! </p>
+
+
+  
+<?php print_author_info(); ?>

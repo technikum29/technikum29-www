ad80f821ba8d1aecb8b51ddd8a925c2284e44c6b
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-26 12:05:59 +0200

    Update 2022-09-26-Wir_ueberarbeiten_unsere_Webseite.php

--- blog/2022-09-26-Wir_ueberarbeiten_unsere_Webseite.php
+++ blog/2022-09-26-Wir_ueberarbeiten_unsere_Webseite.php
@@ -15,7 +15,7 @@
 <p>Unsere Webseite ist über viele Jahre entstanden. Nach dem Tod des Museumsgründers Heribert Müller in 2018 haben wir die Seite nur 
 	ganz behutsam an wenigen Stellen geändert. </p>
 
-<p>Wir haben die letzten Jahre genutzt, dem technikum29 einen neuen Sammlungsschwerpunkt zu geben: wir werden uns ausschließlich auf 
+<p>Wir haben die letzten Jahre genutzt, dem technikum29 einen <b>neuen Sammlungsschwerpunkt</b> zu geben: wir werden uns ausschließlich auf 
 	Rechen- und Rechnertechnik bis ca 1980 konzentrieren. Dazu kommt die Telegrafie, die Fernschreib- und FAXtechnik sowie das BTX: 
 	alles Techniken, die in einem direkten Bezug zur Entwicklung der Computertechnik stehen. Nicht mehr im Fokus stehen die Rundfunk- 
 	Magnetton- und Fernsehtechnik. Aus Platzgründen mussten sie aus der Ausstellung weichen.</p>
@@ -25,7 +25,7 @@
 	Seitenformat und die zugrundeliegende Technik beizubehalten - auch als Hommage an die Grundlagen, die Heribert mit dem Seitenkonzept 
 	gelegt hat.</p>
 
-<p>Was hat sich geändert bzw. wird sich ändern ?</p>
+<p><b>Was hat sich geändert bzw. wird sich ändern ?</b></p>
 <p>Seiten, die nicht mehr zur Ausstellung gehören, wandern in die Rubrik <a href="/de/archiv/rundfunk.php">Archiv</a>.</p>
 <p>Die aktuellen Neuzugänge wurden in die bestehende Seitenstruktur eingepflegt wie zB <a href="/de/rechnertechnik/personalcomputer.php#ibm5100">unsere IBM5100</a></p>
 <p>Viele Exponate bekamen in der Vorstellung einen weiterführenden Link wie zB. 

28759b97572f866693b0cdfcb2754d8fd3955660
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-28 20:24:56 +0200

    Update Nixdorf820_Details.php

--- de/geraete/Nixdorf820/Nixdorf820_Details.php
+++ de/geraete/Nixdorf820/Nixdorf820_Details.php
@@ -30,8 +30,8 @@
 </p>
 
 <div class="box center"> 
-		<img src="/shared/photos/rechnertechnik/820_Geraetebilder/820_Stand_2022.jpg" width="573"  height="325"/>
-		<img src="/shared/photos/rechnertechnik/820_Geraetebilder/820_Stanzer_Stand_2022.jpg" width="218"  height="325"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Stand_2022.jpg" width="573"  height="325"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Stanzer_Stand_2022.jpg" width="218"  height="325"/>
 		<p class="bildtext"><b>Nixdorf 820 </b> in unserer Ausstellung (links), rechts der Lochkartenstanzer im Depot</p>
 	</div>
 
@@ -42,8 +42,8 @@
 	<p/>
 
 <div class="box center"> 
-		<img src="/shared/photos/rechnertechnik/820_Geraetebilder/820_Geraeteschrank_1.jpg" width="427"  height="320"/>
-		<img src="/shared/photos/rechnertechnik/820_Geraetebilder/820_Geraeteschrank_2.jpg" width="427"  height="320"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Geraeteschrank_1.jpg" width="427"  height="320"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Geraeteschrank_2.jpg" width="427"  height="320"/>
 		<p class="bildtext">Zwei <b>Nixdorf 820 </b> Modulschränke 0753 mit Netzteilen und Modulen</p>
 	</div>
 

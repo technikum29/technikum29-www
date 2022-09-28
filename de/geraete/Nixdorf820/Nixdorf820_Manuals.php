a93afc64f4b5228da0c77141fef4e7a98dadda34
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-28 20:27:14 +0200

    Update Nixdorf820_Manuals.php

--- de/geraete/Nixdorf820/Nixdorf820_Manuals.php
+++ de/geraete/Nixdorf820/Nixdorf820_Manuals.php
@@ -9,26 +9,26 @@
 <h3>Im technikum29 vorhandene Manuals zur Nixdorf 820</h3>
 
 <div class="box left"> 
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_1_und_2.jpg" width="300"  height="205"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_1_und_2_Inhalt.jpg" width="293"  height="205"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_MKC_Teil_3.jpg" width="140"  height="205"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_MKC_Teil_1_und_2.jpg" width="300"  height="205"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_MKC_Teil_1_und_2_Inhalt.jpg" width="293"  height="205"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_MKC_Teil_3.jpg" width="140"  height="205"/>
 		<p class="bildtext">Magnetkontencomputer Systembeschreibung Teil 1 - 3</p>
 	</div>
 
 <div class="box left"> 
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/Nixdorf820_Programmierhandbuch_T29.jpeg" width="177"  height="250"/>	
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_15_Service_Manual.jpg" width="181"  height="250"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_25_35_Service_Manual.jpg" width="181"  height="250"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_30_tech_Handbuch.jpg" width="171"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/Nixdorf820_Programmierhandbuch_T29.jpeg" width="177"  height="250"/>	
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_15_Service_Manual.jpg" width="181"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_25_35_Service_Manual.jpg" width="181"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_30_tech_Handbuch.jpg" width="171"  height="250"/>
 		<p class="bildtext">Programmierhandbuch 820, Service Manual 820/15 und 820/25/35 sowie techn. Handbuch 820/30</p>
 	</div>
 
 
 <div class="box left"> 
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_Lochkartenleser_0043_44_Service_Manual.jpg" width="178"  height="250"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_Lochkartenstanzer_009x.jpg" width="184"  height="250"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_Lochkartenstanzer_0091_92_Aufbaulehrgang.jpg" width="180"  height="250"/>
-		<img src="/shared/photos/rechnertechnik/820_Manuals_Bilder/820_Magnetbandkassette_0733.jpg" width="164"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Lochkartenleser_0043_44_Service_Manual.jpg" width="178"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Lochkartenstanzer_009x.jpg" width="184"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Lochkartenstanzer_0091_92_Aufbaulehrgang.jpg" width="180"  height="250"/>
+		<img src="/de/geraete/Nixdorf820/Nixdorf820_Bilder/820_Magnetbandkassette_0733.jpg" width="164"  height="250"/>
 		<p class="bildtext">Service Manual Lochkartenleser 0043 und 0044, "Mechanik und Elektronik" sowie "Aufbaulehrgang" Lochkartenstanzer 
 		0091/92/94, Service-Unterlagen zur Magnetbandkassette 0733</p>
 	</div>

ad21c11ed5ad09efcb2ab473395b3c840cea9501
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-29 11:26:40 +0200

    Update siemens_6610_Details.php

--- de/geraete/Siemens_6610/siemens_6610_Details.php
+++ de/geraete/Siemens_6610/siemens_6610_Details.php
@@ -3,6 +3,43 @@
 	$version = '$Id';
 	$titel = 'Siemens System 6610';
 	
-	require '../../lib/technikum29.php';
+	require '../../../lib/technikum29.php';
 ?>
     <h2><?php print $title; ?></h2>
+
+
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+</p>
+
+	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer Siemens System 6610  und die Wiederinbetriebnahme der Maschine.
+	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
+</p>
+
+<h3>einige Detailbilder unserer Q1</h3>
+
+	<p>Zum Vergrößern der Bilder "Rechtsklick", dann "in neuem Tab öffnen" wählen</p>
+
+<div class="box center"> 
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/Q1_Blick_ins-Chassis.jpg" width="700"  height="434"/>
+		<p class="bildtext"><b>Siemens 6610</b> Blick in die geöffnete Maschine.</p>
+	</div>
+
+
+
+<h3>Wiederinbetriebnahme der Anlage</h3>
+
+
+	<p>_________
+		<p/>
+	<p> *** wird fortgesetzt *** 
+		</p>
+
+<h3>Broschüren und Manuals zum Download</h3>
+
+<p>zum Download unserer Scans geht es <a href="/de/geraete/Siemens_6610/Siemens_6610_PDFs">hier</a>
+</p>
+
+
+

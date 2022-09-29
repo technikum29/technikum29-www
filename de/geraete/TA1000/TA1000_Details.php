c2d60b95bf7923397160544690486031783125ed
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-29 10:13:10 +0200

    Update TA1000_Details.php

--- de/geraete/TA1000/TA1000_Details.php
+++ de/geraete/TA1000/TA1000_Details.php
@@ -16,6 +16,10 @@
 	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
 </p>
 
+<h3>Wie kam die TA1000 ins technikum29?</h3>
+<p>Hier Links zur <a href="/blog/2022-04-27-Neuzugaenge.php">Herkunft</a> und zur 
+	<a href="/blog/2022-06-06-Neuzugaenge-Kassel-2022.php">Ankunft im technikum29</a>.
+</p>
 
 <h3>Aufbau unserer TA1000</h3>
 

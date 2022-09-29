bde90bacecb84b88f42c7eef3e331dfa92dbff9f
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-29 11:55:34 +0200

    Update siemens_6610_Details.php

--- de/geraete/Siemens_6610/siemens_6610_Details.php
+++ de/geraete/Siemens_6610/siemens_6610_Details.php
@@ -13,25 +13,42 @@
 	*** ACHTUNG BAUSTELLE ! ***
 </p>
 
-	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer Siemens System 6610  und die Wiederinbetriebnahme der Maschine.
+	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserem Siemens System 6610 und die Wiederinbetriebnahme der Maschine.
 	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
 </p>
 
-<h3>einige Detailbilder unserer Q1</h3>
+<h3>einige Detailbilder</h3>
 
 	<p>Zum Vergrößern der Bilder "Rechtsklick", dann "in neuem Tab öffnen" wählen</p>
 
 <div class="box center"> 
-		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/Q1_Blick_ins-Chassis.jpg" width="700"  height="434"/>
-		<p class="bildtext"><b>Siemens 6610</b> Blick in die geöffnete Maschine.</p>
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/6610_offen_hinten.jpeg" width="467"  height="350"/>
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/6610_offen_hochgeklappt.jpeg" width="362"  height="350"/>
+		<p class="bildtext"><b>Siemens 6610</b> Blick in die geöffnete Maschine. Das Chassis ist sehr reparaturfreundkich aufgebaut, 
+			das Gerät ist im aufgeklappten Zustand betriebsfähig !</p>
 	</div>
 
+<div class="box center"> 
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/6610_Meldung_1.jpeg" width="307"  height="250"/>
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/6610_Meldung_2.jpeg" width="165"  height="250"/>
+		<p class="bildtext"><b>Siemens 6610</b>Start- und Systemmeldungen </p>
+	</div>
 
-
+<div class="box center"> 
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/6610_Meldung_3.jpeg" width="308"  height="250"/>
+		<img src="/de/geraete/Siemens_6610/Siemens_6610_Bilder/6610_Meldung_4.jpeg" width="417"  height="250"/>
+		<p class="bildtext"><b>Siemens 6610</b>Start- und Systemmeldungen </p>
+	</div>
+<p>
+</p>
 <h3>Wiederinbetriebnahme der Anlage</h3>
 
 
-	<p>_________
+	<p>Die 6610 war nach kleineren Reparaturen im Netzteil betriebsbereit (Ta-Elkos gewechselt, Überspannungs-Schutzschaltung repariert, CRT-Hochspannungsteil 
+		mit neuem Leistungstransistor versehen). Zu unserem Bestand gehört ein Servicekoffer (in der Größe eines Überseekoffers der 30er Jahre !), der alle 
+		Baugruppen teils mehrfach enthält, was die Fehlersuche sehr vereinfacht !
+</p>
+<p>
 		<p/>
 	<p> *** wird fortgesetzt *** 
 		</p>

9e35ce311980c2569c2ab5e70912b393f0443d53
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-09-30 15:26:13 +0200

    Add files via upload

---
+++ de/geraete/Q1_lite/Q1_lite_Details.php
@@ -0,0 +1,76 @@
+<?php
+	$seiten_id = 'Q1 lite';
+	$version = '$Id';
+	$titel = 'Q1 lite';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $titel; ?></h2>
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+</p>
+
+	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer Q1 lite und die Wiederinbetriebnahme der Maschine.
+	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
+</p>
+
+
+
+<h3>einige Detailbilder unserer Q1</h3>
+
+	<p>Zum Vergrößern der Bilder "Rechtsklick", dann "in neuem Tab öffnen" wählen</p>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Blick_ins-Chassis.jpg" width="700"  height="434"/>
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Hauptplatine.jpg" width="300"  height="200"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_PCB_Display.jpg" width="382"  height="200"/>
+		<p class="bildtext"><b>Q1 lite</b> Hauptplatine (links) und Displayplatine (rechts)</p>
+	</div>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_links.jpg" width="350"  height="216"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_rechts.jpg" width="352"  height="216"/>
+		<p class="bildtext"><b>Q1 lite</b> Netzteilplatine linke und rechte Seite</p>
+	</div>
+
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jpg" width="300"  height="225"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display_nah.jpg" width="372"  height="225"/>
+		<p class="bildtext"><b>Q1 lite</b> Plasmadisplay Gesamtansicht und Nahaufnahme</p>
+	</div>
+
+<div class="box center"> 
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Typschild.jpeg" width="281"  height="225"/>
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Printer_Typschild.jpeg" width="340"  height="225"/>
+		<p class="bildtext"><b>Q1 lite</b> Typenschild Computer und Drucker</p>
+	</div>
+
+
+
+<h3>Wiederinbetriebnahme der Anlage</h3>
+
+
+	<p>Nach allen Unterlagen, die uns zugänglich sind, ist ein Betrieb der Q1 ohne Diskettenstation nicht möglich. Von
+		daher wird unsere Q1 ein "ruhendes" Expemplar bleiben. Ob in ROMs ein Monitorprogramm steckt, das evtl.
+		einen rudimentären Betrieb zulässt, kann erst geprüft werden, wenn die Maschine nach dem Einschalten auf
+		Tastendrücke reagiert (was sie im Augenblick noch nicht tut).
+		<p/>
+	<p> *** wird fortgesetzt *** 
+		</p>
+
+<h3>Broschüren und Manuals zum Download</h3>
+
+<p>zum Download unserer Scans geht es <a href="/de/geraete/Q1_lite/Q1_PDFs">hier</a>
+</p>
+<p>Besonderer Dank geht an Robin Vowels, der uns mit Scans einiger seltener Manuals versorgt hat !
+</p>
+
+

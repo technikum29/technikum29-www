5457ac987dfb4d299fe8c066bafd5a1b3a36acad
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-01 13:23:14 +0200

    Update kommerzielle.php

--- de/rechnertechnik/kommerzielle.php
+++ de/rechnertechnik/kommerzielle.php
@@ -106,8 +106,7 @@
 	<h3 id="ta1000">TA1000</h3>
 	
 		<div class="box center auto-bildbreite">
-			<a href="" title="Zur Detailaufnahme der TA1000-Anlage">
-			<img src="/shared/photos/rechnertechnik/TA1000.jpg" alt="TA1000" width="640" height="480" /></a>
+			<img src="/shared/photos/rechnertechnik/TA1000.jpg" alt="TA1000" width="640" height="480" />
 			<p class="bildtext"><b>TA1000</b></p>
 		</div>
 		<p>Dieser Magnetkontenbuchungsautomat Triumph Adler TA1000 wurde bis 1989 in der Haushaltsabteilung der TU Darmstadt zur Verwaltung des gesamten jährlichen Haushaltsvolumen (250 Mio. DM) 

38363243390a1a084c12d3cdfe1a87f748284e4c
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-01 13:10:14 +0200

    Update p652-details.php

--- de/geraete/Olivetti_P652/p652-details.php
+++ de/geraete/Olivetti_P652/p652-details.php
@@ -14,16 +14,23 @@ CTU1000 sowie einen Lochstreifenstanzer PN20. Wir werden versuchen, aus dem Mate
 
 <p>In der Ausstellung präsentieren wir z. Zt. eine kleine Kombination aus P652, ST4 und CTU1000
 
+<p>Zum Vergrößern der Bilder bitte anklicken !</p>
 
 <div class="box center">
-    <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/P652_offen.jpeg" width="594" height="446" alt="P652 offen" />
+    	<a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/P652_offen.jpeg" class="popup">
+	<img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/P652_offen.jpeg" width="594" height="446" alt="P652 offen" />
+	</a>
     <p>Unsere erste P652 ist in sehr gutem Zustand, sie rechnet und druckt. Die Magnetkarteneinheit muss noch überprüft werden.
     </p>
 </div>
 
 <div class="box center">
+	<a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600_Cassette_2.jpg" class="popup">
 	<img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600_Cassette_2.jpg" width="320" height="240" alt="MLU600 Erweiterung" />
+	</a>
+	<a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600_Cassette.jpg" class="popup">
 	<img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600_Cassette.jpg" width="320" height="240" alt="MLU600 Cassette" />
+	</a>
 	<p>Eine der Ersatz-P652en hat eine Erweiterung "GOLU MLU600" eingebaut <b>(links)</b>, sie ist Voraussetzung, um eines der Magnetlaufwerke MLU600
 		in Betrieb nehmen zu können. Leider suchen wir noch nach den passenden Magnetbandcassetten. Wie sie aussehen müssen wissen wir
 		aus einer Abbildung in unserer P652-Betriebsanleitung <b>(rechts)</b>.

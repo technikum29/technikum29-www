131a8b990ac882d9d4cb0a1f8569e4a339efcb2e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-03 21:46:19 +0200

    Create 2022-10-03-Virtueller_Rundgang.php

---
+++ blog/2022-10-03-Virtueller_Rundgang.php
@@ -0,0 +1,22 @@
+<?php
+	$blog_title = "Virtueller Museumsrundgang";
+	$blog_title_kurz = "Virtueller Museumsrundgang";
+	$blog_author = "Roland";
+	$blog_date = "2022-10-03";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+
+
+<p>Auf der deutschen Startseite wurde ein Link zu einem  <a href="/de/virtueller_Rundgang.php">virtuellen Museumsrundgang</a> eingefügt. 
+  Nach unseren Auf- und Umräumaktionen der letzten Monate präsentieren wir in 17 Bildern hier das Ergebnis</p
+
+<p>Viel Spass beim Durchklicken durch die Bilder !! </p>
+
+
+  
+<?php print_author_info(); ?>

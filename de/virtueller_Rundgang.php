cb75655d5e9049beb89eef87d04ea4fa3b4bec45
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-03 20:36:41 +0200

    Update virtueller_Rundgang.php

--- de/virtueller_Rundgang.php
+++ de/virtueller_Rundgang.php
@@ -10,41 +10,30 @@
 <div class="slider-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
 	<!-- Erstes Bild (Noscript: Default) -->
 	<div class="slide">
-		<h2>
-		<br>
-		<span class="subtext"></span>
-		</h2>
+		
 		<img class="no-copyright" src="/shared/photos/start/startseite.jpg" width="940" height="289">
 	</div>
 
 
 	<div class="slide js">
-		<h2>
-		<br>
-		</h2>
+		
 		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-2.jpg" width="940" height="290">
 	</div>
 
 	<div class="slide js">
-		<h2>
-		<br>
-		</h2>
+		
 		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-3.jpg" width="940" height="290">
 	</div>
 
 	 
 	<div class="slide js">
-		<h2>
-		<br>
-		</h2>
+		
 		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-4.jpg" width="940" height="290">
 	</div>
 	
   
 	<div class="slide js">
-		<h2>
-		<br>
-		</h2>
+		
 		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-5.jpg" width="940" height="290">
 	</div>
 	

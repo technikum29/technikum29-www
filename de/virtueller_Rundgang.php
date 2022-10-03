1307ed2948153abbfd8e692be54fdafa32a743bc
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-03 21:24:35 +0200

    Update virtueller_Rundgang.php

--- de/virtueller_Rundgang.php
+++ de/virtueller_Rundgang.php
@@ -12,87 +12,87 @@
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_1.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Im Zentrum der Austellung unsere UNIVAC 9400</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_2.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">TA 1000, MAEL 4000 und UNIVAC 9400</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_3.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">LGP30, IBM 1130 und TA 1000</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_4.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">LGP21 (mitte) und LGP 30 (re)</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_5.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Blick in die Ausstellung</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_6.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Div. Oszilloskope und Anelex-Drucker</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_7.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Div. DEC - PDP Minicomputer</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_8.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">DEC PDP "straight 8" und lab8e</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_9.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">elektronische Tischrechner (hi. mitte), IBM 5100 und IBM 5320</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_10.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">BULL Tabelliermaschine, div. Lochkartenstanzer, IBM 083 Sortiermaschine</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_11.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">JUKI Lochkartenstanzer und IBM 083 Lochkarten-Sortiermaschine</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_12.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Regal mit mechanischen Rechenmaschinen</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_13.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Im Vordergrund: MAEL 4000 und TA 1000, im Hintergrund elektr. Klavier "Pianola"</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_14.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Elektronische Tischrechner von Hewlett Packard und WANG</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_15.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">frühe Rechner mit Mikroprozessoren: Q1 lite, Siemens 6610 und CPT 8800</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_16.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">EDV-Literatur</p>
 	</div>
 	<div class="slide">
 		
 		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_17.jpeg" width="940" height="705">
-		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+		<p class="bildtext">Blick in eines unserer Depots im Tresorraum des ehem. Sparkassengebäudes !</p>
 	</div>
 
 

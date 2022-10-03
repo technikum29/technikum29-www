e5c7b2f3c13fee5e132dda54ee4bb2f8732c1070
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-03 21:03:35 +0200

    Update virtueller_Rundgang.php

--- de/virtueller_Rundgang.php
+++ de/virtueller_Rundgang.php
@@ -11,32 +11,92 @@
 	<!-- Erstes Bild (Noscript: Default) -->
 	<div class="slide">
 		
-		<img class="no-copyright" src="/shared/photos/start/startseite.jpg" width="940" height="289">
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_1.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_2.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_3.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_4.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_5.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_6.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_7.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_8.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_9.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_10.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_11.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_12.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_13.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_14.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_15.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_16.jpeg" width="940" height="705">
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+	<div class="slide">
+		
+		<img class="no-copyright" src="/shared/photos/rundgang/rundgang_17.jpeg" width="940" height="705">
 		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
 	</div>
 
 
-	<div class="slide js">
-		
-		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-2.jpg" width="940" height="290">
-	</div>
-
-	<div class="slide js">
-		
-		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-3.jpg" width="940" height="290">
-	</div>
-
-	 
-	<div class="slide js">
-		
-		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-4.jpg" width="940" height="290">
-	</div>
 	
-  
-	<div class="slide js">
-		
-		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-5.jpg" width="940" height="290">
-	</div>
 	
 </div>
 </header>

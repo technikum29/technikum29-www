ae7a3639e1ed7c41a07e8ffb7b3fbdad4284edc6
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-10-03 20:25:23 +0200

    Update virtueller_Rundgang.php

--- de/virtueller_Rundgang.php
+++ de/virtueller_Rundgang.php
@@ -7,3 +7,46 @@
 ?>
 
 <h2><?php print $titel; ?></h2>
+
+<div class="slider-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
+	<!-- Erstes Bild (Noscript: Default) -->
+	<div class="slide">
+		<h2>
+		Vergangenheit verstehen <br>
+		<span class="subtext">Zukunft gestalten</span>
+		</h2>
+		<img class="no-copyright" src="/shared/photos/start/startseite.jpg" width="940" height="289">
+	</div>
+
+
+	<div class="slide js">
+		<h2>
+		Der Beginn der EDV<br>
+		</h2>
+		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-2.jpg" width="940" height="290">
+	</div>
+
+	<div class="slide js">
+		<h2>
+		Minicomputer<br>
+		</h2>
+		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-3.jpg" width="940" height="290">
+	</div>
+
+	 
+	<div class="slide js">
+		<h2>
+		Die Zeit der Lochkarten<br>
+		</h2>
+		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-4.jpg" width="940" height="290">
+	</div>
+	
+  
+	<div class="slide js">
+		<h2>
+		Mechanisches Rechnen<br>
+		</h2>
+		<img class="no-copyright" src="/shared/photos/rechnertechnik/header-start-5.jpg" width="940" height="290">
+	</div>
+	
+</div>

430a7ada2ffe7da8229fcd3ef5d63400810a1768
Detlef Gerhardt <feedback@dgerhardt.de>
2022-11-06 16:53:52 +0100

    Hoffentlich letzte Korrekturen

--- de/geraete/JUKI/JUKI_Video.php
+++ de/geraete/JUKI/JUKI_Video.php
@@ -8,12 +8,12 @@
 
 <h2><?php print $titel; ?></h2>
 <p>
-	Hier in dem kleinen Filmchen sieht man den JUKI Lochkartenstanzer in Aktion : </p>
+	Hier in dem kleinen Filmchen sieht man den JUKI Lochkartenstanzer in Aktion:</p>
 
 <p>
 <video width="1280" height="720" controls style="width:100%;height:auto;">
   <source src="/de/geraete/JUKI/JUKI_Videos/Stanzer_in_Aktion.mp4" type="video/mp4">
-  <!-- <source src="/de/geraete/JUKI/JUKI_Videos/Stanzer_in_Aktion.mov" type="video/quicktime"> -->
+  <source src="/de/geraete/JUKI/JUKI_Videos/Stanzer_in_Aktion.mov" type="video/quicktime">
   <em>Leider unterstützt Ihr Browser keines der angebotenen Videoformate.
   Probieren Sie doch einmal <a href="/de/geraete/JUKI/JUKI_Videos/">den Download der Videofiles</a>.
 </video>

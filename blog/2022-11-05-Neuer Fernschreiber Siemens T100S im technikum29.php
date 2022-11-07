41f6106d1522b77766d0587c4e2ca1c8b2d9436f
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-11-07 11:12:08 +0100

    neues Teammitglied, neue NEWS
    Detlef ins Team aufgenommen, seinen Beitrag zum T100S erstellt

---
+++ blog/2022-11-05-Neuer Fernschreiber Siemens T100S im technikum29.php
@@ -0,0 +1,36 @@
+<?php
+	$blog_title = "Neuer Fernschreiber Siemens T100S im technikum29";
+	$blog_title_kurz = "Neuer Fernschreiber Siemens T100S im technikum29";
+	$blog_author = "Detlef";
+	$blog_date = "2022-11-05";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+
+
+<p>Bereits im Frühjahr 2022 holte Detlef einen Fernschreiber Siemens T100S bei einem 
+ehemaligen Post- und Telekom-Techniker in Nordhessen ab. Nach der Instandsetzung 
+(hauptsächlich Reinigung und Schönheitsreparaturen) wurde der Fernschreiber nun von 
+Franz und Detlef im Kommunikationsraum aufgestellt und als vierter Fernschreiber an 
+das i-Telex-Netzwerk angeschlossen. Nachdem nach längerer Fehlersuche alle Brücken 
+in der Ado8-Anschlussdose richtig gesetzt waren (manchmal liegt es nur an Kleinigkeiten, 
+Danke an Kai für die Hilfe bei der Fehlersuche), funktionierte dann auch alles ordnungsgemäß. 
+Bei der Gelegenheit wurde auch eine weitere Schnittstellenkarte in die i-Telex-Vermittlung 
+eingestetzt, so dass jetzt bis zu sechs Fernschreiber mit TW39-Schnittstelle angeschlossen 
+werden können.
+</p
+
+<p>Der T100S gehört zur letzten Generation der mechanischen Fernschreiber. Er wurde ab 
+1965 gebaut und war bis in die achziger Jahre in Betrieben und Behörden im Einsatz. 
+Er besitzt eine automatische Umschaltung zwischen Buchstaben und Zeichen und einen 
+mechanischen 20-Zeichen-Puffer für Schnellschreiber (schneller als 400 Anschläge pro 
+Minute).
+</p>
+
+
+  
+<?php print_author_info(); ?>

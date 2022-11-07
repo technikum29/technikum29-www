db2975a3d9dc50a55037185e0e34ec828ea5bf54
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-11-07 13:42:15 +0100

    nochmal LGP30 angelegt
    This reverts commit 0df0ebae404a4ee298b49332aa6f3dd1c3d05a4b.

---
+++ de/geraete/LGP30/LGP30_Details.php
@@ -0,0 +1,103 @@
+<?php
+	$seiten_id = 'LGP30_Details';
+	$version = '$Id';
+	$titel = 'LGP30';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $titel; ?></h2>
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+	Stand: Nov. 2022
+</p>
+
+	<p> An dieser Stelle berichten wir über die Fortschritte bei der Restaurierung der LGP30.
+</p>
+<p>
+<h3>Bestandsaufnahme November 2022</h3>
+</p>
+
+<p>Bereits in 2018 entschieden wir uns, die defekte Trommel durch eine Replika auf FPGA-Basis zu
+ersetzen. Die Schäden an der Trommel und ggf. auch an den Magnetköpfen waren zu gravierend. 
+FPGA-Hard- und Softwaredesign zogen sich einige Monate hin, das Resultat lief aber 
+leider nicht zufriedenstellend auf unserer Maschine. Ob ein Fehler in der Replika vorlag 
+oder in der LGP30 (oder beides) liess sich nicht entscheiden.</p>
+
+<p>Wir entschieden uns, die Replika an der funktionierenden LGP30 im <a href="http://computermuseum.informatik.uni-stuttgart.de/"target="_blank">Computermuseum-Stuttgart</a>
+ zu testen. Im Januar 2020 konnten wir erfolgreich ein Spiel Black Jack mit einem Programmabbild auf der 
+ Trommel auf der dortigen LGP30 spielen - die Replika funktioniert !</p>
+ 
+ <p>Um nicht weiter nur an einzelnen Fehlersymptomen herumzuwerkeln haben wir unsere Maschine einer
+ gründlichen Inspektion unterworfen:</p>
+ <p>- Das Netzteil wurde geprüft, alle Spannungen wo notwendig auf Sollwerte gebracht
+</p>
+
+<p>- alle Röhren und Dioden wurden geprüft und ggf. ersetzt.
+</p>
+
+<p>- alle Einschubkarten wurden getestet.
+</p>
+ <p>Aus der Phase hier einige Bilder der offenen Anlage, das letzte Foto zeigt die FPGA-Platine. 
+ Durch Anklicken vergrößern sich die Bilder !
+ </p>
+<div class="box center">
+   <a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_1.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_1.jpeg" width="200"/>
+      </a>
+	<a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_2.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_2.jpeg" width="200"/>
+      </a>
+    <a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_3.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_3.jpeg" width="200"/>
+      </a>
+    <a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_4.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_4.jpeg" width="156"/>
+      </a>
+    <p class="bildtext">Gleichspannungsnetzteil, Frontansicht, Front von oben und 
+        Rückansicht linke Seite mit Analogboards</p>
+</div>
+
+<p>
+</p>
+
+<div class="box center">
+   <a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_5.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_5.jpeg" width="146"/>
+      </a>
+	<a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_6.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_6.jpeg" width="200"/>
+      </a>
+    <a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_7.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_7.jpeg" width="200"/>
+      </a>
+    <a  href="/de/geraete/LGP30/LGP30_Bilder/LGP30_8.jpeg" class="popup">
+      <img src="/de/geraete/LGP30/LGP30_Bilder/LGP30_8.jpeg" width="200"/>
+      </a>
+    <p class="bildtext">Rückseite rechte Seite mit Digitalboards und Relaiskasten, 
+        Blick auf die Spannungskonstanter, Rückseite Oszilloskop und FPGA-Platine</p>
+</div>
+<p>Wir hatten die letzten Monate genutzt, einige Depots aufzuräumen. Dabei fanden wir
+eine Menge Abdeckungen und Kleinteile zur LGP30, die Heribert dort zwischengelagert 
+hatte - jetzt ist unsere Maschine auch mechanisch wieder komplett !
+</p>
+
+
+<h3>Weitere Arbeiten zur Restaurierung</h3>
+
+<p>Zur Zeit wird die FPGA nochmals überprüft, ob zwischenzeitlich ggf. ein Defekt 
+entstanden ist. Vielleicht noch im Dezember, spätestens Anfang 2023 steht ein neuer
+Testlauf an !
+</p>
+
+<p>
+</p>
+
+<p>
+</p>
+
+<p> *** wird fortgesetzt *** 
+		</p>
+
+

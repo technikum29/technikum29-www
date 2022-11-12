58437b67979a2a532f6c7c6b308dd3320f73c3fd
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-11-12 16:32:13 +0100

    neue Bilder Magnetcassetten MLU600

--- de/geraete/Olivetti_P652/p652-details.php
+++ de/geraete/Olivetti_P652/p652-details.php
@@ -8,7 +8,7 @@
 
 <h2><?php print $title; ?></h2>
 
-	Stand: Nov. 2022
+	Stand: 12. Nov. 2022
 
 <p>Im Bestand des technikum29 haben wir 3 Olivetti P652, 2 Schreibsysteme ST4, 2 Magnetbandeinheiten MLU600, eine Festplatte DAS600, 2 Doppel-Cassettenlaufwerke
 CTU1000 sowie einen Lochstreifenstanzer PN20. Wir werden versuchen, aus dem Material eine funktionsfähige Komplettanlage zusammenzustellen. 
@@ -40,7 +40,7 @@ CTU1000 sowie einen Lochstreifenstanzer PN20. Wir werden versuchen, aus dem Mate
     </p>
 </div>
 
- <p>6.11.2022: Über das Forum des VzEhC haben uns Bilder einer MLU-Cassette erreicht, vielen Dank an Franz Reitmeier !
+ <p><b>6.11.2022:</b> Über das Forum des VzEhC haben uns Bilder einer MLU-Cassette erreicht, vielen Dank an Franz Reitmeier !
  </p>
 <div class="box center">
    <a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/Olivetti_MLU600_Cassette_1.jpeg" class="popup">
@@ -60,7 +60,47 @@ CTU1000 sowie einen Lochstreifenstanzer PN20. Wir werden versuchen, aus dem Mate
 <br>
 
 
+ <p><b>12.11.2022: Es geschehen noch Wunder ! </b>Einer unserer Unterstützer fand tatsächlich 2 Cassetten
+ für die MLU 600 in unseren Depots !!! Die Suche nach passenden Cassetten hat ein Ende. Nach dem Reinigen
+ ein vorsichtiger Blick ins Innere der Cassetten zeigt eine solide Mechanik, die ein Endlos-Magnetband 
+ an einem Tonkopf vorbei transportiert. Leider hat sich in beiden Cassetten eine Gummi-Andruckrolle 
+ in eine teerartige Masse verwandelt. In einer Cassette ist das Band nur geringfügig verschmutzt, in
+ der zweiten Cassette hat es wohl einen Transportversuch gegeben: das Band hat sich um die klebrige
+ Rolle gewickelt, verknittert und die Magnetschicht löst sich an einigen kleinen Stellen ab.
+ </p>
+ <br>
 
+ <div class="box center">
+   <a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_a.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_a.jpeg" width="450"/>
+      </a>
+	<a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_b.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_b.jpeg" width="300"/>
+      </a>
+    <p class="bildtext">2 Magnetbandcassetten für die MLU600, zum Vergrößern der Bilder diese bitte anklicken !</p>
+</div>
+<div class="box center">
+   <a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_c.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_c.jpeg" width="130"/>
+      </a>
+	<a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_f.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_f.jpeg" width="130"/>
+      </a>
+    <a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_d.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_d.jpeg" width="130"/>
+      </a>
+    <a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_e.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_e.jpeg" width="170"/>
+      </a>
+    <a  href="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_g.jpeg" class="popup">
+      <img src="/de/geraete/Olivetti_P652/Olivetti_P652_Bilder/MLU600-P652_32_g.jpeg" width="170"/>
+      </a>
+    <p class="bildtext">weitere Bilder, zum Vergrößern der Bilder diese bitte anklicken !</p>
+</div>
+<br>
+ <p>Als nächstes steht der Ersatz der Andruckrolle und das vorsichtige Reinigen des Bandes an.
+ </p
+ <br>
 
 
 <p> wird fortgesetzt ... </p>

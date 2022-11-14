96df715edc6f0db0166207e382c9537819c36a29
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2022-11-14 14:45:07 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -53,6 +53,23 @@
 
 	<div class="box termin clear-after">		
 	<p class="date left"><em>2022</em> 19. Nov</p>
+	<h4>Privatführung 20ster Geburtstag Lorenz R.</h4> 
+	
+
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 19. November 2022
+		<dt>Uhrzeit
+		<dd>13:00 - 16:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+				
+		<dd>geschlossene Gesellschaft
+	</dl>
+</div><!--/.termin -->
+	
+	<div class="box termin clear-after">		
+	<p class="date left"><em>2022</em> 19. Nov</p>
 	<h4>allgemeiner Helfertag</h4> 
 	<p>An diesem Termin ist ein Betreuer von 10:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
 		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt

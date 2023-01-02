20b8e4427c13de58cc314f42934bda700bf1d7ee
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-01-02 16:06:11 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -92,8 +92,55 @@
 	</dl>
 </div><!--/.termin -->
 	
+	
+	<div class="box termin clear-after">		
+	<p class="date left"><em>2023</em> 28. Jan</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 10:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 28. Januar 2023
+		<dt>Uhrzeit
+		<dd>10:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+			
+		<dd>Instandsetzung div. Geräte nach Absprache
+		
+	</dl>
+</div><!--/.termin -->
+
+	
+	<div class="box termin clear-after">		
+	<p class="date left"><em>2023</em> 14. Jan</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 10:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 14. Januar 2023
+		<dt>Uhrzeit
+		<dd>10:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+			
+		<dd>Instandsetzung div. Geräte nach Absprache
+		
+	</dl>
+</div><!--/.termin -->
+
+	
+	
+	
+	
+	
 <div class="box termin clear-after">		
-	<p class="date left"><em>2022</em> 25. Nov</p>
+	<p class="date left"><em>2022</em> 25.Nov</p>
 	<h4>Privatführung 70ster Geburtstag Wolfgang D.</h4> 
 	
 

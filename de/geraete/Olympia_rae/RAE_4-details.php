4d80648359eea1698ea728bca79913909ffed898
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-02-06 17:37:43 +0100

    RAE 4/15 und 4/30 korrigiert und Reparaturblog angelegt

---
+++ de/geraete/Olympia_rae/RAE_4-details.php
@@ -0,0 +1,17 @@
+<?php
+	$seiten_id = 'rae415';
+	$version = '$Id';
+	$titel = 'RAE 4/15 Details';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $title; ?></h2>
+
+	Stand: 6. Feb. 2023
+
+<p>Im Bestand des technikum29 haben wir 8 Olympia RAE- Tischrechner, darunter die Modelle RAE 4/15-2, 4/30-1, -2 und -3. 
+Über den Fortschritt bei der Restaurierung werden wir hier berichten.</p>
+
+
+<p> wird fortgesetzt ... </p>

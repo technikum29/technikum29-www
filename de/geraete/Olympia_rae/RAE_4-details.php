a0d93fb00a2ed077c9d6867187ac9b47067968fa
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-02-10 12:50:31 +0100

    Reparaturbericht RAE 4/15 eingepflegt

--- de/geraete/Olympia_rae/RAE_4-details.php
+++ de/geraete/Olympia_rae/RAE_4-details.php
@@ -19,21 +19,166 @@
 </p>
 
 
-<h3>Reparatur unserer 4/15-2</h3>
+<h3>Vorstellung unserer Olympia RAE</h3>
+
+
+<p>1965 stellte Olympia die Tischrechner der RAE-Serie vor. Die Elektronik basiert auf Germanium-Transistoren, 
+	für die Anzeige werden Nixie-Röhren benutzt. Ein Kernspeicher dient der Speicherung der verschiebenen Rechen- 
+	bzw Speicherregister. Die einzelnen Modellbezeichnungen der Form X/YY-Z bedeuten:</p>
+	<p>X = 4 steht für die 4 Grundrechenarten</p>
+	<p>Y = 15 oder 30 gibt die maximale Stellenanzahl für Ergebnisse an</p>
+	<p>Z = 1, 2 oder 3 gibt die Anzahl der frei verfügbaren Speicherregister an (neben den 3 Rechenregistern)</p>
 
 <div class="box center"> 
-		 <a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1900.jpeg" class="popup">
-		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1900kl.jpeg" width="427"  height="320"/>
+		 <a  href="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_15_2_front.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_15_2_front_kl.jpeg" width="200"  height="166"/>
 	</a>
-		 <a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser1619/IMG_1950.jpeg" class="popup">
-		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser1619/IMG_1950kl.jpeg" width="427"  height="320"/>
+		 <a  href="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_30_1_front.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_30_1_front_kl.jpeg" width="200"  height="166"/>
 	</a>
-		<p class="bildtext">Zwei <b>RAE 4/15-2 </b> mit Unterschieden im Aufbau der Netzteile.</p>
+			 <a  href="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_30_2_front.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_30_2_front_kl.jpeg" width="200"  height="166"/>
+	</a>
+		 <a  href="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_30_3_front.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/RAE_4_30_3_front_kl.jpeg" width="200"  height="166"/>
+	</a>
+	
+	
+	
+		<p class="bildtext">Verschiedene <b>Olympia RAE 4</b> in einer Reihe: RAE 4/15-2, 4/30-1, 4/30-2 und 4/30-3.<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
 	</div>
 
 
+<h3>Reparatur unserer 4/15-2</h3>
+
+<p>Als erste Maschine haben wir uns die RAE 4/15-2 Ser. Nr. 4692 vorgenommen. Mutiges Einschalten über Regel-Trenntrafo
+	resultierte in einem Kurzschluß des Netzeingangs-Filterkondensators. Nach dem Auswechseln desselben lief der Lüfter an und alle 
+	Nixies zeigten eine "0" nach dem Einschalten. Auch die 3 Anzeigelampen leuchteten. Sie konnten über die Tasten I, IV 
+	und V ausgeschaltet werden, ansonsten reagierte die Maschine auf keine Tasteneingabe. Nach kurzer Betriebszeit erlosch 
+	die Anzeige - die Maschine war "tot". Die Fehlersuche begann...</p>
+<p> Nach dem Lösen von 4 Schrauben auf der Unterseite (*nicht* die 4 Schrauben in den Standfüßen !) kann das Oberteil 
+	abgehoben werden. Achtung: die 4 Kabel vom Netzschalter lösen (Reihenfolge aufschreiben !), dann kann es weggelegt 
+	werden. Dann kann die Maschine aus dem Unterteil rausgehoben werden. Dabei darauf achten, dass keiner der 8 Gummipuffer 
+	in den 4 Montagewinkeln verloren geht. <b>Dann zeigt sich die Maschine so:</b></p>
+	
+<div class="box center"> 
 
 
+	<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1900.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1900kl.jpeg" width="135"  height="101"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1901.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1901kl.jpeg" width="135"  height="101"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1902.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1902kl.jpeg" width="135"  height="101"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1904.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1904kl.jpeg" width="135"  height="101"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1929.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1929kl.jpeg" width="135"  height="101"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1951.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1951kl.jpeg" width="135"  height="88"/>
+	</a>
+		<p class="bildtext">Innenansichten der <b>RAE 4/15-2 </b> mit Serien-Nr 04692. Für große Bilder bitte klicken !</p>
+	</div>
+
+
+<div class="box right"> 
+	<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Bauteillage_1.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Bauteillage_1_kl.jpeg" width="400"  height="300"/>
+	</a>
+
+	</div>
+	<p>Zunächst muss die korrekte Spannungsversorgung sichergestellt werden. Die Maschine benötigt +12 V, -12 V, +80V und +200V. 
+	Die Lage der einzelnen Bauteile ist über die gesamte Maschine verstreut (s. Bild rechts). Wichtig für das Verständnis der Funktion der 
+	Stromversorgung ist, dass aus den +12V auch die +80/+200V generiert werden. Die + 12V sind sowohl über eine 4A Schmelzsicherung 
+	als auch über eine elektronische Strombegrenzung abgesichert. </p>
+	<p>Die einzelnen Spannungen können rechts hinten an der Steckverbindung 28 abgegriffen werden: Pin 1 = 0V, Pin 3 = +12V, Pin 5 
+	= -12V, Pin 6 = +80 V, Pin 7 = +200V. </p>
+	<p>Wenn wie bei unserer Maschine weder die Nixies leuchten noch die Anzeigelampen, ist die +12 V 
+	verdächtig: die 4A Schmelzsicherung war ganz, aber es wurde nur < 1 V auf der 12 V gemessen. Offenkundig greift die elektronische 
+	Strombegrenzung ein. Abziehen des Steckers 28 von der Maschine brachte keine Änderung, also liegt der Kurzschluß nicht auf
+	einer der 13 Rechenplatinen. Prüfung aller Elkos ergab auch keinen Hinweis. Verbleiben als Ursache der Taktgenerator ("Treiberwandler") 
+	und die 80/200V-Versorgung (beide vorne links). Beide werden durch Gegentakt-Zerhacker (2 * AC124) betrieben, in unserem Fall 
+	war der Taktgenerator defekt. Auswechseln der AC124 <b>und schon sind alle Spannungen im Soll und die Maschine begrüßt mit 15 
+	leuchtenden Nixies !</b> Sie reagierte aber noch nicht bzw. nur erratisch auf Tasteneingaben.<p>
+	
+	<div class="box right"> 
+
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Bauteillage_2.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Bauteillage_2_kl.jpeg" width="200"  height="150"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Takt.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Takt_kl.jpeg" width="200"  height="150"/>
+	</a>
+	</div>
+	
+	<p>Der Treiberwandler erzeugt zwei ca 25 kHz gegenphasige Rechtecksignale, die für den Betrieb der RAE essentiell sind. 
+	Fehlen sie, so zeigt die Maschine nur einzelne statische Ziffern und reagiert nicht auf Tasteneingaben.</p>
+	Jetzt konzentrierte sich die weitere Fehlersuche auf die 13 Logikplatinen. Tipps zum Ausbau siehe auf dem Bild ganz rechts. 
+	Durch Tauschen mit Platinen aus einer zweiten Maschine konnte unsere RAE zumindest ansatzweise wieder zur Arbeit bewegt werden. 
+	Da der Status der Tauschplatinen unklar ist, müssen aufbauend auf dem Status nun alle Platinen überprüft werden. </p>
+	
+	<div class="box center"> 
+
+
+	<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1890.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1890kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1891.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1891kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1892.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1892kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1893.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1893kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1894.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1894kl.jpeg" width="160"  height="120"/>
+	</a>
+	
+	</div>
+	<div class="box center"> 
+
+
+	<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1895.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1895kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1896.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1896kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1897.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1897kl.jpeg" width="160"  height="120"/>
+	</a>
+		<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1898.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/IMG_1898kl.jpeg" width="160"  height="120"/>
+	</a>
+	
+		<p class="bildtext">Die 13 Logikplatinen der <b>RAE 4/15-2 </b> mit Serien-Nr 04692. Für große Bilder bitte klicken !</p>
+	</div>
+	
+	<div class="box right"> 
+	<a  href="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Anzeige.jpeg" class="popup">
+		<img src="/de/geraete/Olympia_rae/Bilder_RAE/Bilder_RAE_415_Ser4692/Anzeige_kl.jpeg" width="200"  height="150"/>
+	</a>
+
+	</div>
+	
+	
+	
+	<p>Wir nutzen dazu einen Komponententester, wie er zB im Oszilloskop Hameg 604 eingebaut ist. Nach dem Ersatz von 6 defekten AA132-Dioden 
+	(alle hatten keinen Duchgang mehr) haben wir mittlerweile einen Zwischenstand erreicht, dass die Maschine in allen Registern speichert 
+	und alle Grundrechenarten durchführt. Rechts das Ergebnis der Division 10.000.000 : 7.</p>
+	<p>Es verbleiben noch einzelne kleine Fehler, die Suche auf den Platinen geht weiter...</p>
+	
+	<br>
+	<br>
+	
 
 	<p>... in Arbeit ...
 </p>

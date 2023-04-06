722ec37d08145a00dc1fa6933271a6e90c165a57
Detlef Gerhardt <feedback@dgerhardt.de>
2023-04-06 23:07:43 +0200

    Formatierung

--- de/kommunikationstechnik/fernschreiber.php
+++ de/kommunikationstechnik/fernschreiber.php
@@ -14,7 +14,7 @@
         <div class="bildtext"><b>Siemens Springschreiber T32</b><br/>(zum Vergrößern anklicken)</div>
     </div>
 
-	<p>Der ratternde Fernschreiber bestimmte Jahrzehnte das Bild der schnellen Nachrichtenübermittlung. 1930 erstmals von Siemens & Halske vorgestellt, wurde der hier abgebildete T32 
+    <p>Der ratternde Fernschreiber bestimmte Jahrzehnte das Bild der schnellen Nachrichtenübermittlung. 1930 erstmals von Siemens & Halske vorgestellt, wurde der hier abgebildete T32 
     am 16. Oktober 1933 von der Deutschen Reichspost eingeführt. Die erste amtliche Verbindung bestand zwischen Berlin und Hamburg. Für das Fernschreibnetz mussten separate Leitungen verlegt werden.<br/>
     Von 21 Teilnehmern 1933 stieg die Zahl auf 1500 im Jahre 1939 und schließlich auf über 90.000 um 1975. Der mechanische Fernschreiber wurde nun auf elektronische umgestellt.<br>
     Die Blütenzeit der Mechanik in der Nachrichtentechnik bestand u.a. in den verschiedenen Fernschreibertypen. Der relativ seltene Springschreiber T32 wurde ab den 30er Jahren bis in die
@@ -23,73 +23,67 @@
     vereinigt. Seine Betriebssicherheit ist nach dem Urteil der Praxis außerordentlich groß".</blockquote>
     <p>In diesem voll mechanisiertem Zeitalter benötigten die "Büromaschinenmechaniker" ein gefühlvolles Händchen, gute Ohren um eventuelle Störgeräusche einordnen zu können und jede Menge
     Ölkännchen mit verschiedenen Ölsorten. In den 80er Jahren ist dieser Beruf schließlich ausgestorben.
-    <p>
-    Wir zeigen in der Ausstellung Fernschreiber unterschiedlichster Bauart (Blattschreiber und Streifenschreiber) von ca. 1940 bis 1960. Zwei weitere Fernschreiber sollen hinzukommen und
+    <p>Wir zeigen in der Ausstellung Fernschreiber unterschiedlichster Bauart (Blattschreiber und Streifenschreiber) von ca. 1940 bis 1960. Zwei weitere Fernschreiber sollen hinzukommen und
     den Zeitraum bis 1980 abdecken.</p>
     <br/>
+
 
     <h2 id="lorenz-lo15b">Lorenz Fernschreiber Lo15b</h2>
 		
 	<div class="box center clear-after">
-		<img src="/shared/photos/kommunikationstechnik/Lorenz-Lo15b.jpg" width="420" height="336" alt="Lorenz Lo15b" />		
-		<div class="bildtext">
-            <b>Lorenz Fernschreiber Lo15b Bj. ca. 1960</b> 
-		</div>
+	    <img src="/shared/photos/kommunikationstechnik/Lorenz-Lo15b.jpg" width="420" height="336" alt="Lorenz Lo15b" />		
+	    <div class="bildtext"><b>Lorenz Fernschreiber Lo15b Bj. ca. 1960</b></div>
 	</div>
-    <div><p>Der Fernschreiber Lo15b gehört zur zweiten Generation von Fernschreibern der Lo15-Serie, die von der Firma Lorenz seit 1930 als Lizenzbau des Teletype Model 15 gebaut wurden.
-    Unser Modell stammt von ca. 1960 und war laut dem original erhaltenen Kennungsgeber noch bis 1983 bei der Firma Rudolf Hoffmann in Nürnberg im Einsatz.
-    Die mechanischen Fernschreiber aus dieser Zeit waren bei regelmäßiger Wartung nahezu unverwüstlich und über viele Jahre im Einsatz.<br/>
-    Der grundsätzliche mechanische Aufbau des Lo15 wurde über alle Gerätegenerationen kaum verändert. Wie auch die Siemens Fernschreiber wurde der Lo15 in den unterschiedlichsten
-    Gehäuseformen angeboten. Unser Holzstandgehäuse befindet sich, wie der Fernschreiber selbst, in einem sehr guten Zustand.<p/>
-    <p>Um Zeit und damit Geld bei der Übermittlung der Nachrichten zu sparen, besitzt der Lo15b einen integrierten 5-Kanal-Lochstreifenstanzer und -sender, um Nachrichten "offline"
-    vorzustanzen und dann direkt vom Lochstreifen zusenden.
-    Um Lochstreifen lokal bearbeiten zu können, war ein Fernschaltgerät (FSG) mit Lokalmodus nötig, das die erfoderliche Linienspannung zur Verfügung stellte. Der Lokalmodus war ein Zusatz,
-    der exta bezahlt werden musste, der sich aber bei häufigem Gebrauch durch die gesparten Verbindungskosten rechnete.
-    Das FSG diente ansonten der Anwahl anderer Teilnehmer (Wählscheibe) und schaltete bei eingehenden Verbindungen den Fernschreiber automatisch ein.</p></div>
-    <br/>
+        <p>Der Fernschreiber Lo15b gehört zur zweiten Generation von Fernschreibern der Lo15-Serie, die von der Firma Lorenz seit 1930 als Lizenzbau des Teletype Model 15 gebaut wurden.
+        Unser Modell stammt von ca. 1960 und war laut dem original erhaltenen Kennungsgeber noch bis 1983 bei der Firma Rudolf Hoffmann in Nürnberg im Einsatz.
+        Die mechanischen Fernschreiber aus dieser Zeit waren bei regelmäßiger Wartung nahezu unverwüstlich und über viele Jahre im Einsatz.<br/>
+        Der grundsätzliche mechanische Aufbau des Lo15 wurde über alle Gerätegenerationen kaum verändert. Wie auch die Siemens Fernschreiber wurde der Lo15 in den unterschiedlichsten
+        Gehäuseformen angeboten. Unser Holzstandgehäuse befindet sich, wie der Fernschreiber selbst, in einem sehr guten Zustand.<p/>
+        <p>Um Zeit und damit Geld bei der Übermittlung der Nachrichten zu sparen, besitzt der Lo15b einen integrierten 5-Kanal-Lochstreifenstanzer und -sender, um Nachrichten "offline"
+        vorzustanzen und dann direkt vom Lochstreifen zusenden.
+        Um Lochstreifen lokal bearbeiten zu können, war ein Fernschaltgerät (FSG) mit Lokalmodus nötig, das die erfoderliche Linienspannung zur Verfügung stellte. Der Lokalmodus war ein Zusatz,
+        der exta bezahlt werden musste, der sich aber bei häufigem Gebrauch durch die gesparten Verbindungskosten rechnete.
+        Das FSG diente ansonten der Anwahl anderer Teilnehmer (Wählscheibe) und schaltete bei eingehenden Verbindungen den Fernschreiber automatisch ein.</p>
+        <br/>
+
 
     <h2 id="siemens-t37">Siemens T37</h2>
        
 	<div class="box center clear-after">
-		<img src="/shared/photos/kommunikationstechnik/siemens-t37.jpg" width="390" height="402" alt="Siemens T37" />		
-		<div class="bildtext">
-	        <b>Siemens Fernschreiber ca. Bj. 1952</b> 
-		</div>
-	 </div>
-	<div><p>Im Gegensatz zur Firma Lorenz, die zunächst auf lizenzierte Nachbauten des Teletype Model 15 setze, entwickelte Siemens eigene Springschreiber, darunter auch den 
-    Blattschreiber T37, der ab ca. 1933 gebaut wurde.
-    Unser T37 im schicken Holzgehäuse ist Baujahr 1952 und war laut Kennungsgeber bis ca. 1986 bei einer Textilvertretung in Hösbach im Einsatz.</p>
-    <p>Auch dieser Fernschreiber besitzt ein FSG mit Lokalmodus und einen eingebauten Lochstreifenempänger bzw. -stanzer. Der Lochstreifensender ist separat im Standgehäuse auf der
-    rechten Seite unterhalb des FSGs untergebracht.</p>
-    </div>
-    <br/>
+	    <img src="/shared/photos/kommunikationstechnik/siemens-t37.jpg" width="390" height="402" alt="Siemens T37" />		
+	    <div class="bildtext"><b>Siemens Fernschreiber ca. Bj. 1952</b></div>
+	</div>
+	<p>Im Gegensatz zur Firma Lorenz, die zunächst auf lizenzierte Nachbauten des Teletype Model 15 setze, entwickelte Siemens eigene Springschreiber, darunter auch den 
+        Blattschreiber T37, der ab ca. 1933 gebaut wurde.
+        Unser T37 im schicken Holzgehäuse ist Baujahr 1952 und war laut Kennungsgeber bis ca. 1986 bei einer Textilvertretung in Hösbach im Einsatz.</p>
+        <p>Auch dieser Fernschreiber besitzt ein FSG mit Lokalmodus und einen eingebauten Lochstreifenempänger bzw. -stanzer. Der Lochstreifensender ist separat im Standgehäuse auf der
+        rechten Seite unterhalb des FSGs untergebracht.</p>
+        <br/>
 
-    <!--
-    Dieser ebenfalls sehr gut erhaltene Fernschreiber war authentisch der erste Fernschreiber des Zeitungsverlages "Darmstädter Echo". Unten ist ein Lochstreifen-Sender zu sehen. Mit ihm konnte man vorher langsam geschriebene
-    Texte schnell versenden, ähnlich wie heute eine SMS erzeugt und gesendet wird. Die Anlage ist natürlich noch voll funktionsfähig.</p> -->
 
     <h2 id="siemens-t100s">Siemens T100S</h2>
        
 	<div class="box center clear-after">
-		<img src="/shared/photos/kommunikationstechnik/siemens-t100s.jpg" width="390" height="402" alt="Siemens T100S" />		
-		<div class="bildtext"><b>Siemens Fernschreiber T100S Bj. 1971</b></div>
-	 </div>
+	    <img src="/shared/photos/kommunikationstechnik/siemens-t100s.jpg" width="390" height="402" alt="Siemens T100S" />		
+	    <div class="bildtext"><b>Siemens Fernschreiber T100S Bj. 1971</b></div>
+	</div>
 	<p>Der Siemens T100S gehört zur letzten Generation der mechanischen Fernschreiber und wurde bis 1976 gebaut. Obwohl diese Fernschreiber immer noch
-        auf der gleichen Technik wie die ersten Springschreiber der 1930er Jahre basiert, wurden inzwischen doch einige Komfortfunktionen eingebauten.
+        auf der Technik der ersten Springschreiber von 1930 Jahre basiert, wurden inzwischen doch einige Komfortfunktionen eingebauten.
         Die Umschaltung zwischen der Buchstaben- und der Zifferneben erfolgt nun automatisch und es ist ein mechanischer 20-Zeichen-Pufferspeicher vorhanden,
         damit Schnellschreiber (größer 400 Anschläge pro Minute) durch die langsame Übertragung nicht aus dem Tritt gebracht wurden.
         Wie die frühen Fernschreiber verfügt das Gerät über einen 5-Kanal-Lochstreifen Sender- und Empfänger. Das Fernschaltgerät mit der Wählscheibe wurde ins
         Gehäuse integriert.</br>
         Unser T100S besaß spezielle Zusatzausstattungen (Faltpapierzuführung mit eingelegtem Telegrammpapier, Anrufsignalisierung), die darauf schließen lassen,
-        dass er im internen Telegramm-Netz (Gentex) der Deutschen Post im Einsatz. Der Originalkennungsgeber war leider nicht mehr vorhanden.
+        dass er im internen Telegramm-Netz (Gentex) der Deutschen Post im Einsatz war. Der Originalkennungsgeber ist leider nicht erhalten.
         </p>
         <br/>
+
 
     <h2 id="siemens-t68d">Siemens Streifenschreiber T68d</h2>
        
 	<div class="box center clear-after">
-		<img src="/shared/photos/kommunikationstechnik/siemens-t68d.jpg" width="420" height="310" alt="Siemens T68d" />		
-		<div class="bildtext"><b>Siemens Streifenschreiber T68d Bj. 1957</b></div>
+	    <img src="/shared/photos/kommunikationstechnik/siemens-t68d.jpg" width="420" height="310" alt="Siemens T68d" />		
+	    <div class="bildtext"><b>Siemens Streifenschreiber T68d Bj. 1957</b></div>
 	 </div>
 	<p>Mit dem Aufkommen der Blattschreiber in den 1920er und 1930er Jahren, war die Zeit der Streifenschreiber noch lange nicht vorbei. Die Streifenschreiber
         waren kompakter, leichter und günstiger als die Blattschreiber und waren daher noch bis in die 1960er Jahre im Einsatz. Als mobile Fernschreiber beim Militär
@@ -99,31 +93,31 @@
         Fernschaltgerät untergebracht. Die Papier- und Lochstreifenrollen befinden sich in den Schubladen unter der Tastatur.</p>
         <br/>
 
+
     <h2 id="fernschreibcode">Der 5-Bit-Fernschreibcode</h2>
 
 	<div class="box center clear-after">
-        <img src="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg" width="414" height="391" alt="Fernschreiber" />
-		<div class="bildtext">
-            <b id="decoder">5-Bit-Fernschreibcode </b> <a class="popup" href="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg"> Bild vergrößern</a>
-        </div>
+            <img src="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg" width="414" height="391" alt="Fernschreiber" />
+	    <div class="bildtext"><b id="decoder">5-Bit-Fernschreibcode </b> <a class="popup" href="/shared/photos/kommunikationstechnik/telegrafenalphabet.jpg"> Bild vergrößern</a></div>
 	</div>
-    <div><p>Diesen sehr großen (70cm x 60cm) und schönen "Decoder", der den 5-Bit-Code in das internationale Telegrafenalphabet (umgangssprachlich auch Baudot-Code genannt)
+    <p>Diesen sehr großen (70cm x 60cm) und schönen "Decoder", der den 5-Bit-Code in das internationale Telegrafenalphabet (umgangssprachlich auch Baudot-Code genannt)
     übersetzt, haben wir vor der Entsorgung an einer Technischen Universität gerettet.<br/>
     Er ist von den dortigen Werkstätten ca. in der Zeit zwischen 1937 und 1949 als aufwändiges Demonstrationsgerät für Vorlesungen und Praktika gebaut worden.<br/>
     Man stellt einen beliebigen 5-Bit-Code an den groß dimensionierten Schaltern ein, dreht die Kodierscheibe und erhält an einer bestimmten Stelle das dazugehörige, 
     oben aufleuchtende Zeichen. Hier würde z.B. bei der Einstellung 10101 der Buchstabe "Y" leuchten.<br>
-    Was damals fortschrittliche Nachrichtentechnik war, dient heute im technikum29 als sogenannte "Dechiffriermaschine".</p></div>
+    Was damals fortschrittliche Nachrichtentechnik war, dient heute im technikum29 als sogenannte "Dechiffriermaschine".</p>
     <br/>
+
 
     <h2 id="itelex">Das i-Telex</h2>
     
 	<div class="box center clear-after">
 	    <img src="/shared/photos/kommunikationstechnik/i-telex.jpg" width="342" height="262" />
-		<div class="bildtext"><b>Unsere i-Telex-Anlage</b><br/>Links die Netzwerkkarte zur Verbindung mit dem Internetrouter, in der Mitte zwei TW39-Steckkarten für 4 TW39-Schnittstellen
-        und rechts daneben das Netzteilmodul, das die Elektronik versorgt und die benötigte Linienspannung von ca. 85 Volt liefert (einige Fernschaltgeräte benötigen diese hohe Spannung).
-        Der Linienstrom bei bestehender Verbindung beträgt ca. 40 mA.</div>
+	    <div class="bildtext"><b>Unsere i-Telex-Anlage</b><br/>Links die Netzwerkkarte zur Verbindung mit dem Internetrouter, in der Mitte zwei TW39-Steckkarten für 4 TW39-Schnittstellen
+            und rechts daneben das Netzteilmodul, das die Elektronik versorgt und die benötigte Linienspannung von ca. 85 Volt liefert (einige Fernschaltgeräte benötigen diese hohe Spannung).
+            Der Linienstrom bei bestehender Verbindung beträgt ca. 40 mA.</div>
 	</div>
-    <div><p>2007 wurde das öffentliche Fernschreibnetz in Deutschland abgeschaltet. Um ihre Fernschreibersammlungen trotzdem weiterbetreiben zu können, wurde von einigen
+    <p>2007 wurde das öffentliche Fernschreibnetz in Deutschland abgeschaltet. Um ihre Fernschreibersammlungen trotzdem weiterbetreiben zu können, wurde von einigen
     Enthusiasten das i-Telex entwickelt.
     Darüber konnten nun Fernschreiber zunächst nur über das Telefonnetz und inzwischen auch über Internet miteinander kommunizieren. Ein zentraler Server setzt
     die im i-Telex-Netzwerk vergebenen Telexnummern in Internetadressen um, so dass empfangsbereite Fernschreiber weltweit von anderen i-Telex-Teilnehmern direkt angewählt werden können
@@ -135,18 +129,16 @@
     <p>Einige unserer Fernschreiber sind mit einer solchen i-Telex-Anlage verbunden, so dass die Besucher selbst den Verbindungsaufbau und die Nachrichtenübermittlung
     (über Tastatur oder von Lochstreifen) ausprobieren können.<br/>
     Im Endausbau sollen in unserer Ausstellung 6 funktionsfähige Fernschreiber an das i-Telex angeschlossen sein.</p>
-    <p><a href="https://www.i-telex.net/" target="_blank">www.i-telex.net</a></p></div>
+    <p><a href="https://www.i-telex.net/" target="_blank">www.i-telex.net</a></p>
     <br/>
+
 
     <h2>Schreibautomaten</h2>
 
     <div class="box center auto-bildbreite">
-	    <img src="/shared/photos/kommunikationstechnik/olympia-schreibautomat.jpg" width="629" height="242" alt="Schreibautomat" />
-		<p class="bildtext">
-            Ein "Abfallprodukt" der Fernschreiber waren die elektromechanischen "Textverarbeitungssysteme" (1962-64), die mit Lochstreifen und -karten als
-            Datenträger Texte duplizieren und automatisch schreiben konnte. Eine aufwändige Technik deutscher Gründlichkeit, die sich damals nur große Firmen
-            leisten konnten. Hier ist ein Olympia Schreibautomat mit zwei Lochstreifenlesern und einem Lochstreifenstanzer aus dem Jahre 1962 abgebildet.
-        </p>
-	</div>
-
-    
+        <img src="/shared/photos/kommunikationstechnik/olympia-schreibautomat.jpg" width="629" height="242" alt="Schreibautomat" />
+	<p class="bildtext">
+        Ein "Abfallprodukt" der Fernschreiber waren die elektromechanischen "Textverarbeitungssysteme" (1962-64), die mit Lochstreifen und -karten als
+        Datenträger Texte duplizieren und automatisch schreiben konnte. Eine aufwändige Technik deutscher Gründlichkeit, die sich damals nur große Firmen
+        leisten konnten. Hier ist ein Olympia Schreibautomat mit zwei Lochstreifenlesern und einem Lochstreifenstanzer aus dem Jahre 1962 abgebildet.</p>
+    </div>

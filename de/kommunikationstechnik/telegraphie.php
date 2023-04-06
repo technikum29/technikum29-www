aabdcab66d2361a785f199b96c3a79add8815727
Detlef Gerhardt <feedback@dgerhardt.de>
2023-04-06 15:40:02 +0200

    Morsegeber und Morsehandlocher

--- de/kommunikationstechnik/telegraphie.php
+++ de/kommunikationstechnik/telegraphie.php
@@ -41,26 +41,33 @@
 
     <div class="box center auto-bildbreite">
         <img src="/shared/photos/kommunikationstechnik/morsetelegraph-um1900.jpg" width="439" height="249" alt="Fotografie eines Morsetelegraphes um 1900" />
-        <p class="bildtext">Dieser Telegraph stammt aus der Zeit um 1900. Kommunikation per Kabel lief vor über 100 Jahren noch sehr gemächlich. Die Zeiten waren ruhig und Hektik kein Thema.</p>
+        <p class="bildtext">Dieser Telegraph stammt aus der Zeit um 1900. Kommunikation per Kabel lief zu dieser Zeit noch sehr gemächlich. Die Zeiten waren ruhig und Hektik kein Thema.</p>
     </div>
 
     <div class="box center auto-bildbreite">
         <img src="/shared/photos/kommunikationstechnik/morseempfaenger.jpg" width="439" height="254" alt="Fotografie eines Morseempfängers" />
-        <p class="bildtext">Hier die seitliche Ansicht des Morse-Empfängers. Die Anlage ist im Museum an einen Lochstreifen-Morsesender der 60er Jahre angeschlossen.</p>
+        <p class="bildtext">Hier die seitliche Ansicht des Morse-Empfängers.</p>
     </div>
-    
+
     <h3 id="hell-mg12">Hell Morsegeber MG12</h3>
-    
+
 	<div class="box center"> 
 		<img src="/shared/photos/kommunikationstechnik/hell-mg12.jpg" width="427"  height="320"/>
 		<p class="bildtext"><b>Hell Morsegeber MG12</b></p>
 	</div>
-        <p>Beschreibunbgstext zum Hell MG12</p>
+        <p>Der Morsegeber ermöglichte das Senden von auf Lochstreifen vorgestanzten Morsenachrichten. Die Daten sind nach dem Wheatstone-Verfahren
+        kodiert und können mit einem Handlocher mit Schreibmaschinentastatur erstellt werden. Der Einsatz der Morsegeber hatte zwei Vorteile:
+        Zum einen konnten die Lochstreifen von Personal erstellt werden, die den Morsecode nicht beherrschten und zum anderen erfolgte
+        die Aussendung absolut gleichmäßig. Außerdem konnten die Sendung beliebig wiederholt werden. Auch für die Ausbildung wurden solche Morsegeber eingesetzt.
+        Kombinationen aus Handlocher und Morsegeber waren zum Beispiel bei Radio Norddeich im Einsatz. Dabei lief der Lochstreifen nach dem Stanzen direkt
+        in den Morsegeber und wurde direkt gesendet.</p>
 
     <h3 id="lorenz-hell-mg12">Lorenz Morsehandloch ML T36</h3>
     
 	<div class="box center"> 
 		<img src="/shared/photos/kommunikationstechnik/lorenz-ml-t36.jpg" width="427"  height="320"/>
-		<p class="bildtext"><b>Hell Morsegeber MG12</b></p>
+		<p class="bildtext"><b>Lorenz Morsehandloch ML T36</b></p>
 	</div>
-        <p>Beschreibunbgstext zum Lorenz ML T36</p>
+        <p>Mit dem Morsehandlocher wurden die Wheatstone-Lochstreifen für den Morsegeber erstellt. Da die Texte einfach über eine Schreibmaschinentastatur
+        eingegeben werden konnten, musste man zum Erstellen keine Morsecode können. Der Speziallochstreifen hat eine Breite von 12 mm und der Code
+        verwendet zwei Kanäle, ein Kanal zum Einschalten des Signals und einen zum Ausschalten. Zusätzlich gibt es noch das Transportloch.</p>

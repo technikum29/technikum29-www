85b5168e4b080e6c27d1afcf5cc4d287f5ca27c3
Detlef Gerhardt <feedback@dgerhardt.de>
2023-04-06 14:15:02 +0200

    englische Kommunikationsseiten

---
+++ en/communication/telegraphy.php
@@ -0,0 +1,66 @@
+<?php
+	$seiten_id = 'telegraphie';
+	$version = '$Id$';
+	$title = 'Telegraphy';
+	
+	require "../../lib/technikum29.php";
+?>
+    <!-- Etwas unkonventionell - mehrere h2 auf der Seite verteilt -->
+    <h2>Telegraphy</h2>
+
+    <div class="box left clear-after">
+        <a href="/en/devices/morse_telegraph.php" name="backlink-morse-telegraph"><img src="/shared/photos/kommunikationstechnik/telegrafie-regal.jpg" alt="The telegraph's rack" width="240" height="464" /></a>
+        <div class="bildtext">
+            <p>An extract from the area fax and writing engineering.</p>
+            <p>Humans always wanted to communicate over very long distances. In the early 20th century
+            "Morse" was almost synonymously used for the telegraph technology. Around 1938 the first
+            traffic telegraphs arised.</p>
+
+            <div class="center">
+                <img src="/shared/photos/kommunikationstechnik/morseschreiber.jpg" alt="Morse writer (1876)" style="float:none;"/>
+            </div>
+
+            <p>The picture above was printed in the little book "The technical telegraph service" from 1876. As you
+               can see, morse telegraphs were already used at that time. This kind of technology is amazing due
+               to it's overwhelming simplicity.
+               <br/>Clicking on the picture which shows the rack yields the <a class="go"
+               href="/en/devices/morse_telegraph.php">telegraph station</a>, made by S.A. HASLER (Bern, Switzerland).
+            </p>
+        </div>
+    </div>
+
+
+    <!--<div class="box left">
+        <img src="/shared/photos/kommunikationstechnik/ausschnitt-faxtechnik.jpg" alt="Ausschnitt aus dem Bereich Fax- und Schreibtechnik" width="436" height="327" />
+        <div class="bildtext" style="padding-top: 127px;">Extract from the area fax and writing engineering.</div>
+        <div class="clear">&nbsp;</div>
+    </div>
+    
+    <div class="box left" id="telegraf-zu">
+        <img src="/shared/photos/kommunikationstechnik/telegrafenstation-halboffen.jpg" alt="Telegrafenstation halboffen" class="nomargin-bottom" width="341" height="201" onclick="switchTelegrafenstation();" />
+        <div class="bildtext">
+            What is inside this inconspicuous wooden box? Klick it to open it.
+            - old text. This is the <a href="/en">box from the homepage</a>, now partly openend. Click on the old box and be astonished.-</div>
+        <div class="clear">&nbsp;</div>
+    </div>
+    
+    <div class="box center" id="telegraf-offen">
+        <img src="/shared/photos/kommunikationstechnik/telegrafenstation-offen.jpg" alt="Telegrafenstation offen"width="680" height="516" />
+        <div class="bildtext">
+            <h3>TELEGRAPH STATION, made by S.-A. HASLER, Bern (Switzerland)</h3>
+            -<p>More details will follow soon.</p>-
+        </div>
+    </div>-->
+
+     <div class="box center auto-bildbreite">
+          <img src="/shared/photos/kommunikationstechnik/morsetelegraph-um1900.jpg" width="439" height="249" alt="Morsetelegraph um 1900" />
+          <p class="bildtext">This telegraph station was built in the time about 1900. More than 100 years ago, no one cared about time
+            thus communication was quite unhurried.</p>
+      </div>
+
+     <div class="box center auto-bildbreite">
+          <img src="/shared/photos/kommunikationstechnik/morseempfaenger.jpg" width="439" height="254" alt="Morse reciever" />
+          <p class="bildtext">This picture shows the edgewise view from the morse reciever. The apparature is connected to a paper tape morse transmitter from the 60s.</p>
+     </div>
+	 
+	 

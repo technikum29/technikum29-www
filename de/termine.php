ba391fccadd92da31b9f2a8952f62109f7f30c12
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-04-20 08:48:20 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -34,15 +34,56 @@
 	Aufräumen und allen sonst so in einem Museum anfallenden Arbeiten unterstützen möchten. Auch hier bitte Anmeldung
 	per Mail an <a href="mailto:kontakt@technikum29.de">kontakt@technikum29.de</a>
 
-<div id="termine">	
+	
+<?php
+
+	
+	
+		
 <div class="box termin clear-after">		
-	<p class="date left"><em>2023</em> 30. Apr</p>
-	<h4>Privatführung Steven P.</h4> 
+	<p class="date left"><em>2023</em> 20. Mai</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 10:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
 	<dl class="daten">
 		<dt>Tag
-		<dd class="termin">Sonntag, 30. April 2023
+		<dd class="termin">Samstag,20. Mai 2023
 		<dt>Uhrzeit
-		<dd>10:00 - 14:00 Uhr
+		<dd>10:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
+	</dl>
+</div><!--/.termin -->
+		
+<div class="box termin clear-after">		
+	<p class="date left"><em>2023</em> 6. Mai</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 10:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag,6. Mai 2023
+		<dt>Uhrzeit
+		<dd>10:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
+	</dl>
+</div><!--/.termin -->
+
+	
+<div id="termine">	
+<div class="box termin clear-after">		
+	<p class="date left"><em>2023</em> 3. Mai</p>
+	<h4>Privatführung Mohanad R.</h4> 
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Sonntag, 3. Mai 2023
+		<dt>Uhrzeit
+		<dd>14:00 - 16:00 Uhr
 		<dt>Leitung
 		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>	
 		<dd>geschlossene Gesellschaft

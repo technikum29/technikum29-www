7ac102a3e081db98f3cc5080754fa16a9a16563c
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-04-26 17:20:31 +0200

    Neuanlage Tek 4052

---
+++ de/geraete/Tektronix_4052/Tektronix_4052_Details.php
@@ -0,0 +1,81 @@
+<?php
+	$seiten_id = 'TA1000';
+	$version = '$Id';
+	$titel = 'TA1000';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $titel; ?></h2>
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+	Stand: Sept. 2022
+</p>
+
+	<p> An dieser Stelle berichten wir über die Bestandsaufnahme zu unserer TA1000 und die Wiederinbetriebnahme der Maschine.
+	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
+</p>
+
+<h3>Wie kam die TA1000 ins technikum29?</h3>
+<p>Hier Links zur <a href="/blog/2022-04-27-Neuzugaenge.php">Herkunft</a> und zur 
+	<a href="/blog/2022-06-06-Neuzugaenge-Kassel-2022.php">Ankunft im technikum29</a>.
+</p>
+
+<h3>Aufbau unserer TA1000</h3>
+
+<p>Die Zentraleinheit unserer TA1000 besteht aus dem Rack mit Netzteil, div Module (CPU, ROM, ...) in einem Montagerahmen und zwei 8" 
+	Diskettenstationen. Daneben ein sw-Monitor und die große Druckeinheit für Magnetkontenkarten und Protokollausdrucke
+</p>
+<p>Details zu den einzelnen <b>Rechnermodulen</b> finden sich <a href="/de/geraete/TA1000/TA1000_Module.php">hier</a>
+</p>
+
+<h3>einige Detailbilder unserer TA1000</h3>
+
+	<p>Zum Vergrößern der Bilder bitte anklicken !</p>
+
+<div class="box center"> 
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_Typschild.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_Typschild.jpeg" width="200"  height="173"/>
+	</a>
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_Module.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_Module.jpeg" width="271"  height="300"/>
+	</a>
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_Gehaeuse_innen.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_Gehaeuse_innen.jpeg" width="225"  height="300"/>
+	</a>
+		<p class="bildtext"><b>TA1000</b> Typschild und Blick ins Gehäuse von vorn und hinten</p>
+	</div>
+
+<div class="box center"> 
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_Draufsicht_Floppies.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_Draufsicht_Floppies.jpeg" width="200"  height="252"/>
+	</a>
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_Draufsicht_Floppy.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_Draufsicht_Floppy.jpeg" width="189"  height="252"/>
+	</a>
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_front_Netzteil1.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_front_Netzteil1.jpeg" width="150"  height="113"/>
+	</a>
+		<a  href="/de/geraete/TA1000/TA1000_Bilder/TA1000_front_Netzteil2.jpeg" class="popup">
+		<img src="/de/geraete/TA1000/TA1000_Bilder/TA1000_front_Netzteil2.jpeg" width="150"  height="113"/>
+	</a>
+		<p class="bildtext"><b>TA1000</b> Draufsicht mit Floppies, Floppy einzeln und Netzteile</p>
+	</div>
+
+
+
+
+<h3>Wiederinbetriebnahme der Anlage</h3>
+
+
+	<p>Ohne Software auf Disketten, ohne Manuals und Schaltpläne wird die Inbetriebnahme "herausfordernd" werden. Evtl. 
+		findet der Gerätespender ja noch Unterlagen in seinen Depots.
+		<p/>
+	<p> *** wird fortgesetzt *** 
+		</p>
+
+<h3>Broschüren und Manuals zum Download</h3>
+
+<p>zum Download unserer Scans zur TA1000 geht es <a href="/de/geraete/TA1000/TA1000_PDFs">hier</a>
+</p>

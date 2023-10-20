a5c41585ea56d5c8d78010c04d638a30b7615706
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-10-20 14:17:55 +0200

    Einpflegen der STantec

---
+++ de/geraete/Stantec/Stantec-details.php
@@ -0,0 +1,59 @@
+<?php
+	$seiten_id = 'Stantec-details';
+	$version = '$Id';
+	$titel = 'Wiederinbetriebnahme der Stantec Zebra MK. II';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $title; ?></h2>
+
+<p>Stand: 20. Okt. 2023   <br><br>
+	</p>
+	
+<p>*** diese Seite wird noch bearbeitet ! ***
+	</p>
+
+<h3>Vorstellung unserer Stantec Zebra Mk. II</h3>
+
+<p>2023 wurde dem technikum29 eine Stantec Zebra Mk. II von Josef Röger (Weiden, OPflz.) übereignet.</p>
+
+<!--
+<div class="box center"> 
+		 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_Anlieferung_gr.jpeg" class="popup">
+		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_Anlieferung_kl.jpeg" width="200"  height="166"/>
+	</a>
+		 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_CPU_PCB_gr.jpeg" class="popup">
+		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_CPU_PCB_kl.jpeg" width="200"  height="166"/>
+	</a>
+			 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_a_gr.jpeg" class="popup">
+		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_a_kl.jpeg" width="200"  height="166"/>
+	</a>
+		 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_b_gr.jpeg" class="popup">
+		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_b_kl.jpeg" width="200"  height="166"/>
+	</a>
+	
+	
+	
+		<p class="bildtext">Die Pr1me 300 bei Anlieferung im technikum29 links neben dem Anelex-Drucker(li) sowie 
+		Bilder der Prozessorplatine (2.v.li) und der beiden unterschiedlichen RAM-Platinen (2v.re. und re.)<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
+	</div>
+-->	
+
+<h3>Wiederinbetriebnahme unserer Stantec</h3>
+
+
+<p> ..... </p>
+
+
+<h3>Eingescannte Manuals und weitere Bilder zur Stantec:</h3>
+
+	<p>zum Download unserer Dokumente geht es <a href="/de/geraete/Stantec/Stantec_PDF">hier</a>
+</p>
+	
+
+<br>
+<br>
+
+<p> wird fortgesetzt ... </p>
---
+++ de/rechnertechnik/stantec.php
@@ -0,0 +1,46 @@
+<?php
+	$seiten_id = 'stantec';
+	$version = '$Id$';
+	$titel = 'EDV-Anlage der 2. Generation: Stantec Mk. II';
+	
+	require "../../lib/technikum29.php";
+?>
+    <h2>EDV-Anlage der 2. Generation: <b>Stantec Zebra Mk. II</b></h2>
+ 
+ 
+ <p>Stand: 20.10.2023<br><br>
+
+ </p>
+
+		
+<p>Im November 2023 erwartet das technikum29 den Zugang einer wirklichen Computer-Rarität: <br>
+    eine <b>Stantec Zebra Mk. II.</b> von 1962 !!
+</p>
+	
+<div class="box center"> 
+		 <a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Werbung.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Werbung.jpeg" width="700" />
+	</a>
+		<p class="bildtext">Foto aus einer Werbebroschüre</p>
+	</div>
+	
+		
+<p>Unsere Stantec war von 1962 ab im Einsatz im Lebensmittel-Großhandel zur Lagerverwaltung. 1972 wurde sie eingelagert und
+	stand in einem alten Lagerhaus bis 2023, wo sie von ihrem Besitzer dem Museum angeboten wurde. Zur 
+	Zeit bereiten wir im technikum29 den Platz für den Computer vor, ca. Mitte November 2023 soll der Transfer
+	ins Museum erfolgen.
+</p>
+	
+<div class="box center"> 
+	<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_Fundort_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_Fundort_kl.jpeg" width="350" />
+		</a>
+	 <a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Zeitungsbericht.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Zeitungsbericht.jpeg" width="310" />
+		</a>
+		<p class="bildtext">li: die Konsole der Stantec Mk. II im aktuellen Zustand,<br>
+		re: ein Zeitungsartikel zur Inbetriebnahme der Maschine in 1962</p>
+	</div>
+	<br>
+	<p>Über den Fortschritt in der Reparatur der Stantec Zebra Mk. II werden wir im 
+	<a class="go" href="/de/geraete/Stantec/Stantec-details.php">Stantec-Reparaturblog</a> berichten.</p>

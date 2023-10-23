9a665bb62353364e5ccb3d0a5fbe2d0c582097a2
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-10-23 23:44:45 +0200

    Vorstellung Hamann 500 automatic

---
+++ blog/2023-10-2023-Neuzugang_Hamann_automatic_500.php
@@ -0,0 +1,44 @@
+<?php
+	$blog_title = "Neuzugang Hamann auutomatic 500";
+	$blog_title_kurz = "Neuzugang Hamann automatic 500";
+	$blog_author = "Roland";
+	$blog_date = "2023-10-23";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>Ein weiterer Neuzugang: eine elektro-mechanische Rechenmaschine <b>Hamann automatic 500</b>. Sie wurde uns von Rainer Schmitt (Wiesbaden) gespendet.
+	</p>
+	
+	
+<div class="box center"> 
+		<a  href="/de/geraete/Hamann500/Hamann500_Bilder/Hamann500_automatic_gr.jpeg" class="popup">
+		<img src="/de/geraete/Hamann500/Hamann500_Bilder/Hamann500_automatic_kl.jpeg" width="500"  "/>
+	</a>
+		
+		<p class="bildtext"><b>Hamann automatic 500</b> </p>
+	</div>
+
+<p>Die Hamann automatic 500 wurde 1959 erstmalig vorgestellt. Sie ist eine Vier-Spezies-Maschine  mit 9 * 8 * 16 Stellen, automatischer (verkürzter) 
+	Multiplikation und Division sowie Rückübertragung. Sie ist eine Simplexmaschine nach dem System Sperrklinke und hat eine "moderne" Zehnertastatur 
+	ähnlich unserer heutiger Taschenrechner. Mit 17 kg ist sie nicht gerade ein Leichtgewicht. Sie ist sehr gut gekapselt und sehr leise im Betrieb.<br>
+	Das Baujahr unserer Maschine ist unbekannt. Unsere Hamann war in sehr gutem Erhaltungszustand. Nach der 
+	üblichen Reinigung von verharztem Öl und Fett und der Gängigmachung verklebter Mechanik läuft sie wieder problemlos.<br>
+	</p>
+
+
+
+
+<b>Wir bedanken uns bei Dr. Rainer Schmitt für diese tolle Ergänzung unserer Sammlung !</b><br><br>
+
+Roland
+
+
+
+
+  
+<?php print_author_info(); ?>
+

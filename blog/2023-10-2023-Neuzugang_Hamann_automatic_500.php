616d014ce6d6eee28855ae79d89c0377ac1393b5
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-10-24 17:02:18 +0200

    Update 2023-10-2023-Neuzugang_Hamann_automatic_500.php

--- blog/2023-10-2023-Neuzugang_Hamann_automatic_500.php
+++ blog/2023-10-2023-Neuzugang_Hamann_automatic_500.php
@@ -30,7 +30,7 @@
 	Multiplikation und Division sowie Rückübertragung. Sie ist eine Simplexmaschine nach dem System Sperrklinke und hat eine "moderne" Zehnertastatur 
 	ähnlich unserer heutiger Taschenrechner. Mit 17 kg ist sie nicht gerade ein Leichtgewicht. Sie ist sehr gut gekapselt und sehr leise im Betrieb.<br><br>
 	Das Baujahr unserer Maschine ist wohl 1959: sie hat eine frühe Seriennummer (150.198) und ist die 198ste Maschine von ca 9600 produzierten Einheiten.<br><br>Unsere Hamann war in sehr gutem Erhaltungszustand. Nach der 
-	üblichen Reinigung von verharztem Öl und Fett und der Gängigmachung verklebter Mechanik läuft sie wieder problemlos.<br>
+	üblichen Reinigung von verharztem Öl und Fett, dem Austausch zweier defekter Kondensatoren und der Gängigmachung verklebter Mechanik läuft sie wieder problemlos.<br>
 	</p>
 
 <div class="box center"> 

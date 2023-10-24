c1ea5bf0b23d57363a48040d71788e95786568b2
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-10-24 17:01:09 +0200

    Update 2023-10-2023-Neuzugang_Hamann_automatic_500.php

--- blog/2023-10-2023-Neuzugang_Hamann_automatic_500.php
+++ blog/2023-10-2023-Neuzugang_Hamann_automatic_500.php
@@ -28,8 +28,8 @@
 
 <p>Die Hamann automatic 500 wurde 1959 erstmalig vorgestellt und bis 1962 produziert. Sie ist eine Vier-Spezies-Maschine  mit 9 * 8 * 16 Stellen, automatischer (verkürzter) 
 	Multiplikation und Division sowie Rückübertragung. Sie ist eine Simplexmaschine nach dem System Sperrklinke und hat eine "moderne" Zehnertastatur 
-	ähnlich unserer heutiger Taschenrechner. Mit 17 kg ist sie nicht gerade ein Leichtgewicht. Sie ist sehr gut gekapselt und sehr leise im Betrieb.<br>
-	Das Baujahr unserer Maschine ist wohl 1959: sie hat eine frühe Seriennummer (150.198) und ist die 198ste Maschine von ca 9600 produzierten Einheiten.<br>Unsere Hamann war in sehr gutem Erhaltungszustand. Nach der 
+	ähnlich unserer heutiger Taschenrechner. Mit 17 kg ist sie nicht gerade ein Leichtgewicht. Sie ist sehr gut gekapselt und sehr leise im Betrieb.<br><br>
+	Das Baujahr unserer Maschine ist wohl 1959: sie hat eine frühe Seriennummer (150.198) und ist die 198ste Maschine von ca 9600 produzierten Einheiten.<br><br>Unsere Hamann war in sehr gutem Erhaltungszustand. Nach der 
 	üblichen Reinigung von verharztem Öl und Fett und der Gängigmachung verklebter Mechanik läuft sie wieder problemlos.<br>
 	</p>
 

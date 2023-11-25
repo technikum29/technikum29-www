cbd3c89a6ce3a6b514e6f32335b9ffd82dca80fd
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2023-11-25 16:52:40 +0100

    viele Stantec-Details eingefügt

--- de/geraete/Stantec/Stantec-details.php
+++ de/geraete/Stantec/Stantec-details.php
@@ -8,7 +8,7 @@
 
 <h2><?php print $title; ?></h2>
 
-<p>Stand: 16. Nov. 2023   <br><br>
+<p>Stand: 24. Nov. 2023   <br><br>
 	</p>
 	
 <p>*** diese Seite wird noch bearbeitet ! ***
@@ -18,30 +18,122 @@
 
 <p>2023 wurde dem technikum29 eine Stantec Zebra Mk. II von der ehem. SPAR-Zentrale Oberpfalz durch Herrn Josef Röger 
 	(Weiden, OPflz.) als Dauerleihgabe übergeben.</p>
-
-<!--
+	
 <div class="box center"> 
-		 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_Anlieferung_gr.jpeg" class="popup">
-		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_Anlieferung_kl.jpeg" width="200"  height="166"/>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_geschlossen_links_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_geschlossen_links_kl.jpeg" width="600" />
 	</a>
-		 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_CPU_PCB_gr.jpeg" class="popup">
-		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_CPU_PCB_kl.jpeg" width="200"  height="166"/>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Typschild_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Typschild_kl.jpeg" width="200" />
 	</a>
-			 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_a_gr.jpeg" class="popup">
-		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_a_kl.jpeg" width="200"  height="166"/>
-	</a>
-		 <a  href="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_b_gr.jpeg" class="popup">
-		<img src="/de/geraete/Pr1me300/Pr1me300_Bilder/Pr1me300_RAM_PCB_b_kl.jpeg" width="200"  height="166"/>
-	</a>
-	
-	
-	
-		<p class="bildtext">Die Pr1me 300 bei Anlieferung im technikum29 links neben dem Anelex-Drucker(li) sowie 
-		Bilder der Prozessorplatine (2.v.li) und der beiden unterschiedlichen RAM-Platinen (2v.re. und re.)<br>
+		<p class="bildtext">unsere <b>Stantec Zebra Mk. II</b> in der Ausstellung, rechts das Typschild<br>
 		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
 	</div>
--->	
+	
+	
+<p><b>Ansicht der Bedienkonsole:</b></p>	
 
+
+<div class="box center"> 
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_komplett_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_komplett_kl.jpeg" width="340" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_links_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_links_kl.jpeg" width="132" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_mitte_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_mitte_kl.jpeg" width="88" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_rechts_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Konsole_rechts_kl.jpeg" width="132"/>
+	</a>	
+		<p class="bildtext">Die Konsole der Stantec im Detail<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
+	</div>
+
+<p><b>Blick in den Elektronikschrank:</b></p>	
+
+<div class="box center"> 
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_links_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_links_kl.jpeg" width="50" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_links_oben_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_links_oben_kl.jpeg" width="125" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_links_1_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_links_1_kl.jpeg" width="127" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_kl.jpeg" width="45"/>
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_1_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_1_kl.jpeg" width="125" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_2_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_2_kl.jpeg" width="115" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_3_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_mitte_3_kl.jpeg" width="120"/>
+	</a>	
+		<p class="bildtext">Blick in den Elektronikschrank von vorne (links und mitte)<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
+	</div>
+	
+<div class="box center"> 
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_kl.jpeg" width="57" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_1_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_1_kl.jpeg" width="155" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_2_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_2_kl.jpeg" width="153" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_3_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_3_kl.jpeg" width="151"/>
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_quer_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Rack_vorne_rechts_quer_kl.jpeg" width="118" />
+	</a>
+
+		<p class="bildtext">Blick in den Elektronikschrank von vorne (rechts)<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
+	</div>	
+	
+<p><b>Die Platinen:</b></p>	
+
+<div class="box center"> 
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Platine_vorne_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Platine_vorne_kl.jpeg" width="300" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Platine_rueck_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Platine_rueck_kl.jpeg" width="300" />
+	</a>
+	<p class="bildtext">Beispiel einer der Platinen<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
+	</div>		
+	
+<p><b>Diverse Hinweise:</b></p>	
+
+<div class="box center"> 
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Umschlag_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Umschlag_kl.jpeg" width="200" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Hinweis_Brief_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Hinweis_Brief_kl.jpeg" width="150" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Hinweis_Trommel_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Hinweis_Trommel_kl.jpeg" width="105" />
+	</a>
+<a  href="/de/geraete/Stantec/Stantec_Bilder/Stantec_Notiz_Tuer_gr.jpeg" class="popup">
+		<img src="/de/geraete/Stantec/Stantec_Bilder/Stantec_Notiz_Tuer_kl.jpeg" width="130" />
+	</a>
+	<p class="bildtext">Ein Brief lag in dem Elektronikschrank (li, 2.v.li), ein Hinweis klebte an der Trommel (2.v.re) 
+		und ein Zettel klebte in einer der Türen (re)<br>
+		Für eine vergrößerte Ansicht bitte auf die Bilder klicken !</p>
+	</div>		
+		
+	
 <h3>Wiederinbetriebnahme unserer Stantec</h3>
 
 

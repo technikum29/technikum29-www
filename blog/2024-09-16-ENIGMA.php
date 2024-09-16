cfacf046caea9d29f23d7ccf50d5971085cd9dbd
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2024-09-16 10:55:37 +0200

    Neuer Blogbeitrag zur Enigma und Enigma Geräteseite

---
+++ blog/2024-09-16-ENIGMA.php
@@ -0,0 +1,52 @@
+<?php
+	$blog_title = "technikum29 erhielt eine ENIGMA !";
+	$blog_title_kurz = "technikum29 erhielt eine ENIGMA !";
+	$blog_author = "Roland";
+	$blog_date = "2024-09-16";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p><b>Neu im technikum29: eine ENIGMA !</b></p>
+
+<p>Am 9.9.2024 verzeichnete das technikum29 einen besonderen Neuzugang: eine funktionstüchtige <b>ENIGMA Rotor 
+	Chiffriermaschine</b>. Genauer gesagt: eine sehr detailgetreue Replika des Modells ENIGMA I.</p>
+	
+<div class="box center"> 
+		<a  href="/de/geraete/Enigma_Replika/Enigma_Replika_Bilder/Enigma_Replika_Gehaeuse_gr.jpeg" class="popup">
+		<img src="/de/geraete/Enigma_Replika/Enigma_Replika_Bilder/Enigma_Replika_Gehaeuse_kl.jpeg" width="300"  "/>
+	</a>
+		<a  href="/de/geraete/Enigma_Replika/Enigma_Replika_Bilder/Enigma_Replika_offen_Uebersicht_gr.jpeg" class="popup">
+		<img src="/de/geraete/Enigma_Replika/Enigma_Replika_Bilder/Enigma_Replika_offen_Uebersicht_kl.jpeg" width="155"  "/>
+	</a>
+		<a  href="/de/geraete/Enigma_Replika/Enigma_Replika_Bilder/Enigma_Replika_Ansicht_gr.jpeg" class="popup">
+		<img src="/de/geraete/Enigma_Replika/Enigma_Replika_Bilder/Enigma_Replika_Ansicht_kl.jpeg" width="250"  "/>
+	</a>		
+		<p class="bildtext">Zum Vergrößern bitte das Bild anklicken !</p>
+	</div>
+
+<p>In einem schönen Holzgehäuse (Bild links) befindet sich der sehr detailgetreue Nachbau einer ENIGMA I (Bild mitte und rechts). 
+	Entworfen und gebaut wurde sie von Gunther Tünnermann (Kelkheim). Wir erhielten dieses schöne Stück Technikgeschichte 
+	von seiner Witwe als Spende an das Museum.</p>
+	
+<p>Chiffriermaschinen wie die ENIGMA wurden zur Ver- und Entschlüsselung von Nachrichten genutzt. Über eine Tastatur 
+	wird der Text eingegeben. Über einen mehrstufigen Prozess wird der eingegebene Buchstabe mehrfach (7 - 10 mal) durch 
+	alphabetische Substitution "verwürfelt" und über ein Glühlampenfeld ausgegeben. Diese mehrfache Substitution ist bei der 
+	ENIGMA konstruktionsbedings symmetrisch. Das bedeutet, dass die Entschlüsselung in gleicher Weise erfolgt: man gibt den 
+	chiffrierten Text Buchstaben für Buchstaben ein und liest die Nachricht am Glühlampenfeld ab.</p>
+	
+	
+<p>Wir werden in Kürze detailliert über dieses schöne Stück <a href="/de/geraete/Enigma_Replika/Enigma_Replika_Details.php">hier</a>  
+	berichten !</p>
+
+
+<br>
+Roland
+<br><br>
+
+
+  
+<?php print_author_info(); ?>
---
+++ de/geraete/Enigma_Replika/Enigma_Replika_Details.php
@@ -0,0 +1,115 @@
+<?php
+	$seiten_id = 'EnigmaReplika';
+	$version = '$Id';
+	$titel = 'ENIGMA Replika';
+	
+	require '../../../lib/technikum29.php';
+?>
+
+<h2><?php print $titel; ?></h2>
+
+	<p>
+	*** ACHTUNG BAUSTELLE ! ***
+	Stand: 16.9.2024
+</p>
+
+	<p> An dieser Stelle werden wir über die Bestandsaufnahme zu unserer <b>ENIGMA Replika</b> berichten.
+	Die Seite wird kontinuierlich mit neuen Inhalten ergänzt.
+</p>
+
+
+
+<h3>einige Detailbilder unserer Q1</h3>
+
+<!--
+	<p>Zum Vergrößern der Bilder bitte anklicken !</p>
+
+<div class="box center"> 
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Blick_ins-Chassis.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Blick_ins-Chassis.jpg" width="700"  height="434"/>
+	</a>
+		<p class="bildtext"><b>Q1 lite</b> Blick in die geöffnete Maschine von hinten</p>
+	</div>
+
+
+<div class="box center"> 
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Hauptplatine.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Hauptplatine.jpg" width="300"  height="200"/>
+	</a>
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_PCB_Display.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_PCB_Display.jpg" width="382"  height="200"/>
+	</a>
+		<p class="bildtext"><b>Q1 lite</b> Hauptplatine (links) und Displayplatine (rechts)</p>
+	</div>
+
+<div class="box center"> 
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_links.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_links.jpg" width="350"  height="216"/>
+	</a>
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_rechts.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Netzteil_rechts.jpg" width="352"  height="216"/>
+	</a>
+		<p class="bildtext"><b>Q1 lite</b> Netzteilplatine linke und rechte Seite</p>
+	</div>
+
+
+<div class="box center"> 
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display.jpg" width="300"  height="225"/>
+	</a>
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display_nah.jpg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Display_nah.jpg" width="372"  height="225"/>
+	</a>
+		<p class="bildtext"><b>Q1 lite</b> Plasmadisplay Gesamtansicht und Nahaufnahme</p>
+	</div>
+
+<div class="box center"> 
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Typschild.jpeg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Typschild.jpeg" width="281"  height="225"/>
+	</a>
+		<a  href="/de/geraete/Q1_lite/Q1_Bilder/Q1_Printer_Typschild.jpeg" class="popup">
+		<img src="/de/geraete/Q1_lite/Q1_Bilder/Q1_Printer_Typschild.jpeg" width="340"  height="225"/>
+	</a>
+		<p class="bildtext"><b>Q1 lite</b> Typenschild Computer und Drucker</p>
+	</div>
+-->
+
+
+<h3>Bedienung der ENIGMA</h3>
+
+<!--
+	<p>Nach allen Unterlagen, die uns zugänglich sind, ist ein Betrieb der Q1 ohne Diskettenstation nicht möglich. Von
+		daher wird unsere Q1 ein "ruhendes" Expemplar bleiben. Ob in ROMs ein Monitorprogramm steckt, das evtl.
+		einen rudimentären Betrieb zulässt, kann erst geprüft werden, wenn die Maschine nach dem Einschalten auf
+		Tastendrücke reagiert (was sie im Augenblick noch nicht tut).
+		<p/>
+	<p><b>Update 23.10.2022:</b>
+		<p/>	
+	<p>Mittlerweile haben wir Kontakt zu einigen Q1-Besitzern aufgenommen. Zusammen verfügen wir über die notwendigen 
+		Gerätschaften und sogar Floppys mit Software. Gemeinsam sollten wir die eine oder andere Q1 wieder zum Leben
+		erwecken können ! </p>
+	<p><a href="https://www.thebyteattic.com/p/q1.html">Bernardo</a> besitzt eine Q1 mit Floppylaufwerk, er möchte die
+		gesamte Hardware reengineeren</p>
+	<p>Bart von dem <a href="https://www.homecomputermuseum.nl/#intro">Niederländischen Home Computer Museum</a> bietet uns an, 
+		ihre Q1-Floppy zu leihen für Tests</p>
+	<p>Auf einer <a href="http://www.peel.dk/Q1/"> Dänischen Webseite</a> werden gleich 2 (!!) Q1en präsentiert.</p>
+	<p>Mattis vom  <a href="http://www.datormuseum.se/available.html"> Schwedischen "Datormuseum""</a> hat diverse Q1 Hardware im 
+		Bestand. Er hat Versuche unternommen, seine Q1-Floppysammlung zu digitalisieren.</p>
+	<p>Karl and Robin unterstützten uns mit Scans seltener Handbücher und Schaltpläne  !</p>
+-->
+	<p>
+		</p>
+	<p>
+		</p>
+	
+	<p> *** wird fortgesetzt *** 
+		</p>
+	<p>
+		</p>
+
+<h3>Broschüren und Manuals zum Download</h3>
+
+<p>zum Download div Dokumente geht es <a href="/de/geraete/Enigma_Replika/Enigma_Replika_PDFs">hier</a>
+</p>
+
+

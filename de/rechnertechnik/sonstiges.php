9a58b3072d811b32804132f0733e60ef3efadc05
SvenK <git+sveto@svenk.org>
2024-09-19 15:04:34 +0200

    Neue Rubrik "Sonstiges" im Computerbereich

---
+++ de/rechnertechnik/sonstiges.php
@@ -0,0 +1,12 @@
+<?php
+	$seiten_id = 'sonstiges';
+	$titel = 'Sonstige Computertechnik';
+	
+	require "../../lib/technikum29.php";
+?>
+    
+<h2>Sonstige Computertechnik</h2>
+
+<h3 id="enigma">Enigma</h3>
+
+<p>Text zur Enigma erscheint an dieser Stelle.

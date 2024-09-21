fbfdabc2c6fccab4e13e5a1d11392e2026a8a5ef
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2024-09-21 20:48:15 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -96,7 +96,7 @@
 </div>
 	
 <div class="box termin clear-after">		
-	<p class="date left"><em>2024</em> 5. Okt</p>
+	<p class="date left"><em>2024</em> 12. Okt</p>
 	<h4>allgemeiner Helfertag</h4> 
 	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
 		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
@@ -105,12 +105,12 @@
 	 	per Mail anmelden !</b>
 	<dl class="daten">
 		<dt>Tag
-		<dd class="termin">Samstag, 5. Okt 2024
+		<dd class="termin">Samstag, 12. Okt 2024
 		<dt>Uhrzeit
 		<dd>9:00 - 18:00 Uhr
 		<dt>Leitung
 		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
-		<dd>Instandsetzung div. Geräte nach Absprache
+		<dd>Instandsetzung div. Geräte nach Absprache, LGP30 Restaurierung
 	</dl>
 </div>
 

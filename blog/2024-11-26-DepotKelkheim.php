6ccfcb8fa6851c8eb4e8869f446a9810ccff99a0
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2024-11-27 16:04:17 +0100

    Blogbeitrag neues Depot Kelkheim

---
+++ blog/2024-11-26-DepotKelkheim.php
@@ -0,0 +1,50 @@
+<?php
+	$blog_title = "technikum29 bezieht neues Depot !";
+	$blog_title_kurz = "technikum29 bezieht neues Depot !";
+	$blog_author = "Roland";
+	$blog_date = "2024-11-27";
+	$blog_lang = "de";
+
+	require "blog.php";
+	
+	print_blog_title();
+?>
+
+<p>In den letzten Jahren waren unsere Außendepots verstreut über Kelkheim und darüber hinaus. Wir hatten 
+	sie nach und nach entrümpelt und ihre Anzahl reduziert. Die verbliebenen Lagerräume waren provisorisch 
+	und nur vorübergehend zu nutzen. <b>Das hat nun ein Ende !</b> Nachdem wir vor kurzem mit einem Artikel im
+	 <a href="/blog/2024-08-23-technikum29_in_der_Zeitung.php">Höchster Kreisblatt</a> präsent 
+	waren, meldete sich ein Kelkheimer auf unsere Suche nach Depoträumen. Schnell war man sich einig 
+	und der LKW mit der ersten Fuhre war gepackt:</p>
+	
+<div class="box center"> 
+		<a  href="/shared/photos/blog/DepotKelkheim1.jpeg" class="popup">
+		<img src="/shared/photos/blog/DepotKelkheim1kl.jpeg" width="400"  "/>
+	</a>
+		
+		<p class="bildtext">Die erste Fuhre ist verladen (zum Vergrößern bitte das Bild anklicken !)</p>
+	</div>
+
+<p>Das Ziel der Fahrt: eine ehemalige Backstube, sauber, trocken, ideal geeignet, um unsere im Museum 
+	nicht benötigten Stücke zu beherbergen.</p>
+
+<div class="box center"> 
+		<a  href="/shared/photos/blog/DepotKelkheim2.jpeg" class="popup">
+		<img src="/shared/photos/blog/DepotKelkheim2kl.jpeg" width="400"  "/>
+	</a>
+		<p class="bildtext">Die ersten Geräte im neuen Depot: UNIVAC 9200 mit Kartenlocher 1710 und zwei 
+		Diskstationen 8425 sowie ein Kartenleser/-stanzer 1442 (für die IBM 1130)</p>
+	</div>
+
+	
+<p>In den Räumlichkeiten ist noch reichlich Platz, so dass wir in den nächsten Wochen noch andere Gerätschaften 
+	hier lagern werden. Damit sind dann alle Außendepots in diesem "Zentraldepot" zusammengefasst.</p>
+	
+
+<br>
+Roland
+<br><br>
+
+
+  
+<?php print_author_info(); ?>

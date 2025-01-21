21f989954d02d5fabfb0be62c05cd47ae4b99b5e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2025-01-21 12:59:07 +0100

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -147,6 +147,20 @@
 
 <div class="box termin clear-after">		
 	<p class="date left"><em>2025</em> 8. Mär</p>
+	<h4>Führung Gruppe Emil K.</h4> 
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 8. März 2025
+		<dt>Uhrzeit
+		<dd>10:00 - 13:00
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>	
+		<dd>
+	</dl>
+</div>
+	
+<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 8. Mär</p>
 	<h4>allgemeiner Helfertag</h4> 
 	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
 		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt

24add88d27403aabdb4f0e23a123df677f0e6565
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2025-06-07 12:36:30 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -313,6 +313,25 @@
 
 <div class="box termin clear-after">		
 	<p class="date left"><em>2025</em> 12. Juli</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+	<p>Wer sich das Museum nur mal so anschauen will ist ebenfalls herzlich willkommen ! <b>Aber bitte unbedingt
+	 	per Mail anmelden !</b>
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 12. Juli 2025
+		<dt>Uhrzeit
+		<dd>9:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
+	</dl>
+</div>
+	
+	<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 12. Juli</p>
 	<h4>Führung Gruppe Timo B.</h4> 
 	<dl class="daten">
 		<dt>Tag
@@ -325,6 +344,26 @@
 	</dl>
 </div>
 
+<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 6. Sep</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+	<p>Wer sich das Museum nur mal so anschauen will ist ebenfalls herzlich willkommen ! <b>Aber bitte unbedingt
+	 	per Mail anmelden !</b>
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 6. Sep. 2025
+		<dt>Uhrzeit
+		<dd>9:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
+	</dl>
+</div>
+	
+	
 <div class="box termin clear-after">		
 	<p class="date left"><em>2025</em> 6. Sep</p>
 	<h4>Führung Gruppe Arno B.</h4> 

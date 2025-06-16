9b9aa36cb7fd138f12dcb1df23ef68f608012c88
SvenK <git+sveto@svenk.org>
2025-06-16 12:53:32 +0200

    Remvoing PHP: t29v6 PHP front matter to SSG front matter.
    Using the v8-remove-php.py script.

--- de/archiv/computertechnik.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$seiten_id = 'archiv_computertechnik';
-	$version = '$Id$';
-	$titel = 'Computertechnik';
-	
-	require "../../lib/technikum29.php";
-?>
-    <h2>Archiv - Computertechnik</h2>
-
--- v8-remove-php.py
+++ v8-remove-php.py
@@ -31,47 +31,50 @@ php_simple_assignment = re.compile(r"\$([A-Za-z1-9_]\w*)\s*=\s*(.*?)\s*;")
 
 for fname in args.files:
     src_name = pathlib.Path(fname)
-    dst_name = src_name.with_suffix(".html")
+    dst_name = src_name.with_suffix(".htm")
     fname_data = collections.OrderedDict() #{}
     
     with src_name.open(encoding="utf-8") as src:
-        first_line = next(src)
-        if first_line.strip() != "<?php":
-            warn(f"{src_name}: No PHP Header found ({first_line=})")
-            continue
-
-        for src_line in src:
-            src_line = src_line.strip()
-            if not src_line: # empty line
-                continue
-            if src_line == "?>":
-                break
-            if "../lib/technikum29.php" in src_line: # PHP include/require line
+        try:
+            first_line = next(src)
+            if first_line.strip() != "<?php":
+                warn(f"{src_name}: No PHP Header found ({first_line=})")
                 continue
 
-            m = php_simple_assignment.match(src_line)
-            if m:
-                fname_data[m.group(1)] = m.group(2)
-            else:
-                warn(f"{src_name}: Don't understand line {src_line}")
-        
-        # remove Subversion artefact
-        if "version" in fname_data and "$Id" in fname_data["version"]:
-            del fname_data["version"]
-        
-        # consider switching from single to double quoting, i.e. 'bla' => "bla"
-        # using 'bla' was PHP optimization for string interpolation but reads badly.
-        for k,v in fname_data.items():
-            if v.count("'") == 2 and '"' not in v and v[0] == "'" and v[-1] == "'":
-                fname_data[k] = f'"{v[1:-1]}"'
-        
-        if len(fname_data):
-            with dst_name.open("w", encoding="utf-8") as dst:
-                dst.write("---\n")
-                # to avoid properly parsing quotes, we just dump the PHP-quoted data directly
-                #yaml.safe_dump(dict(fname_data), dst, allow_unicode=True, sort_keys=False)
-                for k,v in fname_data.items():
-                    dst.write(f"{k}: {v}\n")
-                dst.write("---\n")
-                for src_line in src: # write all remaining
-                    dst.write(src_line)
+            for src_line in src:
+                src_line = src_line.strip()
+                if not src_line: # empty line
+                    continue
+                if src_line == "?>":
+                    break
+                if "../lib/technikum29.php" in src_line: # PHP include/require line
+                    continue
+
+                m = php_simple_assignment.match(src_line)
+                if m:
+                    fname_data[m.group(1)] = m.group(2)
+                else:
+                    warn(f"{src_name}: Don't understand line {src_line}")
+            
+            # remove Subversion artefact
+            if "version" in fname_data and "$Id" in fname_data["version"]:
+                del fname_data["version"]
+            
+            # consider switching from single to double quoting, i.e. 'bla' => "bla"
+            # using 'bla' was PHP optimization for string interpolation but reads badly.
+            for k,v in fname_data.items():
+                if v.count("'") == 2 and '"' not in v and v[0] == "'" and v[-1] == "'":
+                    fname_data[k] = f'"{v[1:-1]}"'
+            
+            if len(fname_data):
+                with dst_name.open("w", encoding="utf-8") as dst:
+                    dst.write("---\n")
+                    # to avoid properly parsing quotes, we just dump the PHP-quoted data directly
+                    #yaml.safe_dump(dict(fname_data), dst, allow_unicode=True, sort_keys=False)
+                    for k,v in fname_data.items():
+                        dst.write(f"{k}: {v}\n")
+                    dst.write("---\n")
+                    for src_line in src: # write all remaining
+                        dst.write(src_line)
+        except UnicodeDecodeError:
+            warn(f"{src_name}: Non UTF-8 character found, cannot proceed")

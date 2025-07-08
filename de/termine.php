dac8cb82d090fbf58e59d6875f8b6a74662a0f4e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2025-07-08 10:36:16 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -301,7 +301,7 @@
 -------	ENDE zurueckliegende Termine -->
 	
 
-
+<!-- Pausenkommentar
 	<div class="box termin" style="background-color: #f2eded; color: red";>
   <h4 style="color: #8e1b1b"></h4>
 	
@@ -310,6 +310,8 @@
 	Wir werden uns ab Mitte Juli 2025 wieder mit neuen Terminen zurückmelden, deshalb...
 <p><strong>Bitte behaltet diese Webseite im Auge, neue Termine für 2025 werden hier zu gg. Zt. veröffentlicht.</strong>
 </div>
+
+Ende Pausenkommentar--->
 
 <div class="box termin clear-after">		
 	<p class="date left"><em>2025</em> 12. Juli</p>
@@ -332,7 +334,7 @@
 	
 	<div class="box termin clear-after">		
 	<p class="date left"><em>2025</em> 12. Juli</p>
-	<h4>Führung Gruppe Timo B.</h4> 
+	<h4>Führung Gruppe Timo B. und Leon S.</h4> 
 	<dl class="daten">
 		<dt>Tag
 		<dd class="termin">Samstag, 12. Juli 2025
@@ -341,6 +343,59 @@
 		<dt>Leitung
 		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>	
 		<dd>
+	</dl>
+</div>
+
+
+<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 19. Juli</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+	<p>Wer sich das Museum nur mal so anschauen will ist ebenfalls herzlich willkommen ! <b>Aber bitte unbedingt
+	 	per Mail anmelden !</b>
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 19. Juli 2025
+		<dt>Uhrzeit
+		<dd>9:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
+	</dl>
+</div>
+	
+	<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 19. Juli</p>
+	<h4>Führung Gruppe Malte K.</h4> 
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 19. Juli 2025
+		<dt>Uhrzeit
+		<dd>10:00 - 13:00
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>	
+		<dd>
+	</dl>
+</div>
+
+<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 2. Aug</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+	<p>Wer sich das Museum nur mal so anschauen will ist ebenfalls herzlich willkommen ! <b>Aber bitte unbedingt
+	 	per Mail anmelden !</b>
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 2. August 2025
+		<dt>Uhrzeit
+		<dd>9:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
 	</dl>
 </div>
 

483124fb4eb796a5b87519a538c4d437ddd9212e
Roland <54443215+RolandLangfeld@users.noreply.github.com>
2025-08-21 22:36:39 +0200

    Update termine.php

--- de/termine.php
+++ de/termine.php
@@ -487,6 +487,39 @@ Ende Pausenkommentar--->
 	</dl>
 </div>
 
+<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 18. Okt</p>
+	<h4>allgemeiner Helfertag</h4> 
+	<p>An diesem Termin ist ein Betreuer von 9:00-18:00 vor Ort, Unterstützer und Interessierte können gerne jederzeit
+		dazustossen und an ihren Projekten (weiter-)arbeiten. <b>Zur besseren Planung bitten wir unbedingt
+		um eine Anmeldung per Mail !</b>
+	<p>Wer sich das Museum nur mal so anschauen will ist ebenfalls herzlich willkommen ! <b>Aber bitte unbedingt
+	 	per Mail anmelden !</b>
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 18. Oktober 2025
+		<dt>Uhrzeit
+		<dd>9:00 - 18:00 Uhr
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>
+		<dd>Instandsetzung div. Geräte nach Absprache
+	</dl>
+</div>
+
+<div class="box termin clear-after">		
+	<p class="date left"><em>2025</em> 18. Okt</p>
+	<h4>Führung Gruppe Matthias H.</h4> 
+	<dl class="daten">
+		<dt>Tag
+		<dd class="termin">Samstag, 18. Oktober 2025
+		<dt>Uhrzeit
+		<dd>14:00 - 17:00
+		<dt>Leitung
+		<dd><a href="/de/team.php#roland">Dr. Roland Langfeld</a>	
+		<dd>
+	</dl>
+</div>
+	
 <div style="margin-top:3em">
 	
 	<!--Die folgende Tabelle listet Gruppenanmeldungen und Events auf.-->

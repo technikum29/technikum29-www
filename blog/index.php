a1aa0144c9c4c88a87af69d238b3fd9d2ca33599
SvenK <git+sveto@svenk.org>
2025-10-03 16:53:13 +0200

    Fixing merged new php content

--- blog/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/*
-	   The blog entry pages are now handled by the languages
-	   itself, i.e. /de/blog.php and /en/blog.php.
-	   Since we are lazy, we let the global entry page do the
-	   decision to determine the browser language.
-	*/
-	header("Location: /?suffix=blog.php");
-?>
